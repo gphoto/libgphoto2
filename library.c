01166f447ec631d53899688056755bc0191c4aee
Scott Fritzinger <scottf@unr.edu>
2000-06-19 19:24:24 +0000

    added camera library id's. enumerated camera differently (abilities per-camera now).
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@558 67ed7778-7388-44ab-90cf-0a291f65f57c

--- library.c
+++ library.c
@@ -1,5 +1,7 @@
+#include <dirent.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <gphoto2.h>
 
 #include "core.h"
@@ -7,9 +9,12 @@
 #include "util.h"
 
 extern CameraChoice glob_camera[];
+extern CameraAbilities glob_camera_abilities[];
 extern int     glob_camera_count;
 extern void   *glob_library_handle;
 extern Camera  glob_c;
+extern char   *glob_camera_id[];
+extern int     glob_camera_id_count;
 
 int is_library(char *library_filename) {
 
@@ -40,6 +45,7 @@ int load_library (char *camera_name) {
 			perror("load_library");
 			return (GP_ERROR);
 		}
+		glob_c.id = dlsym(lh, "camera_id");
 		glob_c.abilities = dlsym(lh, "camera_abilities");
 		glob_c.init = dlsym(lh, "camera_init");
 		glob_c.exit = dlsym(lh, "camera_exit");
@@ -68,15 +74,15 @@ int load_library (char *camera_name) {
 	return (GP_ERROR);
 }
 
-int load_camera_list (char *library_filename, CameraChoice *camera, int index) {
+int load_camera_list (char *library_filename) {
 	/* Returns the number of cameras added to the CameraChoice list */
 	/* 0 implies an error occurred */
 
-	char buf[1024];
+	char buf[1024], id[64];
 	void *lh;
 	c_abilities load_camera_abilities;
-	CameraAbilities a;
-	int x;
+	c_id load_camera_id;
+	int x, count;
 
 	sprintf(buf, "%s/%s", CAMLIBS, library_filename);
 
@@ -86,34 +92,78 @@ int load_camera_list (char *library_filename, CameraChoice *camera, int index) {
 		return 0;
 	}
 
+	/* check to see if this library has been loaded */
+	load_camera_id = dlsym(lh, "camera_id");
+	load_camera_id(id);
+#ifdef DEBUG 
+	printf("camera id: %s\n", id);
+#endif
+	for (x=0; x<glob_camera_id_count; x++) {
+		if (strcmp(glob_camera_id[x], id)==0) {
+			dlclose(lh);
+			return (GP_ERROR);
+		}
+	}
+
+	strcpy(glob_camera_id[glob_camera_id_count], id);
+	glob_camera_id_count++;
+
 	/* load in the camera_abilities function */
 	load_camera_abilities = dlsym(lh, "camera_abilities");
-	if (load_camera_abilities(&a) == GP_ERROR) {
+	if (load_camera_abilities(&glob_camera_abilities[glob_camera_count], 
+				  &count) == GP_ERROR) {
 		dlclose(lh);
 		return 0;
 	}
 
-	/* check to see if this library has been loaded */
-	for (x=0; x<index; x++) {
-		if (strcmp(camera[x].name, a.model[0])==0) {
-			dlclose(lh);
-			return (0);
-		}
-	}
-	
-	/* read in the supported camera list */
-	x=0;
-	while (a.model[x]) {
-		printf("camera model: %s\n", a.model[x]);
-		strcpy(camera[x+index].name, a.model[x]);
-		strcpy(camera[x+index].library, library_filename);
-		x++;
+#ifdef DEBUG
+	printf("camera model count: %i\n", count);
+#endif
+
+	/* Copy over the camera name */
+	for (x=glob_camera_count; x<glob_camera_count+count; x++) {
+		strcpy(glob_camera[x].name, glob_camera_abilities[x].model);
+		strcpy(glob_camera[x].library, library_filename);		
+#ifdef DEBUG
+		printf("camera model: %s\n", glob_camera[x].name);
+#endif
 	}
 
-	/* clean up */
-	gp_free_abilities(&a);
+	glob_camera_count += count;
 	
 	dlclose(lh);
 
 	return (x);
+}
+
+int load_cameras() {
+
+        DIR *d;
+        struct dirent *de;
+
+	glob_camera_id_count = 0;
+
+       /* Look for available camera libraries */
+        d = opendir(CAMLIBS);
+        do {
+           /* Read each entry */
+           de = readdir(d);
+           if (de) {
+#ifdef DEBUG
+           printf("core:\tis %s a library? ", de->d_name);
+#endif
+                /* try to open the library */
+                if (is_library(de->d_name) == GP_OK) {
+#ifdef DEBUG
+                        printf("yes\n");
+#endif
+                        load_camera_list(de->d_name);
+#ifdef DEBUG
+                   } else {
+                        printf("no\n");
+#endif
+                }
+           }
+        } while (de);
+	return (GP_OK);
 }
