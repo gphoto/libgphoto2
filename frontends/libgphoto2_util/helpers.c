226d6a1a4bf8dbc1b40ed1b7bc34382507f6243f
Scott Fritzinger <scottf@unr.edu>
2000-06-19 17:20:53 +0000

    finish renaming libgphoto2_util
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@555 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ frontends/libgphoto2_util/helpers.c
@@ -0,0 +1,19 @@
+#include <gphoto2.h>
+
+/*
+	Contains the command-lines for altering various image types
+
+
+JPEG | JPG
+	flip_h
+		jpegtran -copy all -flip horizontal %s
+
+	flip_v
+		jpegtran -copy all -flip vertical %s
+
+	rotate
+		jpegtran -copy all -rotate %i %s
+
+	scale
+		???
+*/
---
+++ frontends/libgphoto2_util/image.c
@@ -0,0 +1,24 @@
+#include <gphoto2.h>
+#include "image.h"
+
+int gp_file_image_rotate (CameraFile *old_file, CameraFile *new_file,
+			  int degrees) {
+
+	return(GP_OK);
+}
+
+int gp_file_image_scale  (CameraFile *old_file, CameraFile *new_file,
+			  int width, int height) {
+
+	return(GP_OK);
+}
+
+int gp_file_image_flip_h (CameraFile *old_file, CameraFile *new_file) {
+
+	return(GP_OK);
+}
+
+int gp_file_image_flip_v (CameraFile *old_file, CameraFile *new_file) {
+
+	return(GP_OK);
+}
