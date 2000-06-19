729da3515b860dd80c08fd113e9918eafaa1ef6c
Scott Fritzinger <scottf@unr.edu>
2000-06-19 17:19:10 +0000

    renaming libgphoto2-util to libgphoto2_util (Automake don't like dashes)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@554 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/libgphoto2-util/image.h
+++
@@ -1,6 +0,0 @@
-int gp_file_image_rotate (CameraFile *old_file, CameraFile *new_file, 
-			  int degrees);
-int gp_file_image_scale  (CameraFile *old_file, CameraFile *new_file, 
-			  int new_width, int new_height);
-int gp_file_image_flip_h (CameraFile *old_file, CameraFile *new_file);
-int gp_file_image_flip_v (CameraFile *old_file, CameraFile *new_file);
