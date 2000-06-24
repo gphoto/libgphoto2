5a17e99beb57b7541a9ead09be5ef1a2327e3e47
Scott Fritzinger <scottf@unr.edu>
2000-06-24 01:48:24 +0000

    readded serial/usb/parallel/ieee1394 flags in CameraAbilities struct. works much nicer
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@583 67ed7778-7388-44ab-90cf-0a291f65f57c

--- util.c
+++ util.c
@@ -8,49 +8,32 @@ int gp_dump_abilities (CameraAbilities *abilities) {
 	int x=0;
 	char buf[32];
 
-	printf("Abilities for camera:                   : %s\n", 
+	printf("core: Abilities for camera                  : %s\n", 
 		abilities->model);
-	switch (abilities->port_type) {
-		case GP_PORT_SERIAL:
-			strcpy(buf, "serial");
-			break;
-		case GP_PORT_PARALLEL:
-			strcpy(buf, "parallel");
-			break;
-		case GP_PORT_USB:
-			strcpy(buf, "usb");
-			break;
-		case GP_PORT_IEEE1394:
-			strcpy(buf, "ieee1394");
-			break;
-		case GP_PORT_IRDA:
-			strcpy(buf, "irda");
-			break;
-		case GP_PORT_SOCKET:
-			strcpy(buf, "socket");
-			break;
-		default:
-			strcpy(buf, "none");
-			break;
-	}
-	printf("Connection type:                      : %s\n", buf);
-
+	printf("core: Serial port support                   : %s\n", 
+		abilities->serial == 0? "no":"yes");
+	printf("core: Parallel port support                 : %s\n", 
+		abilities->parallel == 0? "no":"yes");
+	printf("core: USB support                           : %s\n", 
+		abilities->usb == 0? "no":"yes");
+	printf("core: IEEE1394 support                      : %s\n", 
+		abilities->ieee1394 == 0? "no":"yes");
 	if (abilities->speed[0] != 0) {
-	printf("Transfer speeds supported             :\n");
+	printf("core: Transfer speeds supported             :\n");
 		do {	
-	printf("                                        :%i\n", abilities->speed[x]);
+	printf("core:                                       : %i\n", abilities->speed[x]);
 			x++;
 		} while (abilities->speed[x]!=0);
 	}
-	printf("Capture from computer support         : %s\n", 
+	printf("core: Capture from computer support         : %s\n", 
 		abilities->capture == 0? "no":"yes");
-	printf("Configuration  support                : %s\n", 
+	printf("core: Configuration  support                : %s\n", 
 		abilities->config == 0? "no":"yes");
-	printf("Delete files on camera support        : %s\n", 
+	printf("core: Delete files on camera support        : %s\n", 
 		abilities->file_delete == 0? "no":"yes");
-	printf("File preview (thumnail) support       : %s\n", 
+	printf("core: File preview (thumnail) support       : %s\n", 
 		abilities->file_preview == 0? "no":"yes");
-	printf("File upload support                   : %s\n", 
+	printf("core: File upload support                   : %s\n", 
 		abilities->file_put == 0? "no":"yes");
 
 	return (GP_OK);
