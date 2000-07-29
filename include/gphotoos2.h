c1b516b611529bfc234e614c1982f6756e7934bd
bvl <simpson2@users.sourceforge.net>
2000-07-29 09:28:09 +0000

    bvl: various changes and updates to OS/2 specific files
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@793 67ed7778-7388-44ab-90cf-0a291f65f57c

--- include/gphotoos2.h
+++ include/gphotoos2.h
@@ -1,6 +1,9 @@
 #ifndef GPHOTO_OS2_INCLUDED
+#include <sys/param.h>
 #define GPHOTO_OS2_INCLUDED
+
 #define CAMLIBS getenv("CAMLIBS")
 #define RTLD_LAZY       0x001
 #define VERSION "2"
+#define usleep(t)          _sleep2(((t)+500)/ 1000)
 #endif
