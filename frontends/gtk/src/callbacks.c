79c2c1fdbf2add4336624143dfe090619edcbd17
Jae Gangemi <jgangemi@users.sourceforge.net>
2000-08-15 01:31:24 +0000

    - finished config_camera_cb     - still need to add gp_message calls and implement the build/retrieve       routines
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@880 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/gtk/src/callbacks.c
+++ frontends/gtk/src/callbacks.c
@@ -14,6 +14,7 @@
 #include "interface.h"
 #include "support.h"
 
+#include "gp_util.h"
 #include "camera_util.h"
 #include "tree_list_util.h"
 
@@ -62,22 +63,58 @@ void config_camera_cb(GtkMenuItem *menuitem, gpointer user_data) {
   GtkWidget *window, *ok, *cancel;
   int set_count = 0;
 
-  /* wrap around debug code later */
-  g_print("camera config\n");
-  
- if (!gp_gtk_camera_init)
+  gp_debug_print("camera configure");
+
+  if (!gp_gtk_camera_init)
     if (camera_set() == GP_ERROR) {
       return;
     }
   
- cw = gp_widget_new(GP_WIDGET_WINDOW, "Camera Configuration");
- 
- if (gp_camera_config_get(gp_gtk_camera, cw) == GP_ERROR) {
-   /* error message */
-   gp_widget_free(cw);
-   return;
- }
+  cw = gp_widget_new(GP_WIDGET_WINDOW, "Camera Configuration");
+  
+  if (gp_camera_config_get(gp_gtk_camera, cw) == GP_ERROR) {
+    /* error message */
+    gp_widget_free(cw);
+    return;
+  }
+  
+  if (gp_gtk_debug) 
+    gp_widget_dump(cw);
+  
+  /* create our config window */
 
+  window = gtk_dialog_new();
+  gtk_window_set_title(GTK_WINDOW(window), gp_widget_label(cw));
+  gtk_window_set_modal(GTK_WINDOW(window), TRUE);
+  gtk_window_set_default_size(GTK_WINDOW(window), 300, 400);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  
+  ok = gtk_button_new_with_label("ok");
+  gtk_box_pack_start(GTK_BOX(GTK_DIALOG(window)->action_area), ok, FALSE,
+		     TRUE, 0);
+  gtk_widget_show(ok);
+  
+  cancel = gtk_button_new_with_label("cancel");
+  gtk_box_pack_start(GTK_BOX(GTK_DIALOG(window)->action_area), cancel, FALSE,
+		     TRUE, 0);
+  gtk_widget_show(cancel);
+  
+  camera_config_build_rec(cw, GTK_DIALOG(window)->vbox, window);
+  
+  if (gp_wait_for_hide(window, ok, cancel) == 0) {
+    gp_widget_free(cw);
+    return;
+  }
+  
+  camera_config_retrieve_rec(cw, window, settings, &set_count);
+  
+  if (gp_camera_config_set(gp_gtk_camera, settings, set_count) == GP_ERROR)
+    /* error message */
+    g_print("error\n");
+  
+  /* clean up and say byes */
+  gp_widget_free(cw);
+  gtk_widget_destroy(window);
 
 } /* end config_camera_cb */
 
