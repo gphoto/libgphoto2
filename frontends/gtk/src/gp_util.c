2a57361a4c4b89467fd377e2cbfa9a1e42855644
Jae Gangemi <jgangemi@users.sourceforge.net>
2000-08-16 00:33:36 +0000

    - added gp_idle   - completed gp_interface_message_long     - create_message_win_long unimplemented   - completed gp_interface_message     - create_message_win unimplemented
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@896 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/gtk/src/gp_util.c
+++ frontends/gtk/src/gp_util.c
@@ -8,11 +8,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <gphoto2.h>
 #include <gtk/gtk.h>
 
 #include "globals.h"
 #include "gp_util.h"
+#include "support.h"
+#include "interface.h"
 
 int gp_interface_progress(Camera *camera, CameraFile *file, float percentage) {
   
@@ -34,9 +37,48 @@ int gp_interface_status(Camera *camera, char *status) {
 
 int gp_interface_message(Camera *camera, char *message) {
 
-  return 1;
+  GtkWidget *window, *label, *ok;
 
-} 
+  if (strlen(message) > 1024) {
+    gp_interface_message_long(camera, message);
+    return (GP_OK);
+  } 
+  
+  if (GTK_WIDGET_VISIBLE(gp_gtk_progress_window)) {
+    label = (GtkWidget*) lookup_widget(gp_gtk_progress_window, "message");
+    gtk_label_set_text(GTK_LABEL(label), message);
+    gp_idle();
+    return (GP_OK);
+  }
+
+  window = create_message_win(); // not yet implemented
+  label = (GtkWidget*)lookup_widget(window, "message");
+  ok = (GtkWidget*)lookup_widget(window, "close");
+  
+  gtk_label_set_text(GTK_LABEL(label), message);
+  
+  gp_wait_for_hide(window, ok, NULL);
+  gtk_widget_destroy(window);
+  
+  return (GP_OK);
+  
+} /* end gp_interface_message */
+
+int gp_interface_message_long(Camera *camera, char *message) {
+
+  GtkWidget *window, *ok, *text;
+
+  window = create_message_win_long(); // not yet implemented
+  text = (GtkWidget*)lookup_widget(window, "message");
+  ok = (GtkWidget*)lookup_widget(window, "close");
+  gtk_label_set_text(GTK_LABEL(text), message);
+
+  gp_wait_for_hide(window, ok, NULL);
+
+  gtk_widget_destroy(window);
+  return (GP_OK);
+
+} /* end gp_interface_message_long */
 
 int gp_wait_for_hide(GtkWidget *dialog, GtkWidget *ok, GtkWidget *cancel) {
 
@@ -51,3 +93,14 @@ void gp_debug_print(char *message) {
     g_print("DEBUG : %s\n", message);
   
 } /* end gp_debug_print */ 
+
+void gp_idle(void) {
+  /* let gtk do some processing */
+  
+  while (gtk_events_pending())
+    gtk_main_iteration();
+  usleep(SUSPEND_TIME);
+  while (gtk_events_pending())
+    gtk_main_iteration();
+  
+} /* end gp_idle */
--- frontends/gtk/src/gp_util.h
+++ frontends/gtk/src/gp_util.h
@@ -7,13 +7,18 @@
 
 */
 
+#define SUSPEND_TIME 100000
+
 int gp_gtk_debug;
 int gp_gtk_camera_init;
 Camera *gp_gtk_camera;
+GtkWidget *gp_gtk_progress_window;
 
 void gp_debug_print(char *message);
 int gp_wait_for_hide(GtkWidget *dialog, GtkWidget *ok, GtkWidget *cancel);
+void gp_idle(void);
 
+int gp_interface_message_long(Camera *camera, char *message);
 
 /* 
    these are defined in gphoto2.h - but we'll list them here anyway :)
