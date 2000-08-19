4016eb1d84cdc2f8e3479826973891ce0d25b0e9
Philippe Marzouk <philm@users.sourceforge.net>
2000-08-19 15:33:38 +0000

    fixed a typo in serial.c
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@922 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/serial.c
+++ camlibs/canon/serial.c
@@ -122,7 +122,7 @@ int canon_usb_probe(gpio_device *gdev, int i)
 
 	if (gpio_usb_find_device(gdev, camera_to_usb[i].idVendor,
 							 camera_to_usb[i].idProduct) == GPIO_OK) {
-		printf("found '%s' @ %d/%d\n", camera_to_usb->name,
+		printf("found '%s' @ %d/%d\n", camera_to_usb[i].name,
 			gdev->usb_device->bus->dirname, gdev->usb_device->filename);
 		return 1;
 	}
