0e8efb47098b810e0c8140cc78adf1947eecc1aa
Scott Fritzinger <scottf@unr.edu>
2000-08-28 21:54:30 +0000

    moving gtk-old to gtkam repository
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@968 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/gtk-old/src/globals.h
+++
@@ -1,7 +0,0 @@
-extern GtkWidget *gp_gtk_main_window;
-extern GtkWidget *gp_gtk_progress_window;
-extern int	  gp_gtk_debug;
-extern int	  gp_gtk_camera_init;
-extern int	  gp_gtk_old_width;
-extern char	  gp_gtk_camera_model[];
-extern Camera*	  gp_gtk_camera;
--- frontends/gtk-old/src/util.h
+++
@@ -1,8 +0,0 @@
-#include <gtk/gtk.h>
-
-int wait_for_hide (GtkWidget *dialog, GtkWidget *ok_button, GtkWidget *cancel_button);
-int exec_command (char *command, char *args);
-int file_exists (char *filename);
-GtkWidget *gtk_directory_selection_new(char *title);
-int gdk_image_new_from_data (char *image_data, int image_size, int scale,
-	GdkPixmap **pixmap, GdkBitmap **bitmap);
