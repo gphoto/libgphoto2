72906b9f0e3441354c3c1d420106b2c1de0f6764
Gus Hartmann <gphoto@gus-the-cat.org>
2000-10-14 20:51:44 +0000

    Added EXIF support to dimagev_get_picture(), with correct preprocessor magic to prevent compilation if the include file can't be found.
    Added dimagev_get_thumbnail(), which at this point does return a thumbnail
    image, but in a file format described solely as Y:Cr:Cb (4:2:2). I am looking
    into the conversion process at this time.
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@1216 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/minolta/dimagev/util.c
+++ camlibs/minolta/dimagev/util.c
@@ -25,6 +25,9 @@ int dimagev_get_picture(dimagev_t *dimagev, int file_number, CameraFile *file) {
 	int length, total_packets, i;
 	dimagev_packet *p, *r;
 	unsigned char char_buffer, command_buffer[8];
+#ifdef _gphoto_exif_
+	exifparser exifdat;
+#endif
 
 	if ( dimagev->data->host_mode != 1 ) {
 
@@ -206,6 +209,21 @@ int dimagev_get_picture(dimagev_t *dimagev, int file_number, CameraFile *file) {
 
 	sleep(2);
 
+#ifdef _gphoto_exif_
+	exifdat.header = file->data;
+	exifdat.data = file->data + 12 ;
+
+	if ( stat_exif(&exifdat) != 0 ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_picture::unable to stat EXIF tags");
+		}
+		return GP_OK;
+	}
+
+	gpe_dump_exif(&exifdat);
+
+#endif
+
 	return GP_OK;
 }
 
@@ -360,3 +378,194 @@ int dimagev_delete_picture(dimagev_t *dimagev, int file_number) {
 int dimagev_delete_all(dimagev_t *dimagev) {
 	return GP_OK;
 }
+
+int dimagev_get_thumbnail(dimagev_t *dimagev, int file_number, CameraFile *file) {
+	dimagev_packet *p, *r;
+	unsigned char char_buffer, command_buffer[8];
+
+	if ( dimagev->data->host_mode != 1 ) {
+
+		dimagev->data->host_mode = 1;
+
+		if ( dimagev_send_data(dimagev) == GP_ERROR ) {
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to set host mode");
+			}
+			return GP_ERROR;
+		}
+	}
+
+	gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::file_number is %d", file_number);
+
+	/* Maybe check if it exists? Check the file type? */
+	
+	/* First make the command packet. */
+	command_buffer[0] = 0x0d;
+	command_buffer[1] = (unsigned char)( file_number / 256 );
+	command_buffer[2] = (unsigned char)( file_number % 256 );
+	if ( ( p = dimagev_make_packet(command_buffer, 3, 0) ) == NULL ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to allocate command packet");
+		}
+		return GP_ERROR;
+	}
+
+	if ( gpio_write(dimagev->dev, p->buffer, p->length) == GPIO_ERROR ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to send set_data packet");
+		}
+		return GP_ERROR;
+	} else if ( gpio_read(dimagev->dev, &char_buffer, 1) == GPIO_ERROR ) {
+		if ( dimagev->debug != 0 ) {
+			perror("dimagev_get_thumbnail::no response from camera");
+		}
+		return GP_ERROR;
+	}
+		
+	free(p);
+
+	switch ( char_buffer ) {
+		case DIMAGEV_ACK:
+			break;
+		case DIMAGEV_NAK:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera did not acknowledge transmission\n");
+			}
+			return GP_ERROR;
+			break;
+		case DIMAGEV_CAN:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera cancels transmission\n");
+			}
+			return GP_ERROR;
+			break;
+		default:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera responded with unknown value %x\n", char_buffer);
+			}
+			return GP_ERROR;
+			break;
+	}
+
+	if ( ( p = dimagev_read_packet(dimagev) ) == NULL ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to read packet");
+		}
+		return GP_ERROR;
+	}
+
+	if ( ( r = dimagev_strip_packet(p) ) == NULL ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to strip packet");
+		}
+		return GP_ERROR;
+	}
+		
+	free(p);
+
+	/* Unlike normal images, we are guaranteed 9600 bytes *exactly* */
+
+	/* Allocate an extra byte just in case. */
+	if ( ( file->data = malloc(9601) ) == NULL ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to allocate buffer for file");
+		}
+		return GP_ERROR;
+	}
+
+	memcpy(file->data, r->buffer, r->length );
+	file->size +=  r->length - 1 ;
+
+	free(r);
+
+	while ( file->size < 9599 ) {
+
+		char_buffer=DIMAGEV_ACK;
+		if ( gpio_write(dimagev->dev, &char_buffer, 1) == GPIO_ERROR ) {
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to send ACK");
+			}
+			return GP_ERROR;
+		}
+	
+		if ( ( p = dimagev_read_packet(dimagev) ) == NULL ) {
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to read packet");
+			}
+			return GP_ERROR;
+		}
+
+		if ( ( r = dimagev_strip_packet(p) ) == NULL ) {
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to strip packet");
+			}
+			return GP_ERROR;
+		}
+		
+		free(p);
+
+		memcpy(&( file->data[ ( file->size + 1) ] ), r->buffer, r->length );
+		file->size += r->length;
+
+		free(r);
+
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::current file size is %d\n", file->size);
+		}
+	}
+
+	file->size++;
+
+	char_buffer=DIMAGEV_EOT;
+	if ( gpio_write(dimagev->dev, &char_buffer, 1) == GPIO_ERROR ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to send ACK");
+		}
+		return GP_ERROR;
+	}
+
+	if ( gpio_read(dimagev->dev, &char_buffer, 1) == GPIO_ERROR ) {
+		if ( dimagev->debug != 0 ) {
+			perror("dimagev_get_thumbnail::no response from camera");
+		}
+		return GP_ERROR;
+	}
+		
+	switch ( char_buffer ) {
+		case DIMAGEV_ACK:
+			break;
+		case DIMAGEV_NAK:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera did not acknowledge transmission\n");
+			}
+			return GP_ERROR;
+			break;
+		case DIMAGEV_CAN:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera cancels transmission\n");
+			}
+			return GP_ERROR;
+			break;
+		default:
+			if ( dimagev->debug != 0 ) {
+				gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::camera responded with unknown value %x\n", char_buffer);
+			}
+			return GP_ERROR;
+			break;
+	}
+
+	/* Now set the camera back into local mode. */
+	dimagev->data->host_mode = 0;
+
+	if ( dimagev_send_data(dimagev) == GP_ERROR ) {
+		if ( dimagev->debug != 0 ) {
+			gp_debug_printf(GP_DEBUG_HIGH, "dimagev", "dimagev_get_thumbnail::unable to set host mode");
+		}
+		return GP_ERROR;
+	}
+
+	sleep(2);
+
+	return GP_OK;
+}
+
