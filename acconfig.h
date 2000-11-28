02915e2fe415cc3c227c63b46c165682cf3fc05b
Lutz Mueller <lutz@users.sourceforge.net>
2000-11-28 21:07:42 +0000

    gnu gettext support.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@1349 67ed7778-7388-44ab-90cf-0a291f65f57c

--- acconfig.h
+++ acconfig.h
@@ -4,6 +4,13 @@
 /* Version of package.	*/
 #undef VERSION
 
+/* Languages */
+#undef ENABLE_NLS
+#undef HAVE_CATGETS
+#undef HAVE_GETTEXT
+#undef HAVE_LC_MESSAGES
+#undef HAVE_STPCPY
+
 /* Needed for driver selection	*/
 #undef CONFIG_DRIVER_BARBIE
 #undef CONFIG_DRIVER_DIRECTORY
