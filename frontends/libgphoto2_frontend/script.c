20dcc0be46f0df3986acaf7854dcd86ca46d2dfb
bvl <simpson2@users.sourceforge.net>
2000-11-30 23:30:57 +0000

    OS/2 update
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@1370 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/libgphoto2_frontend/script.c
+++ frontends/libgphoto2_frontend/script.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <gphoto2-datatypes.h>
 #include <gphoto2-frontend.h>
 
 int gpfe_script (char *script_line) {
 
-	printf("scripting: got %s\n", script_line);
+        printf("scripting: got %s\n", script_line);
 
 
-	return (GP_OK);
+        return (GP_OK);
 }
