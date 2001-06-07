10b840a6a507fede7822a7e2fcdc55c1826e2f3b
Lutz Mueller <lutz@users.sourceforge.net>
2001-06-07 20:49:03 +0000

    Correction and expansion as suggested by aliu@telostech.com
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@1874 67ed7778-7388-44ab-90cf-0a291f65f57c

--- util.c
+++ util.c
@@ -37,10 +37,12 @@ int gp_abilities_dump (CameraAbilities *abilities) {
             gp_debug_printf(GP_DEBUG_LOW, "core", "                                      : Video\n");
         if (abilities->operations & GP_OPERATION_CAPTURE_AUDIO)
             gp_debug_printf(GP_DEBUG_LOW, "core", "                                      : Audio\n");
+	if (abilities->operations & GP_OPERATION_CAPTURE_PREVIEW)
+	    gp_debug_printf(GP_DEBUG_LOW, "core", "                                      : Preview\n");
         gp_debug_printf(GP_DEBUG_LOW, "core", "Configuration support                 : %s",
                 abilities->operations & GP_OPERATION_CONFIG? "yes":"no");
 	gp_debug_printf(GP_DEBUG_LOW, "core", "Delete files on camera support        : %s", 
-		abilities->file_operations & GP_FILE_OPERATION_DELETE? "no":"yes");
+		abilities->file_operations & GP_FILE_OPERATION_DELETE? "yes":"no");
 	gp_debug_printf(GP_DEBUG_LOW, "core", "File preview (thumbnail) support      : %s", 
 		abilities->file_operations & GP_FILE_OPERATION_PREVIEW? "no":"yes");
 	gp_debug_printf(GP_DEBUG_LOW, "core", "File upload support                   : %s", 
