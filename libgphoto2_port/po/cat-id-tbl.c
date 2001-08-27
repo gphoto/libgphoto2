320f12b8ce2c5dc67bcc7c7265850895c49b882c
Lutz Mueller <lutz@users.sourceforge.net>
2001-08-27 21:42:51 +0000

    Again, line numbers have changed...
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@2103 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/po/cat-id-tbl.c
+++ libgphoto2_port/po/cat-id-tbl.c
@@ -6,13 +6,13 @@
 msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
-"POT-Creation-Date: 2001-03-31 02:57-0800\n"
+"POT-Creation-Date: 2001-08-27 15:09+0200\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=CHARSET\n"
-"Content-Transfer-Encoding: ENCODING\n"
+"Content-Transfer-Encoding: 8bit\n"
 
 #: libgphoto2_port/gp_port.c:72 libgphoto2_port/gp_port.c:99
 msgid "Unknown error"
