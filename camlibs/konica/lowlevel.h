1d2956d026009df683fcefc8e4f5b6497c0fafd6
Lutz Mueller <lutz@users.sourceforge.net>
2001-10-01 16:48:48 +0000

    Another one...
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@2343 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/konica/lowlevel.h
+++ camlibs/konica/lowlevel.h
@@ -1,62 +1,46 @@
-/****************************************************************/
-/* lowlevel.c - Implementation of the protocol defining the way */
-/*		how to send and receive bytes to and from the 	*/
-/*		camera.						*/
-/*								*/
-/* Copyright (C) 2000 The Free Software Foundation		*/
-/*								*/
-/* Author: Lutz Müller <Lutz.Mueller@stud.uni-karlsruhe.de>	*/
-/*								*/
-/* This program is free software; you can redistribute it 	*/
-/* and/or modify it under the terms of the GNU General Public	*/ 
-/* License as published by the Free Software Foundation; either */
-/* version 2 of the License, or (at your option) any later 	*/
-/* version.							*/
-/*								*/
-/* This program is distributed in the hope that it will be 	*/
-/* useful, but WITHOUT ANY WARRANTY; without even the implied 	*/
-/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 	*/
-/* PURPOSE.  See the GNU General Public License for more 	*/
-/* details.							*/
-/*								*/
-/* You should have received a copy of the GNU General Public 	*/
-/* License along with this program; if not, write to the Free 	*/
-/* Software Foundation, Inc., 59 Temple Place, Suite 330, 	*/
-/* Boston, MA 02111-1307, USA.					*/
-/****************************************************************/
+/* lowlevel.h
+ *
+ * Copyright (C) 2001 Lutz Müller <urc8@rz.uni-karlsruhe.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
+int l_init (gp_port* device);
 
-/****************************************************************/
-/* Prototypes							*/
-/****************************************************************/
-gint l_init (gp_port* device);
-
-
-/****************************************************************/
-/* Some comments on the structure of send_buffer, image_buffer,	*/
-/* and receive_buffer:						*/
-/* 								*/
-/* send_buffer:	The first two bytes are low order and high 	*/
-/* 		byte of the command, followed by at least two	*/
-/*		more bytes. The number of sent bytes has to be	*/
-/*		even.						*/
-/*								*/
-/* image_buffer: 	This buffer will contain raw data like	*/
-/*			jpeg or exif data.			*/
-/*								*/
-/* receive_buffer:	The first two bytes will be the same as	*/
-/*			in send_buffer. This will be checked 	*/
-/*			here. Then will follow low and high	*/
-/*			order byte of the return status. After 	*/
-/*			that, depending on command and return	*/
-/*			status, other bytes will follow.	*/
-/****************************************************************/
-gint l_send_receive (
-	gp_port*	device,
-	guchar*		send_buffer,
-	guint 		send_buffer_size,
-        guchar**	receive_buffer, 
-        guint*		receive_buffer_size, 
-	guint 		timeout, 
-	guchar**        image_buffer,
-	guint*          image_buffer_size);
+/*
+ * Some comments on the structure of send_buffer, image_buffer,
+ * and receive_buffer:
+ * 
+ * send_buffer:	The first two bytes are low order and high
+ * 		byte of the command, followed by at least two
+ *		more bytes. The number of sent bytes has to be
+ *		even.
+ *
+ * image_buffer: 	This buffer will contain raw data like
+ *			jpeg or exif data.
+ *
+ * receive_buffer:	The first two bytes will be the same as
+ *			in send_buffer. This will be checked
+ *			here. Then will follow low and high
+ *			order byte of the return status. After
+ *			that, depending on command and return
+ *			status, other bytes will follow.
+ */
+int l_send_receive (gp_port *device,
+	unsigned char *send_buffer, unsigned int send_buffer_size,
+	unsigned char **receive_buffer, unsigned int *receive_buffer_size,
+	unsigned int timeout,
+	unsigned char **image_buffer, unsigned int *image_buffer_size);
