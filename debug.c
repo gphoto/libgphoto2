48c69818c6746d66b2dc36381fedde6f56f6c5a8
Lutz Mueller <lutz@users.sourceforge.net>
2001-10-04 16:11:43 +0000

    2001-10-04  Lutz M�ller <urc8@rz.uni-karlsruhe.de>
            * camlibs/konica/library.c: const
            * frontents/command-line/main.c: Print out last debugging messages
            on error
            * include/gphoto2-debug.h:
            * libgphoto2/debug.c: Remember history
            * include/gphoto2-result.h:
            * libgphoto2/result.c: Revert the result_as_description thing
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@2375 67ed7778-7388-44ab-90cf-0a291f65f57c

--- debug.c
+++ debug.c
@@ -22,23 +22,82 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <gphoto2-result.h>
 
 static int glob_debug = GP_DEBUG_NONE;
+
+static GPDebugFunc debug_func      = NULL;
+static void       *debug_func_data = NULL;
+
+static unsigned int debug_history_size = 0;
+static char        *debug_history   = NULL;
+
+void
+gp_debug_set_func (GPDebugFunc func, void *data)
+{
+	debug_func      = func;
+	debug_func_data = data;
+}
 
 void
 gp_debug_printf (int level, const char *id, const char *format, ...)
 {
+	char buffer[2048];
 	va_list arg;
+	int i;
+
+	va_start (arg, format);
+	vsprintf (buffer, format, arg);
+	va_end (arg);
+
+	/* Create history if needed */
+	if (!debug_history) {
+		debug_history_size = 1024;
+		debug_history = malloc (sizeof (char) * debug_history_size);
+		memset (debug_history, 0, debug_history_size);
+	}
+
+	/* Do we have to forget parts of the debug history? */
+	while (debug_history_size - strlen (debug_history) <
+			strlen (buffer) + 1) {
+
+		/* Determine len of first entry */
+		for (i = 0; debug_history[i] != '\0'; i++)
+			if (debug_history[i] == '\n')
+				break;
+
+		/* Remove the first entry */
+		memmove (debug_history, debug_history + i + 1, 
+			strlen (debug_history) - i);
+	}
+
+	strcat (debug_history, buffer);
+	strcat (debug_history, "\n");
 
 	if (glob_debug == GP_DEBUG_NONE)
 		return;
 
 	if (glob_debug >= level) {
-		fprintf (stderr, "%s: ", id);
-		va_start (arg, format);
-		vfprintf (stderr, format, arg);
-		va_end (arg);
-		fprintf (stderr, "\n");
+		if (debug_func) {
+
+			/* Custom debug function */
+			va_start (arg, format);
+			vsprintf (buffer, format, arg);
+			va_end (arg);
+			debug_func (id, buffer, debug_func_data);
+
+		} else {
+
+			/* Own implementation */
+			fprintf (stderr, "%s: ", id);
+			va_start (arg, format);
+			vfprintf (stderr, format, arg);
+			va_end (arg);
+			fprintf (stderr, "\n");
+		}
 	}
 }
 
@@ -56,3 +115,27 @@ gp_debug_get_level (void)
 {
 	return (glob_debug);
 }
+
+const char *
+gp_debug_history_get (void)
+{
+	return (debug_history);
+}
+
+int
+gp_debug_history_set_size (unsigned int size)
+{
+	char *new_debug_history;
+
+	new_debug_history = realloc (debug_history, sizeof (char) * size);
+
+	if (!new_debug_history)
+		return (GP_ERROR_NO_MEMORY);
+
+	debug_history_size = size;
+	debug_history = new_debug_history;
+
+	return (GP_OK);
+}
+
+
