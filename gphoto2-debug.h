892aa5a9f2a9e48bd3f52d8dd113d499e3b85e10
Lutz Mueller <lutz@users.sourceforge.net>
2001-10-15 14:49:00 +0000

    2001-10-15  Lutz M�ller <urc8@rz.uni-karlsruhe.de>
            * libgphoto2/gphoto2-debug.[c,h]: Documented.
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@2524 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ gphoto2-debug.h
@@ -0,0 +1,41 @@
+/* gphoto2-debug.h
+ *
+ * Copyright (C) 2001 Lutz Müller
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GPHOTO2_DEBUG_H__
+#define __GPHOTO2_DEBUG_H__
+
+/* The debug levels (GP_DEBUG_*) are defined here */
+#include <gphoto2-port-debug.h>
+
+void gp_debug_printf    (int level, const char *id, const char *format, ...);
+
+void gp_debug_set_level (int level);
+int  gp_debug_get_level (void);
+
+/* Custom debugging function */
+typedef void (* GPDebugFunc) (const char *id, const char *msg, void *data);
+void gp_debug_set_func  (GPDebugFunc func, void *data);
+
+/* History */
+int         gp_debug_history_set_size (unsigned int size);
+int         gp_debug_history_get_size (void);
+const char *gp_debug_history_get      (void);
+
+#endif /* __GPHOTO2_DEBUG_H__ */
