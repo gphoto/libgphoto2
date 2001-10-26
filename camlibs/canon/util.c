27733e84bfbf62f5d53f13eab917665d147f7711
Hans Ulrich Niedermann <gp@n-dimensional.de>
2001-10-26 20:57:04 +0000

    added explanatory comment
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@2776 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/util.c
+++ camlibs/canon/util.c
@@ -110,8 +110,14 @@ void
 debug_message (Camera *camera, const char *msg, ...)
 {
 	va_list args;
-        char buf[4096]
-;
+        char buf[4096];
+
+	/* We could stuff these instrution into a 
+	 *      if (camera->pl->debug) { .... }
+	 * statement, but this should be obsolete
+	 * because we're using the official log message 
+	 * mechanism anyway.
+	 */
         va_start (args, msg);
         vsnprintf (buf, sizeof(buf), msg, args);
         va_end (args);
