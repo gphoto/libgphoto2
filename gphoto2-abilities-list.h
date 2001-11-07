47bdd8f4139fc00a74bd43e93a4f99616cf8f855
Lutz Mueller <lutz@users.sourceforge.net>
2001-11-07 18:29:35 +0000

    2001-11-06  Lutz M�ller <urc8@rz.uni-karlsruhe.de>
            * gphoto2-file.c: Added GP_FILE_TYPE_AUDIO for cameras that can
              attach audio data to a picture.
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@2955 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2-abilities-list.h
+++ gphoto2-abilities-list.h
@@ -66,6 +66,7 @@ typedef enum {
         GP_FILE_OPERATION_DELETE        = 1 << 1,
         GP_FILE_OPERATION_PREVIEW       = 1 << 3,
         GP_FILE_OPERATION_RAW           = 1 << 4,
+	GP_FILE_OPERATION_AUDIO		= 1 << 5
 } CameraFileOperation;
 
 /**
