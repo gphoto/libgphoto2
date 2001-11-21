80b46fae70d4cf83fceb184a08dfd028c09bfea3
Dan Fandrich <dan@coneharvesters.com>
2001-11-21 20:58:21 +0000

    Added definitions of MIN and MAX
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@3141 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/command-line/gphoto2-cmd-capture.c
+++ frontends/command-line/gphoto2-cmd-capture.c
@@ -50,6 +50,13 @@
 #  define N_(String) (String)
 #endif
 
+#ifndef MAX
+#define MAX(x, y) (((x)>(y))?(x):(y))
+#endif
+#ifndef MIN
+#define MIN(x, y) (((x)<(y))?(x):(y))
+#endif
+
 int
 gp_cmd_capture_preview (Camera *camera, CameraFile *file)
 {
