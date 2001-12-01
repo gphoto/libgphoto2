d24a4c04a41530c6164f382c6ce31e2cd36751d8
Hans Ulrich Niedermann <gp@n-dimensional.de>
2001-12-01 20:38:43 +0000

    cosmetics
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@3318 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/util.h
+++ camlibs/canon/util.h
@@ -15,15 +15,17 @@
  ****************************************************************************/
 
 void dump_hex(Camera *camera,const char *msg, const unsigned char *buf, int len);
+
 void intatpos (unsigned char *block, int pos, int integer);
 unsigned int get_int (const unsigned char *p);
+
 int is_thumbnail (const char *name);
 int is_image (const char *name);
 int is_movie (const char *name);
 int is_jpeg (const char *name);
 int is_crw (const char *name);
-int comp_dir (const void *a, const void *b);
 
+int comp_dir (const void *a, const void *b);
 
 #endif /* _CANON_UTIL_H */
 
