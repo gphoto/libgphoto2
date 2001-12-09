af2999a86738f10cc47b0326018e1880b6f493ef
Johannes Erdfelt <johannes@erdfelt.com>
2001-12-09 23:51:58 +0000

    Adding a path with a trailing / confuses the core it seems. Strip it off
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@3475 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/digita/digita.c
+++ camlibs/digita/digita.c
@@ -122,6 +122,7 @@ static int folder_list_func(CameraFilesystem *fs, const char *folder,
 	/* Walk through all of the pictures building a list of folders */
 	for (i = 0; i < camera->pl->num_pictures; i++) {
 		int found;
+		char tmppath[PATH_MAX + 1];
 		char *path;
 
 		/* Check to make sure the path matches the folder we're */
@@ -142,19 +143,22 @@ static int folder_list_func(CameraFilesystem *fs, const char *folder,
 		if (strchr(path, '/') != path + strlen(path) - 1)
 			continue;
 
+		strncpy(tmppath, path, MIN(strlen(path) - 1, PATH_MAX));
+		tmppath[PATH_MAX] = 0;
+
 		found = 0;
 		for (i1 = 0; i1 < gp_list_count(list); i1++) {
 			const char *name;
 
 			gp_list_get_name(list, i1, &name);
-			if (!strcmp(name, path)) {
+			if (!strcmp(name, tmppath)) {
 				found = 1;
 				break;
 			}
 		}
 
 		if (!found)
-			gp_list_append(list, path, NULL);
+			gp_list_append(list, tmppath, NULL);
 	}
 
 	return GP_OK;
