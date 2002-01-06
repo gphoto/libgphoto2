26e0e918a450bde2ca586748ce95ba2f16ecdeee
Dan Fandrich <dan@coneharvesters.com>
2002-01-06 22:57:51 +0000

    Added MIN and MAX macros
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@3751 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/util.h
+++ camlibs/canon/util.h
@@ -10,6 +10,13 @@
 
 #include <gphoto2-endian.h>
 
+#ifndef MAX
+# define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
+#ifndef MIN
+# define MIN(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
 /****************************************************************************
  *
  * prototypes
