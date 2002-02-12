5be763f680ecc593394709406c4e7e082cd845e3
Fredrik <fredrik@krixor.xy.org>
2002-02-12 19:12:17 +0000

    * libgphoto2/gphoto2-filesys.c: Fix month off-by-one bug in   Lutz's fix of get_exif_mtime.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@4142 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2/gphoto2-filesys.c
+++ libgphoto2/gphoto2-filesys.c
@@ -113,7 +113,7 @@ get_exif_mtime (const unsigned char *data, unsigned long size)
 
         e->data[4] = e->data[ 7] = e->data[10] = e->data[13] = e->data[16] = 0;
         ts.tm_year = atoi (e->data) - 1900;
-        ts.tm_mon  = atoi (e->data + 5);
+        ts.tm_mon  = atoi (e->data + 5) - 1;
         ts.tm_mday = atoi (e->data + 8);
         ts.tm_hour = atoi (e->data + 11);
         ts.tm_min  = atoi (e->data + 14);
