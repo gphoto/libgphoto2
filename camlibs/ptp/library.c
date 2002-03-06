1d4d59637902d118f59f99ee2384ca9455e913af
Marcus Meissner <marcus@jet.franken.de>
2002-03-06 10:55:19 +0000

    * library.c: translate error messages.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@4244 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp/library.c
+++ camlibs/ptp/library.c
@@ -137,7 +137,7 @@ report_result (GPContext *context, short result)
 
 	for (i = 0; ptp_errors[i].txt; i++)
 		if (ptp_errors[i].n == result)
-			gp_context_error (context, ptp_errors[i].txt);
+			gp_context_error (context, dgettext(PACKAGE, ptp_errors[i].txt));
 }
 
 static int
