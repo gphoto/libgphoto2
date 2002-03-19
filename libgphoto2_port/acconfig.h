4a619e506678c8a1d92d9c35deccb74d875e67a7
Marcus Meissner <marcus@jet.franken.de>
2002-03-19 09:49:49 +0000

    *  configure.in, acconfig.h: Fixed some warnings newer autoconf 	   versions generate. Use AM_PROG_LIBTOOL.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@4291 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/acconfig.h
+++ libgphoto2_port/acconfig.h
@@ -10,17 +10,3 @@
 #undef HAVE_GETTEXT
 #undef HAVE_LC_MESSAGES
 #undef HAVE_STPCPY
-
-/* Debugging */
-#undef DISABLE_DEBUGGING
-
-#undef HAVE_RTS_IOCTL
-
-/* Serial port locking */
-#undef HAVE_BAUDBOY
-#undef HAVE_TTYLOCK
-#undef HAVE_LOCKDEV
-
-#undef HAVE_GNU_REGEX
-
-#undef HAVE_LTDL
