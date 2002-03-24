65e14b1e02f8d9ad73fb0aa540561057e3c9257a
Lutz Mueller <lutz@users.sourceforge.net>
2002-03-24 21:51:47 +0000

    Fix indentation.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@4343 67ed7778-7388-44ab-90cf-0a291f65f57c

--- frontends/command-line/gphoto2-cmd-config.c
+++ frontends/command-line/gphoto2-cmd-config.c
@@ -498,8 +498,7 @@ show_range (CmdConfig *cmd_config, CameraWidget *range)
 
 	CHECK (gp_widget_get_range (range, &min, &max, &increment));
 
-	/* Pretty ugly check for number of digits follows. */
-	if (get_digits(increment) == 0)
+	if (!get_digits (increment))
 		return (show_range_int (cmd_config, range));
 	else
 		return (show_range_float (cmd_config, range));
