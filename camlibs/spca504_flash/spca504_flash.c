722bd0050c84ce5c954698c5fde48da3eb2b4aa2
Arnaud Launay <asl@launay.org>
2002-11-22 13:05:54 +0000

    Delete unused macros
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@5742 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/spca504_flash/spca504_flash.c
+++ camlibs/spca504_flash/spca504_flash.c
@@ -37,25 +37,6 @@
 
 #define GP_MODULE "spca504_flash"
 
-#ifdef ENABLE_NLS
-#  include <libintl.h>
-#  undef _
-#  define _(String) dgettext (PACKAGE, String)
-#  ifdef gettext_noop
-#    define N_(String) gettext_noop (String)
-#  else
-#    define N_(String) (String)
-#  endif
-#else
-#  define _(String) (String)
-#  define N_(String) (String)
-#endif
-
-#define REPORT_ERROR(context,function,text,data1,data2)\
-{                                                           \
-  gp_context_error (context, _(function ":" text),data1,data2); \
-  return (GP_ERROR);                                        \
-}
 static int yuv2rgb (int y, int u, int v, int *_r, int *_g, int *_b);
 
 static int
