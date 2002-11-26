fe017b1c7d6632ec0703bd92f2a8b70992b1f51c
Colin Marquardt <colin@marquardt-home.de>
2002-11-26 16:41:31 +0000

    (camera_about): Make sure author names are 7bit ASCII only.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@5813 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/spca504_flash/library.c
+++ camlibs/spca504_flash/library.c
@@ -176,9 +176,10 @@ camera_manual (Camera *camera, CameraText *manual, GPContext *context)
 static int
 camera_about (Camera *camera, CameraText *about, GPContext *context)
 {
+        /* Translators: please translate "Krauss" to "Krauﬂ" if possible. */
 	strcpy (about->text,
 		_
-		("spca504_flash camlib\n Authors: Till Adam\n <till@adam-lilienthal.de>\nbased on work by Matthias Krauﬂ\nand Mark A. Zimmerman <mark@foresthaven.com>"));
+		("spca504_flash camlib\n Authors: Till Adam\n <till@adam-lilienthal.de>\nbased on work by Matthias Krauss\nand Mark A. Zimmerman <mark@foresthaven.com>"));
 	return GP_OK;
 }
 static int
