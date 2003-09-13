4a5b2496763447b6ed275d6faf20b1ca62f87a62
Hans Ulrich Niedermann <gp@n-dimensional.de>
2003-09-13 14:17:27 +0000

    replace // by /* */
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@6748 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2-filesys.c
+++ gphoto2-filesys.c
@@ -3,17 +3,17 @@
  * Copyright © 2000 Scott Fritzinger
  *
  * Contributions:
- * 	Lutz Müller <lutz@users.sf.net> (2001)
+ *      Lutz Müller <lutz@users.sf.net> (2001)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details. 
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the
@@ -1964,7 +1964,7 @@ gp_get_free_memory (GPContext *context, unsigned *free)
 
 #else
 
-	// No way to know the free memory on this system
+	/* No way to know the free memory on this system */
 	*free = UINT_MAX;
 	return (GP_OK);
 
