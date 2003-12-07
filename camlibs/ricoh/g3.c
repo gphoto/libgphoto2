f8f6e583d97323deadbce5e7ce3715123a939b64
Marcus Meissner <marcus@jet.franken.de>
2003-12-07 22:28:24 +0000

    * added G4 as reported.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@6942 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ricoh/g3.c
+++ camlibs/ricoh/g3.c
@@ -231,6 +231,11 @@ camera_abilities (CameraAbilitiesList *list)
 	a.usb_product	= 0x2202;
 	gp_abilities_list_append(list, a);
 
+	strcpy(a.model, "Ricoh:Caplio G4");
+	a.usb_vendor	= 0x5ca;
+	a.usb_product	= 0x2208;
+	gp_abilities_list_append(list, a);
+
 	return (GP_OK);
 }
 
