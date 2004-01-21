80554ae212b5c8f34db0f833053c13ba46bbeb16
Marcus Meissner <marcus@jet.franken.de>
2004-01-21 10:32:09 +0000

    * added che-ez! splash as reported.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@7085 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/stv0674/stv0674.c
+++ camlibs/stv0674/stv0674.c
@@ -52,12 +52,8 @@ struct camera_to_usb {
 	  unsigned short idProduct;
 } camera_to_usb[] = {
 	/* http://www.digitaldreamco.com/shop/xtra.htm, SVGA */
-/* I've taken the liberty of replacing the original model string with one that conforms
-*  to the gphoto2 standard of Manufacturer:Model
-*  Michael Rensing <mrensing@users.sourceforge.net>
-*	{ "l'espion xtra by DigitalDream",   0x05DA, 0x1020 }
-*/
-      { "DigitalDream:l'espion xtra",   0x05DA, 0x1020 }
+      { "DigitalDream:l'espion xtra",   0x05DA, 0x1020 },
+      { "Che-ez!:Splash",   		0x0553, 0x1002 }
 
 };
 
