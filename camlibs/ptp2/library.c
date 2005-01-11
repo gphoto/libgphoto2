881e50b6dc1115c0f146df5048bb3a482aaffb47
Marcus Meissner <marcus@jet.franken.de>
2005-01-11 07:20:33 +0000

    added Sony DSC-T3
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@7502 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -299,6 +299,7 @@ static struct {
 	{"Sony:DSC-S75 (PTP mode)",   0x054c, 0x004e},
 	{"Sony:DSC-S85 (PTP mode)",   0x054c, 0x004e},
 	{"Sony:DSC-T1 (PTP mode)",    0x054c, 0x004e},
+	{"Sony:DSC-T3 (PTP mode)",    0x054c, 0x004e},
 	{"Sony:DSC-U20 (PTP mode)",   0x054c, 0x004e},
 	{"Sony:DSC-V1 (PTP mode)",    0x054c, 0x004e},
 	{"Sony:MVC-CD300 (PTP mode)", 0x054c, 0x004e},
