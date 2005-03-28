67d1fd66e4315aad747010e3e99ef66f1157224e
Stephen H. Westin <swestin@earthlink.net>
2005-03-28 18:35:50 +0000

    Fixed initialization that I broke in making the 20D work.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@7688 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/usb.c
+++ camlibs/canon/usb.c
@@ -363,26 +363,7 @@ canon_usb_init (Camera *camera, GPContext *context)
 		}
 	}
 	else {
-		do {
-			i = gp_port_check_int_fast ( camera->port, buffer, 0x10 );
-			if ( i > 0 )
-				read_bytes += i;
-		} while ( read_bytes < 0x10 && i >= 0 );
-		if ( read_bytes < 0x10 ) {
-			GP_DEBUG ( "canon_usb_camera_init() interrupt read returned only %d bytes, status=%d", read_bytes, i );
-			if ( i < 0 )
-				return GP_ERROR_OS_FAILURE;
-			else
-				return GP_ERROR_CORRUPTED_DATA;
-		}
-		else if ( i < 0 ) {
-			GP_DEBUG ( "canon_usb_camera_init() interrupt read failed, status=%d", i );
-			return GP_ERROR_CORRUPTED_DATA;
-		}
-		else if ( i > 0x10 )
-			GP_DEBUG ( "canon_usb_camera_init() interrupt read %d bytes, expected 16", read_bytes );
-		if ( camera->pl->md->model != CANON_CLASS_4
-			&& camera->pl->md->model != CANON_CLASS_6 ) {
+		if ( camera->pl->md->model != CANON_CLASS_4 ) {
 			i = canon_usb_lock_keys(camera,context);
 			if( i < 0 ) {
 				gp_context_error (context, _("lock keys failed."));
