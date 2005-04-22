fc9c8ce0cbf3ddf06860d403422f895bc1d7d741
Marcus Meissner <marcus@jet.franken.de>
2005-04-22 07:54:22 +0000

    * library.c: Added IXUS 50.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@7888 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -423,6 +423,8 @@ static struct {
 	{"Canon:EOS 1D Mark II (PTP mode)",     0x04a9, 0x30ea, 0},
  	{"Canon:EOS 20D (PTP mode)",            0x04a9, 0x30ec, PTPBUG_DCIM_WRONG_PARENT},
 	{"Canon:EOS 350D (PTP mode)",           0x04a9, 0x30ef, PTPBUG_DCIM_WRONG_PARENT},
+	/* Nick Richards <nick@nedrichards.com> */
+	{"Canon:Digital IXUS 50 (PTP mode)",    0x04a9, 0x310e, 0},
 
 	/* Konica-Minolta PTP cameras */
 	{"Konica-Minolta:DiMAGE A2 (PTP mode)",        0x132b, 0x0001, 0},
