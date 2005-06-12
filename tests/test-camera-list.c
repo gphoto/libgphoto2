4e74efa525e93bd2c5dd370da92218f453650ddd
Hans Ulrich Niedermann <gp@n-dimensional.de>
2005-06-12 15:00:09 +0000

    improve camera list test with error checks
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8081 67ed7778-7388-44ab-90cf-0a291f65f57c

--- tests/test-camera-list.c
+++ tests/test-camera-list.c
@@ -21,9 +21,63 @@
 #include "config.h"
 
 #include <stdio.h>
-#include <gphoto2-camera.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+#include "gphoto2-port-log.h"
+#include "gphoto2-camera.h"
+#include "gphoto2-port-portability.h"
 
 #define CHECK(f) {int res = f; if (res < 0) {printf ("ERROR: %s\n", gp_result_as_string (res)); return (1);}}
+
+
+#define TEST_DEBUG
+/* #undef TEST_DEBUG */
+
+#ifdef TEST_DEBUG
+
+/* time zero for debug log time stamps */
+struct timeval glob_tv_zero = { 0, 0 };
+
+static void
+#ifdef __GNUC__
+		__attribute__((__format__(printf,3,0)))
+#endif
+debug_func (GPLogLevel level, const char *domain, const char *format,
+	    va_list args, void *data)
+{
+	struct timeval tv;
+	long sec, usec;
+
+	gettimeofday (&tv, NULL);
+	sec = tv.tv_sec  - glob_tv_zero.tv_sec;
+	usec = tv.tv_usec - glob_tv_zero.tv_usec;
+	if (usec < 0) {sec--; usec += 1000000L;}
+	fprintf (stderr, "%li.%06li %s(%i): ", sec, usec, domain, level);
+	vfprintf (stderr, format, args);
+	fputc ('\n', stderr);
+}
+
+#endif /* TEST_DEBUG */
+
+static void
+print_headline (void)
+{
+	printf("No.|%-20s|%-20s|%s\n",
+	       "camlib",
+	       "driver name",
+	       "camera model");
+}
+
+static void
+print_hline (void)
+{
+	printf("---+%-20s+%-20s+%s\n",
+	       "--------------------",
+	       "--------------------",
+	       "-------------------------------------------");
+}
 
 int
 main (int argc, char *argv [])
@@ -31,6 +85,13 @@ main (int argc, char *argv [])
 	CameraAbilitiesList *al;
 	int i;
 	int count;
+
+#ifdef TEST_DEBUG
+	gettimeofday (&glob_tv_zero, NULL);
+	CHECK (gp_log_add_func (GP_LOG_ALL, debug_func, NULL));
+
+	gp_log (GP_LOG_DEBUG, "main", "test-camera-list start");
+#endif /* TEST_DEBUG */
 
 	CHECK (gp_abilities_list_new (&al));
 	CHECK (gp_abilities_list_load (al, NULL));
@@ -44,19 +105,33 @@ main (int argc, char *argv [])
 		return(1);
 	}
 
-	printf("No.|%-20s|%s\n",
-	       "driver (camlib)",
-	       "camera model");
-	printf("---+%-20s+%s\n",
-	       "--------------------",
-	       "-------------------------------------------");
+	print_hline();
+	print_headline();
+	print_hline();
 	for (i = 0; i < count; i++) {
 		CameraAbilities abilities;
+		char *p,*x;
 		CHECK (gp_abilities_list_get_abilities (al, i, &abilities));
-		printf("%3d|%-20s|%s\n", i+1, 
+		/* remove path part from camlib name */
+		for (x=p=abilities.library; *p != '\0'; p++) {
+			if ((*p == gp_system_dir_delim) 
+			    && (*(p+1) != '\0')) {
+				x = p+1;
+			}
+		}
+		if (((i%25)== 0) && ((count-i) > 5)) {
+			print_hline();
+			print_headline();
+			print_hline();
+		}
+		printf("%3d|%-20s|%-20s|%s\n", i+1, 
+		       x,
 		       abilities.id,
 		       abilities.model);
 	}
+	print_hline();
+	print_headline();
+	print_hline();
 	
 	CHECK (gp_abilities_list_free (al));
 	return (0);
