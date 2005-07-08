e373f3a22aac5188cde5ff8b1dfe4b2705d73297
Marcus Meissner <marcus@jet.franken.de>
2005-07-08 13:30:52 +0000

    added focus metering mode and flash mode
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8154 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -2444,6 +2444,98 @@ _put_FocusMetering(Camera* camera, CameraWidget *widget, PTPPropertyValue *propv
 }
 
 
+static struct deviceproptableu16 focus_mode[] = {
+	{ N_("Manual Focus"),	0x0001, 0 },
+	{ N_("AF-S"),		0x8010, PTP_VENDOR_NIKON},
+	{ N_("AF-C"),		0x8011, PTP_VENDOR_NIKON},
+};
+
+static int
+_get_FocusMode(Camera* camera, CameraWidget **widget, struct submenu *menu, PTPDevicePropDesc *dpd) {
+	int i;
+	char buf[20];
+
+	gp_widget_new (GP_WIDGET_RADIO, _(menu->label), widget);
+	gp_widget_set_name (*widget, menu->name);
+	if (!(dpd->FormFlag & PTP_DPFF_Enumeration))
+		return (GP_ERROR);
+	if (dpd->DataType != PTP_DTC_UINT16)
+		return (GP_ERROR);
+	sprintf(buf, "unknown %04x", dpd->CurrentValue.u16);
+	gp_widget_set_value (*widget, buf);
+	for (i=0;i<sizeof (focus_mode)/sizeof (focus_mode[0]);i++) {
+		gp_widget_add_choice (*widget, _(focus_mode[i].label));
+		if (focus_mode[i].value == dpd->CurrentValue.u16)
+			gp_widget_set_value (*widget, _(focus_mode[i].label));
+	}
+	return (GP_OK);
+}
+
+static int
+_put_FocusMode(Camera* camera, CameraWidget *widget, PTPPropertyValue *propval) {
+	char *value;
+	int i, ret;
+
+	ret = gp_widget_get_value (widget, &value);
+	if (ret != GP_OK)
+		return ret;
+	for (i=0;i<sizeof (focus_mode)/sizeof (focus_mode[0]);i++) {
+		if (!strcmp (value, _(focus_mode[i].label))) {
+			propval->u16 = focus_mode[i].value;
+			return (GP_OK);
+		}
+	}
+	return (GP_ERROR);
+}
+
+
+static struct deviceproptableu16 flash_mode[] = {
+	{ N_("Default"),			0x8010, PTP_VENDOR_NIKON},
+	{ N_("Red-eye Reduction"),		0x0004, 0 },
+	{ N_("Red-eye Reduction + Slow Sync"),	0x8013, PTP_VENDOR_NIKON},
+	{ N_("Slow Sync"),			0x8011, PTP_VENDOR_NIKON},
+	{ N_("Rear Curtain Sync + Slow Sync"),	0x8012, PTP_VENDOR_NIKON},
+};
+
+static int
+_get_FlashMode(Camera* camera, CameraWidget **widget, struct submenu *menu, PTPDevicePropDesc *dpd) {
+	int i;
+	char buf[20];
+
+	gp_widget_new (GP_WIDGET_RADIO, _(menu->label), widget);
+	gp_widget_set_name (*widget, menu->name);
+	if (!(dpd->FormFlag & PTP_DPFF_Enumeration))
+		return (GP_ERROR);
+	if (dpd->DataType != PTP_DTC_UINT16)
+		return (GP_ERROR);
+	sprintf(buf, "unknown %04x", dpd->CurrentValue.u16);
+	gp_widget_set_value (*widget, buf);
+	for (i=0;i<sizeof (flash_mode)/sizeof (flash_mode[0]);i++) {
+		gp_widget_add_choice (*widget, _(flash_mode[i].label));
+		if (flash_mode[i].value == dpd->CurrentValue.u16)
+			gp_widget_set_value (*widget, _(flash_mode[i].label));
+	}
+	return (GP_OK);
+}
+
+static int
+_put_FlashMode(Camera* camera, CameraWidget *widget, PTPPropertyValue *propval) {
+	char *value;
+	int i, ret;
+
+	ret = gp_widget_get_value (widget, &value);
+	if (ret != GP_OK)
+		return ret;
+	for (i=0;i<sizeof (flash_mode)/sizeof (flash_mode[0]);i++) {
+		if (!strcmp (value, _(flash_mode[i].label))) {
+			propval->u16 = flash_mode[i].value;
+			return (GP_OK);
+		}
+	}
+	return (GP_ERROR);
+}
+
+
 static struct deviceproptableu8 canon_macromode[] = {
 	{ N_("Off"),		0x01, 0 },
 	{ N_("Macro"),		0x03, 0 },
@@ -2817,6 +2909,8 @@ static struct submenu image_settings_menu[] = {
         { N_("Exposure Program"), "expprogram", PTP_DPC_ExposureProgramMode, 0, PTP_DTC_UINT16, _get_ExposureProgram, _put_ExposureProgram},
         { N_("Still Capture Mode"), "capturemode", PTP_DPC_StillCaptureMode, 0, PTP_DTC_UINT16, _get_CaptureMode, _put_CaptureMode},
         { N_("Focus Metering Mode"), "focusmetermode", PTP_DPC_FocusMeteringMode, 0, PTP_DTC_UINT16, _get_FocusMetering, _put_FocusMetering},
+        { N_("Focus Mode"), "focusmode", PTP_DPC_FocusMode, 0, PTP_DTC_UINT16, _get_FocusMode, _put_FocusMode},
+        { N_("Flash Mode"), "flash", PTP_DPC_FlashMode, 0, PTP_DTC_UINT16, _get_FlashMode, _put_FlashMode},
         { N_("ISO Speed"), "iso", PTP_DPC_CANON_ISOSpeed, PTP_VENDOR_CANON, PTP_DTC_UINT16, _get_Canon_ISO, _put_Canon_ISO},
         { N_("Macro Mode"), "macromode", PTP_DPC_CANON_MacroMode, PTP_VENDOR_CANON, PTP_DTC_UINT8, _get_Canon_Macro, _put_Canon_Macro},
 	{ N_("WhiteBalance"), "whitebalance", PTP_DPC_CANON_WhiteBalance, PTP_VENDOR_CANON, PTP_DTC_UINT8, _get_Canon_WhiteBalance, _put_Canon_WhiteBalance},
