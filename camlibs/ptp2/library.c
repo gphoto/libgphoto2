ce5d26aeecddeb5d295a91adc8f16b813c598f93
Marcus Meissner <marcus@jet.franken.de>
2005-08-29 06:43:02 +0000

    added lumix fz5 alias
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8227 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -402,6 +402,9 @@ static struct {
 	/* Thomas Luzat <thomas.luzat@gmx.net> */
 	{"Panasonic:DMC-FZ20 (alternate id)", 0x04da, 0x2372, 0},
 
+	/* http://sourceforge.net/tracker/index.php?func=detail&aid=1275100&group_id=8874&atid=358874 */
+	{"Panasonic:Lumix FZ5",           0x04da, 0x2372, 0},
+
 	{"Panasonic:DMC-FZ20",            0x04da, 0x2374, 0},
 
 	/* http://callendor.zongo.be/wiki/OlympusMju500 */
