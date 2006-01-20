6bc69cf7808071a8c805bf3f419ba062bec0f531
Marcus Meissner <marcus@jet.franken.de>
2006-01-20 22:41:11 +0000

    serial devices are called slightly different with FreeBSD >= 6.0
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8507 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/serial/unix.c
+++ libgphoto2_port/serial/unix.c
@@ -108,7 +108,11 @@
 
 /* FreeBSD */
 #ifdef __FreeBSD__
+#if __FreeBSD_version < 600000
 #define GP_PORT_SERIAL_PREFIX   "/dev/cuaa%x"
+#else
+#define GP_PORT_SERIAL_PREFIX   "/dev/cuad%x"
+#endif
 #define GP_PORT_SERIAL_RANGE_LOW        0
 #define GP_PORT_SERIAL_RANGE_HIGH       (0xf)
 #endif
