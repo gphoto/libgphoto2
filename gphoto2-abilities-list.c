a3972363a1a107ee2928a9dc664e8f1f91058971
Marcus Meissner <marcus@jet.franken.de>
2006-02-19 16:41:09 +0000

    ptpip autodetection (still disabled for now)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/libgphoto2@8575 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2-abilities-list.c
+++ gphoto2-abilities-list.c
@@ -440,6 +440,18 @@ gp_abilities_list_detect (CameraAbilitiesList *list,
 			gp_list_append (l, "Mass Storage Camera", info.path);
 			break;
 		}
+#ifdef ENABLE_PTPIP /* only enable if you enable the ptp2 part */
+		case GP_PORT_PTPIP: {
+			char	*s;
+
+			s = strchr (info.path, ':');
+			if (!s) break;
+			s++;
+			if (!strlen(s)) break;
+			gp_list_append (l, "PTP/IP Camera", info.path);
+			break;
+		}
+#endif
 		default:
 			/*
 			 * We currently cannot detect any cameras on this
