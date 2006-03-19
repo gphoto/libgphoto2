79b84aba77b84aa82815c9ee17902d14f7a74d6f
Marcus Meissner <marcus@jet.franken.de>
2006-03-19 20:08:57 +0000

    use static fsfuncs.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8629 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/agfa-cl20/agfa_cl20.c
+++ camlibs/agfa-cl20/agfa_cl20.c
@@ -651,6 +651,12 @@ file_list_func (CameraFilesystem *fs, const char *folder, CameraList *list,
 	return (GP_OK);
 }
 
+static CameraFilesystemFuncs fsfuncs = {
+	.file_list_func = file_list_func,
+	.get_info_func = get_info_func,
+	.get_file_func = get_file_func,
+};
+
 int
 camera_init (Camera *camera, GPContext *context)
 {
@@ -666,14 +672,7 @@ camera_init (Camera *camera, GPContext *context)
         camera->functions->about                = camera_about;
 
 	/* Now, tell the filesystem where to get lists, files and info */
-	gp_filesystem_set_list_funcs (camera->fs, file_list_func,
-				      NULL, camera);
-	gp_filesystem_set_info_funcs (camera->fs, get_info_func, NULL,
-				      camera);
-	gp_filesystem_set_file_funcs (camera->fs, get_file_func,
-				      NULL, camera);
-	gp_filesystem_set_folder_funcs (camera->fs, NULL,
-					NULL, NULL, NULL, camera);
+	gp_filesystem_set_funcs (camera->fs, &fsfuncs, camera);
 
 	/*
 	 * The port is already provided with camera->port (and
