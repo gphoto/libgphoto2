14a0cf1cb0c0b493a808534148f93757e1c8241f
Marcus Meissner <marcus@jet.franken.de>
2006-03-31 18:50:03 +0000

    converted to use fsfuncs.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8681 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/clicksmart310/library.c
+++ camlibs/clicksmart310/library.c
@@ -267,6 +267,12 @@ camera_exit (Camera *camera, GPContext *context)
 	return GP_OK;
 }
 
+static CameraFilesystemFuncs fsfuncs = {
+	.file_list_func = file_list_func,
+	.get_file_func = get_file_func,
+	.delete_all_func = delete_all_func
+};
+
 int
 camera_init(Camera *camera, GPContext *context)
 {
@@ -302,12 +308,7 @@ camera_init(Camera *camera, GPContext *context)
 	if (ret < 0) return ret; 
 
         /* Tell the CameraFilesystem where to get lists from */
-	gp_filesystem_set_list_funcs (camera->fs, file_list_func, 
-					    NULL, camera);
-        gp_filesystem_set_info_funcs (camera->fs, NULL, NULL, camera);
-	gp_filesystem_set_file_funcs (camera->fs, get_file_func, NULL, camera);
-	gp_filesystem_set_folder_funcs (camera->fs, NULL, delete_all_func, 
-					    NULL, NULL, camera);
+	gp_filesystem_set_funcs (camera->fs, &fsfuncs, camera);
 	camera->pl = malloc (sizeof (CameraPrivateLibrary));
 	if (!camera->pl) return GP_ERROR_NO_MEMORY;
 	camera->pl->catalog = NULL;
