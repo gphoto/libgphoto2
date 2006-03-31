f1d4b0f3d21de1bde0bfd93c003be946b74a6517
Marcus Meissner <marcus@jet.franken.de>
2006-03-31 19:22:54 +0000

    converted to fsfuncs.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8685 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/gsmart300/library.c
+++ camlibs/gsmart300/library.c
@@ -170,6 +170,14 @@ camera_about (Camera *camera, CameraText *about, GPContext *context)
 	return (GP_OK);
 }
 
+static CameraFilesystemFuncs fsfuncs = {
+	.file_list_func = file_list_func,
+	.get_file_func = get_file_func,
+	.get_info_func = get_info_func,
+	.del_file_func = delete_file_func,
+	.delete_all_func = delete_all_func
+};
+
 int
 camera_init (Camera *camera, GPContext *context)
 {
@@ -219,19 +227,8 @@ camera_init (Camera *camera, GPContext *context)
 
 		return (ret);
 	}
-
 	/* Set up the CameraFilesystem */
-	CHECK (gp_filesystem_set_list_funcs (camera->fs, file_list_func, 
-				             NULL, camera));
-	CHECK (gp_filesystem_set_file_funcs (camera->fs, get_file_func,
-					     delete_file_func, camera));
-	CHECK (gp_filesystem_set_info_funcs (camera->fs, get_info_func, 
-				             NULL, camera));
-	CHECK (gp_filesystem_set_folder_funcs (camera->fs, NULL, 
-			                       delete_all_func, NULL, NULL, 
-					       camera));
-
-	return (GP_OK);
+	return gp_filesystem_set_funcs (camera->fs, &fsfuncs, camera);
 }
 
 
