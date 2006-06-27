726de5ff18c5ec199880c0b727721975b328e6ea
Patrick van Staveren <trick@vanstaveren.us>
2006-06-27 16:46:14 +0000

    updated MimeTypes, reordered to match gphoto2-file.h
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@8992 67ed7778-7388-44ab-90cf-0a291f65f57c

--- bindings/csharp/CameraFile.cs
+++ bindings/csharp/CameraFile.cs
@@ -15,18 +15,24 @@ namespace LibGPhoto2
 	
 	public class MimeTypes
 	{
-		[MarshalAs(UnmanagedType.LPTStr)] public static string AVI = "video/x-msvideo";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string BMP = "image/bmp";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string CRW = "image/x-canon-raw";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string JPEG = "image/jpeg";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string PGM = "image/x-portable-graymap";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string PNG = "image/png";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string PPM = "image-x-portable-pixmap";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string QUICKTIME = "video/quicktime";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string RAW = "image/x-raw";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string TIFF = "image/tiff";
-		[MarshalAs(UnmanagedType.LPTStr)] public static string UNKNOWN = "application/octet-stream";
 		[MarshalAs(UnmanagedType.LPTStr)] public static string WAV = "audio/wav";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string RAW = "image/x-raw";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string PNG = "image/png";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string PGM = "image/x-portable-graymap";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string PPM = "image-x-portable-pixmap";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string JPEG = "image/jpeg";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string TIFF = "image/tiff";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string BMP = "image/bmp";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string QUICKTIME = "video/quicktime";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string AVI = "video/x-msvideo";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string CRW = "image/x-canon-raw";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string UNKNOWN = "application/octet-stream";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string EXIF = "application/x-exif";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string MP3 = "audio/mpeg";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string OGG = "application/ogg";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string WMA = "audio/x-wma";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string ASF = "audio/x-asf";
+		[MarshalAs(UnmanagedType.LPTStr)] public static string MPEG = "audio/x-wma";
 	}
 
 	public class CameraFile : Object 
