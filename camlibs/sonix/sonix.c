0242b473b043e9ee3314bf71738797b924b43be5
Theodore Kilgore <kilgota@auburn.edu>
2006-08-16 20:16:04 +0000

    Revisions to support added camera
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@9077 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sonix/sonix.c
+++ camlibs/sonix/sonix.c
@@ -30,13 +30,12 @@
 #include <gphoto2-port.h>
 
 #include "sonix.h"
-
 #define GP_MODULE "sonix" 
 
 
 int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 {
-	int i;
+	int i, command_done=1;
 	unsigned char c[6];
 	unsigned char status;
 	unsigned char reading[4];
@@ -45,8 +44,14 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 	c[0] = 0x0c;
 	GP_DEBUG("Running sonix_init\n");
 
+
 	SONIX_READ(port, &status);
 
+	if (status == 0x02) goto skip_a_step; 
+	/* If status is 2 we can skip several steps. Otherwise, status
+	 * needs to be reset to 0 and massaged until it is 2. 
+	*/
+	
 	if (status) {
 		i = 0;
 		
@@ -57,7 +62,7 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 		}
 	}
 		
-	SONIX_COMMAND ( port, c);
+	command_done = SONIX_COMMAND ( port, c);
 
 
 	while (status !=2)
@@ -65,7 +70,12 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 		
 		SONIX_READ(port, &status);			
 
-	/* We are supposed to get c[0]=0x8c here */
+	skip_a_step:
+	
+	/*
+	 * We are supposed to get c[0]=0x8c here; in general the reply
+	 * string ought to start with the command byte c[0], plus 0x80
+	 */
 	SONIX_READ4 (port, reading);	
 	SONIX_READ(port, &status);
 
@@ -75,18 +85,26 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 	SONIX_COMMAND ( port, c );
 
 	/*
-	 * This should always cause a read of the same 4 bytes, namely
-	 * 96 0a 76 07 so it could be that this is a read of the amount 
-	 * of memory in the camera. but if so, in what units?
-	 * Webcam-osx driver says the 0x0a gives the sensor type, which 
-	 * here is OV7630.
+	 * For the Vivicam 3350b this always gives
+	 * 96 0a 76 07. This could be a firmware version. The  
+	 * webcam-osx driver says the 0x0a gives the sensor type, which 
+	 * is OV7630. For the Sakar Digital Keychain 11199 the string is 
+	 * 96 03 31 08, instead. Since the two cameras have different 
+	 * abilities, we must distinguish. 
 	 */
 	 
-	SONIX_READ4 (port, reading);		
+	SONIX_READ4 (port, reading);
+
+	GP_DEBUG("Above is the 4-byte ID string of your camera.");
+	GP_DEBUG("Please report if it is anything other than");
+	GP_DEBUG("96 0a 76 07   or   96 03 31 08	Thanks!\n" );		
+	priv->fwversion = reading[1];
 	SONIX_READ(port, &status);
 
-	/* This sequence gives the photos in the camera and sets a flag 
-	 * in reading[3] if the camera is full. 
+	/*
+	 * This sequence gives the photos in the camera and sets a flag 
+	 * in reading[3] if the camera is full. Alas, it does not count 
+	 * clip frames separately on the Sakar Digital Keychain camera. 
 	 */
 
 	memset (c,0,6);
@@ -98,14 +116,15 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 	SONIX_READ(port, &status);
 
 	SONIX_READ4 (port, reading);		
+	if (reading[0] != 0x98)
+		return GP_ERROR_CAMERA_ERROR;	
 	
 	GP_DEBUG ("number of photos is %d\n", reading[1]+ 256*reading[2]);
 
 	/* 
 	 * If reading[3] = 0x01, it means the camera is full.  
-	 * Important in webcam mode, or in case that capture is to be run. 
-	 * Not much use here if we can't make the capture to work :(
-	 * but if capture can be made to work it is needed here, too. 
+	 * Not much use here if capture doesn't work, but for some models 
+	 * capture does work. So we store this information.  
 	 */
 
 	if (!(reading[3])) priv->full = 0;
@@ -114,63 +133,35 @@ int sonix_init (GPPort *port, CameraPrivateLibrary *priv)
 
 	priv->num_pics = 	(reading[1] + 256*reading[2]);
 
+	memset(c,0,sizeof(c));
 
-
+	for (i = 0; i < priv->num_pics; i++) {
+	GP_DEBUG("getting size_code for picture %i\n", i+1);
+	/* Gets the resolution setting, also flags a multi-frame entry. */
+	c[0] = 0x19;
+	c[1] = (i+1)%256;
+	c[2] = (i+1)/256;
+	
+	SONIX_COMMAND ( port, c );
+	SONIX_READ(port, &status);
+	SONIX_READ4 (port, reading);		
+	if (reading[0] != 0x99)
+		return GP_ERROR_CAMERA_ERROR;	
+	SONIX_READ(port, &status);
+	/* For the code meanings, see get_file_func() in library.c */
+	priv->size_code[i] = (reading[1]&0x0f);
+	}
 	GP_DEBUG("Leaving sonix_init\n");
 
         return GP_OK;
 }
 
-
-int sonix_get_picture_width (GPPort *port, int n)
-{
-	char status;
-	unsigned char c[6];
-	unsigned char reading[4];
-	
-	memset(c,0,sizeof(c));
-
-	GP_DEBUG("running sonix_get_picture_width for picture %i\n", n+1);
-	/* Gets the resolution setting, just prior to download of a photo. */
-	c[0] = 0x19;
-	c[1] = n+1;
-	SONIX_COMMAND ( port, c );
-	SONIX_READ(port, &status);
-	SONIX_READ4 (port, reading);		
-	SONIX_READ(port, &status);
-	
-	/* Only the likely stillcam modes are listed here; others may exist. */ 
-
-	switch (reading[1]&0x0f) {
-	case 0:
-		return 352; /* CIF */
-		break;
-	case 1:
-		return 176; /* QCIF */
-		break;
-
-	case 2: 
-		return 640; /* VGA */
-		break;
-	case 3:
-		return 320; /* SIF */
-		break;
-	default:
-		GP_DEBUG("Width of photo %i is unknown\n", n+1);
-		return (GP_ERROR_NOT_SUPPORTED);
-	}
-}
-
-
 int 
-sonix_read_picture_data (Camera *camera, GPPort *port, 
-					char *data, int n) 
+sonix_read_data_size (Camera *camera, GPPort *port, char *data, int n) 
 {
 	char status;
 	unsigned char c[6];
 	unsigned char reading[4];
-	unsigned int size;
-	unsigned int remainder = 0;
 
 	GP_DEBUG("running sonix_read_picture_data for picture %i\n", n+1);
 	memset (c, 0, 6);
@@ -179,16 +170,10 @@ sonix_read_picture_data (Camera *camera, GPPort *port,
 	c[2] = (n+1)/256;
 	SONIX_COMMAND ( port, c );
 	SONIX_READ(port, &status);
-
 	SONIX_READ4 (port, reading);	
-
-	if (reading[1]%0x40) remainder = 0x40;
-
-	size = 0x40*(reading[1]/0x40) + reading[2]*0x100 
-			+ reading[3] * 0x10000+ remainder;
-	gp_port_read(port, data, size);
-		
-	return size;
+	if (reading[0] != 0x9a)
+		return GP_ERROR_CAMERA_ERROR;	
+	return (reading[1] + reading[2]*0x100 + reading[3] *0x10000);
 }
 
 int 
@@ -196,23 +181,78 @@ sonix_delete_all_pics      (GPPort *port)
 {
 	char status;
 	unsigned char c[6];
+	unsigned char reading[4];
 
 	memset (c,0,6);
-	c[0]=0x05;
 
+	c[0]=0x05; 	
 	SONIX_READ(port, &status);
 	SONIX_COMMAND ( port, c );
 	SONIX_READ(port, &status);
+	SONIX_READ4 (port, reading);	
+	if (reading[0] != 0x85)
+		return GP_ERROR_CAMERA_ERROR;	
+
+	return GP_OK;
+}
+
+int 
+sonix_delete_last      (GPPort *port) 
+{
+	char status;
+	unsigned char c[6];
+	unsigned char reading[4];
+	
+	memset (c,0,6);
+
+	c[0]= 0x05; c[1] = 0x01;
+	SONIX_READ(port, &status);
+	SONIX_COMMAND ( port, c );
+	SONIX_READ(port, &status);
+	SONIX_READ4 (port, reading);	
+	if (reading[0] != 0x85)
+		return GP_ERROR_CAMERA_ERROR;	
+
+	return GP_OK;
+}
+
+int 
+sonix_capture_image      (GPPort *port) 
+{
+	char status;
+	unsigned char c[6];
+	unsigned char reading[4];
+	memset (c,0,6);
+	c[0]=0x0e; 	
+	SONIX_READ(port, &status);
+	SONIX_COMMAND ( port, c );
+	SONIX_READ(port, &status);
+	SONIX_READ4 (port, reading);	
+	if (reading[0] != 0x8e)
+		return GP_ERROR_CAMERA_ERROR;	
+	return GP_OK;
+}
+
+int 
+sonix_exit      (GPPort *port) 
+{
+	char status;
+	unsigned char c[6];
+
+	memset (c,0,6);
+	c[0]=0x14;
+	SONIX_READ(port, &status);
+	SONIX_COMMAND ( port, c );
+	/* No READ4 after exit! */
 	return GP_OK;
 }
 
 /* 
  * The decoding algorithm originates with Bertrik Sikken. This version adapted
- * from the one written for the webcam-osx project, for the sn9c2028.
+ * from the webcam-osx (macam) project. See README for details.
  */
 
 /* Four defines for bitstream operations, for the decode function */
-
 
 #define PEEK_BITS(num,to) {\
     	if (bitBufCount<num){\
@@ -241,42 +281,42 @@ sonix_delete_all_pics      (GPPort *port)
   
 #define PARSE_PIXEL(val) {\
     PEEK_BITS(10,bits);\
-    if ((bits&0x00000200)==0) { \
+    if ((bits&0x200)==0) { \
 	EAT_BITS(1); \
     } \
-    else if ((bits&0x00000380)==0x00000280) { \
+    else if ((bits&0x380)==0x280) { \
 	EAT_BITS(3); \
 	val+=3; \
 	if (val>255) val=255; \
     }\
-    else if ((bits&0x00000380)==0x00000300) { \
+    else if ((bits&0x380)==0x300) { \
 	EAT_BITS(3); \
 	val-=3; \
 	if (val<0) val=0; \
     }\
-    else if ((bits&0x000003c0)==0x00000200) { \
+    else if ((bits&0x3c0)==0x200) { \
 	EAT_BITS(4); \
 	val+=8; \
 	if (val>255) val=255;\
     }\
-    else if ((bits&0x000003c0)==0x00000240) { \
+    else if ((bits&0x3c0)==0x240) { \
 	EAT_BITS(4); \
 	val-=8; \
 	if (val<0) val=0;\
     }\
-    else if ((bits&0x000003c0)==0x000003c0) { \
+    else if ((bits&0x3c0)==0x3c0) { \
 	EAT_BITS(4); \
 	val-=20; \
 	if (val<0) val=0;\
     }\
-    else if ((bits&0x000003e0)==0x00000380) { \
+    else if ((bits&0x3e0)==0x380) { \
 	EAT_BITS(5); \
 	val+=20; \
 	if (val>255) val=255;\
     }\
     else { \
 	EAT_BITS(10); \
-	val=8*(bits&0x0000001f)+0; \
+	val=8*(bits&0x1f)+0; \
     }\
 }
 
@@ -290,7 +330,8 @@ sonix_delete_all_pics      (GPPort *port)
 /* Now the decode function itself */
 
 
-int sonix_decode(unsigned char * dst, unsigned char * src, int width, int height) 
+int 
+sonix_decode(unsigned char * dst, unsigned char * src, int width, int height) 
 {
 	long dst_index = 0;
 	int starting_row = 0;
@@ -305,11 +346,11 @@ int sonix_decode(unsigned char * dst, unsigned char * src, int width, int height
 	for (y = starting_row; y < height; y++) {
 		PEEK_BITS(8, bits);
 		EAT_BITS(8);
-		c1val = bits & 0x000000ff;
+		c1val = bits & 0xff;
 
 		PEEK_BITS(8, bits);
 		EAT_BITS(8);
-		c2val = bits & 0x000000ff;
+		c2val = bits & 0xff;
 
 		PUT_PIXEL_PAIR;
 
@@ -322,6 +363,10 @@ int sonix_decode(unsigned char * dst, unsigned char * src, int width, int height
 	return GP_OK;
 }
 
+/* Three often-used generic commands */
+
+
+/* This reads a one-byte "status" response */
 int 
 SONIX_READ (GPPort *port, char *data) 
 {
@@ -329,6 +374,7 @@ SONIX_READ (GPPort *port, char *data)
 	return GP_OK;
 }
 
+/* This reads a 4-byte response to a command */
 int 
 SONIX_READ4 (GPPort *port, char *data) 
 {
@@ -336,6 +382,7 @@ SONIX_READ4 (GPPort *port, char *data)
 	return GP_OK;
 }
 
+/* A command to the camera is a 6-byte string, and this sends it. */
 int 
 SONIX_COMMAND (GPPort *port, char *command) 
 {
