d19796ddb7775893ab8ca9da2164dd520c1ac2d5
Marcus Meissner <marcus@jet.franken.de>
2006-08-29 16:06:18 +0000

    added Ricoh R4
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@9128 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ricoh/g3.c
+++ camlibs/ricoh/g3.c
@@ -301,6 +301,11 @@ camera_abilities (CameraAbilitiesList *list)
 	a.usb_product	= 0x2216;
 	gp_abilities_list_append(list, a);
 
+	strcpy(a.model, "Ricoh:Caplio R4");
+	a.usb_vendor	= 0x5ca;
+	a.usb_product	= 0x2217;
+	gp_abilities_list_append(list, a);
+
 	return (GP_OK);
 }
 
