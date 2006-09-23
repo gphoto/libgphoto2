15de4806a68ed05ce04cf23a874c99435df3f18f
Theodore Kilgore <kilgota@auburn.edu>
2006-09-23 23:36:06 +0000

    usleep() added to mars_routine()
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@9207 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/mars/mars.c
+++ camlibs/mars/mars.c
@@ -36,15 +36,6 @@
 
 #define INIT 		0xb5
 #define GET_DATA 	0x0f
-#define RESET		0xba
-
-#ifndef MAX
-# define MAX(a, b) ((a) > (b) ? (a) : (b))
-#endif
-#ifndef MIN
-# define MIN(a, b) ((a) < (b) ? (a) : (b))
-#endif
-
 
 int 
 mars_init (Camera *camera, GPPort *port, Info *info) 
@@ -64,17 +55,15 @@ mars_init (Camera *camera, GPPort *port, Info *info)
 		gp_port_write(port, "\x19", 1);
 		gp_port_read(port, c, 16);
 	}
-	while ( (status != 0xb5)){
-	status = mars_routine (info, port, INIT, 0);    
-	GP_DEBUG("status = 0x%x\n", status);
+	else {
+		status = mars_routine (info, port, INIT, 0);    
+		GP_DEBUG("status = 0x%x\n", status);
 	}
-
-
 
 	/* Not a typo. This _will_ download the config data ;) */
 	mars_read_picture_data (camera, info, port, info, 0x2000, 0); 
 
-	/* Removing extraneous line or lines of config data. See "protocol.txt" */
+	/* Removing extraneous line(s) of data. See "protocol.txt" */
 	 
 	if ((info[0] == 0xff)&& (info[1] == 0)&&(info[2]==0xff))
 		memmove(info, info + 16, 0x1ff0); /* Saving config */
@@ -90,35 +79,16 @@ mars_get_num_pics      (Info *info)
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < 0x3fe; i++) if ( !(0xff - info[8*i]) )  
-	{
-	GP_DEBUG ( "i is %i\n", i);
-	memcpy(info+0x1ff0, "i", 1);
-	return i;
-	}
+	for (i = 0; i < 0x3fe; i++) 
+		if ( !(0xff - info[8*i]) )  {
+			GP_DEBUG ( "i is %i\n", i);
+			memcpy(info+0x1ff0, "i", 1);
+			return i;
+		}
 	memcpy(info+0x1ff0, "0", 1);
 	return 0;
 }
 
-int
-mars_chk_compression (Info *info, int n)
-{
-	return	((info[8*n] >>4) & 2);
-}
-
-int
-mars_get_picture_width (Info *info, int n)
-{
-    	switch (info[8*n] & 0x0f) {  
-	case 0x00: return 176;
-	case 0x02: return 352;
-	case 0x06: return 320;
-	case 0x08: return 640;
-	default:
-		GP_DEBUG ("Your pictures have unknown width.\n");
-		return (GP_ERROR_NOT_SUPPORTED);
-	}
-}
 
 int
 mars_get_pic_data_size (Info *info, int n)
@@ -127,16 +97,18 @@ mars_get_pic_data_size (Info *info, int n)
 }
 
 int 
-set_usb_in_endpoint	     (Camera *camera, int inep) 
+set_usb_in_endpoint	(Camera *camera, int inep) 
 {
 	GPPortSettings settings;
 	gp_port_get_settings ( camera ->port, &settings);
 	settings.usb.inep = inep;
+	GP_DEBUG("inep reset to %02X\n", inep);
 	return gp_port_set_settings ( camera ->port, settings);
 }	
 
+
 int 
-mars_read_data         (Camera *camera, GPPort *port, char *data, int size) 
+mars_read_data         (GPPort *port, char *data, int size) 
 { 
 	int MAX_BULK = 0x2000;
 	int len = 0;
@@ -153,6 +125,7 @@ mars_read_data         (Camera *camera, GPPort *port, char *data, int size)
 int 
 mars_read_picture_data (Camera *camera, Info *info, GPPort *port, 
 					char *data, int size, int n) 
+
 {
 	unsigned char c[16];
 	
@@ -161,18 +134,17 @@ mars_read_picture_data (Camera *camera, Info *info, GPPort *port,
 	mars_routine (info, port, GET_DATA, n);
 	/*Data transfer begins*/
 	set_usb_in_endpoint (camera, 0x82); 
-	mars_read_data (camera, port, data, size);
-	set_usb_in_endpoint (camera, 0x83); 	
+	mars_read_data (port, data, size); 
+	set_usb_in_endpoint (camera, 0x83); 
     	return GP_OK;
 } 
 
 int
 mars_reset (GPPort *port)
 {
-	gp_port_write(port, "\x19\x54", 2);	
+	gp_port_write(port, "\x19\x54", 2);
     	return GP_OK;
 }
-
 
 void precalc_table(code_table_t *table)
 {
@@ -184,45 +156,38 @@ void precalc_table(code_table_t *table)
 		val = 0;
 		len = 0;
 		if ((i & 0x80) == 0) {
-			/* code 0 */
-			val = 0;
-			len = 1;
-		}
-		else if ((i & 0xE0) == 0xC0) {
-			/* code 110 */
-			val = -3;
-			len = 3;
-		}
-		else if ((i & 0xE0) == 0xA0) {
-			/* code 101 */
-			val = +3;
-			len = 3;
-		}
-		else if ((i & 0xF0) == 0x80) {
-			/* code 1000 */
-			val = +7;
-			len = 4;
-		}
-		else if ((i & 0xF0) == 0x90) {
-			/* code 1001 */
-			val = -7;
-			len = 4;
-		}
-		else if ((i & 0xF0) == 0xF0) {
-			/* code 1111 */
-			val = -15;
-			len = 4;
-		}
-		else if ((i & 0xF8) == 0xE0) {
-			/* code 11100 */
-			val = +15;
-			len = 5;
-		}
-		else if ((i & 0xF8) == 0xE8) {
-			/* code 11101xxxxx */
-			is_abs = 1;
-			val = 0;	/* value is calculated later */
-			len = 5;
+    			/* code 0 */
+    			val = 0;
+    			len = 1;
+		}else if ((i & 0xE0) == 0xC0) {
+    			/* code 110 */
+    			val = -3;
+    			len = 3;
+		}else if ((i & 0xE0) == 0xA0) {
+    			/* code 101 */
+    			val = +3;
+    			len = 3;
+		}else if ((i & 0xF0) == 0x80) {
+    			/* code 1000 */
+    			val = +7;
+    			len = 4;
+		}else if ((i & 0xF0) == 0x90) {
+    			/* code 1001 */
+    			val = -7;
+    			len = 4;
+		}else if ((i & 0xF0) == 0xF0) {
+    			/* code 1111 */
+    			val = -15;
+    			len = 4;
+		}else if ((i & 0xF8) == 0xE0) {
+    			/* code 11100 */
+    			val = +15;
+    			len = 5;
+		}else if ((i & 0xF8) == 0xE8) {
+    			/* code 11101xxxxx */
+    			is_abs = 1;
+    			val = 0;	/* value is calculated later */
+    			len = 5;
 		}
 		table[i].is_abs = is_abs;
 		table[i].val = val;
@@ -230,28 +195,23 @@ void precalc_table(code_table_t *table)
 	}
 }
 
-#define CLAMP(x)	((x)<20?20:((x)>255)?255:(x))
-/* #define	MAX(a,b) 	((a)>(b)?(a):(b)) */
-/* #define 	MIN(a,b)	((a)<(b)?(a):(b)) */
-#define	MAX3(a,b,c) 	MAX(MAX(a,b),c)
-#define	MIN3(a,b,c) 	MIN(MIN(a,b),c)
-#define	MAX4(a,b,c,d) 	MAX(MAX(a,b),MAX(c,d))
-#define	MIN4(a,b,c,d) 	MIN(MIN(a,b),MIN(c,d))
+#define CLAMP(x)	((x)<0?0:((x)>255)?255:(x))
+#define GET_CODE 	addr = inp + (bitpos >> 3); \
+	    code = (addr[0] << (bitpos & 7)) | (addr[1] >> (8 - (bitpos & 7)))
 
 
-int mars_decompress (unsigned char *inp, 
-				unsigned char *outp, int width, int height)
+int mars_decompress (unsigned char *inp, unsigned char *outp, int width,
+   int height)
 {
 	int row, col;
 	unsigned char code;
 	int val;
 	code_table_t table[256];
 	unsigned char *addr;
-	int bitpos, i;
-	unsigned char A,B,C,D, predictor;
+	int bitpos;
+	unsigned char lp,tp,tlp,trp;
 	/* First calculate the Huffman table */
 	precalc_table(table);
-	
 
 	bitpos = 0;
 
@@ -259,121 +219,67 @@ int mars_decompress (unsigned char *inp,
 	for (row = 0; row < height; row++) {
 		col = 0;
 
-		/* first two pixels in first two rows are stored as raw 8-bit */
-		if (row < 2) {
-			addr = inp + (bitpos >> 3);
-			code = (addr[0] << (bitpos & 7)) | (addr[1] >> (8 - (bitpos & 7)));
-			bitpos += 8;
-			*outp++ = code;
+	/* first two pixels in first two rows are stored as raw 8-bit */
+	if (row < 2) {
+    		GET_CODE;
+    		bitpos += 8;
+    		*outp++ = code;
 
-			addr = inp + (bitpos >> 3);
-			code = (addr[0] << (bitpos & 7)) | (addr[1] >> (8 - (bitpos & 7)));
-			bitpos += 8;
-			*outp++ = code;
+    		GET_CODE;
+    		bitpos += 8;
+    		*outp++ = code;
 
-			col += 2;
-		}
+    		col += 2;
+	}
 
-		while (col < width) {
-			/* get bitcode */
-			addr = inp + (bitpos >> 3);
-			code = (addr[0] << (bitpos & 7)) | (addr[1] >> (8 - (bitpos & 7)));
+	while (col < width) {
+    		/* get bitcode */
+    		GET_CODE;
+    		/* update bit position */
+    		bitpos += table[code].len;
 
-			/* update bit position */
-			bitpos += table[code].len;
+    		/* calculate pixel value */
+    		if (table[code].is_abs) {
+    			/* get 5 more bits and use them as absolute value */
+    			GET_CODE;
+    			val = (code & 0xF8);
+    			bitpos += 5;
 
-			/* calculate pixel value */
-			if (table[code].is_abs) {
-				/* get 5 more bits and use them as absolute value */
-				addr = inp + (bitpos >> 3);
-				code = (addr[0] << (bitpos & 7)) | (addr[1] >> (8 - (bitpos & 7)));
-				val = (code & 0xF8);
-				bitpos += 5;
-			}
-			else {
-				/* value is relative to top or left pixel */
-				val = table[code].val;
-				if (row < 2) {
-					/* top row: relative to left pixel */
-					val += outp[-2];
-				}
-				else if (col < 2) {
-					/* left column: relative to top pixel */
-					/* initial estimate */
-					predictor = (outp[-2*width] +
-						outp[-2*width + 2])/2; 
-					/* corrections */
-					if (row > 3) {
-						B=outp[-2*width ];
-						C=outp[-4*width ];
-						if (MAX((predictor -2*B+C),
-						    (-predictor +2*B-C))
-						    < 1)
-							predictor = 
-							(7*predictor + C)/8;
-					}
-						
-					val += predictor;
-				}
-
-				else if (col > width - 3) {
-					/* left column: relative to top pixel */
-					val += (outp[-2*width] +
-					    outp[-2*width - 2])/2;
-				}
-				
-				else {
-					/* main area: average of left pixel and top pixel */
-					A=outp[-2];
-					C=outp[-2*width-2];
-					B=outp[-2*width ];
-					D=outp[-2*width+2];
-					/* initial estimate for predictor */
-					predictor = (2*A + B + D+1)/4;
-					/* possible vertical edge? */
-					if (4*MAX((C -2*B + D), 
-					    (-C + 2*B - D)) < 1)
-						predictor = 
-						    (3*predictor + D+1)/4;
-					/* or horizontal edge? */
-					if (row >= 4) {
-						D=outp[-4*width - 2];
-					if (4*MAX((A -2*C + D), 
-					    (-A + 2*C - D)) < 1)
-						predictor = 
-						    (3*predictor + A+1)/4;
-					}
-					/* decrease changes for close values */
-					if (MAX3(A,B,D) - MIN3(A,B,D) 
-					< 0x20) {
-						if(val > 0)
-							val -= 1;
-						if (val < 0) 
-							val += 1;
-					}
-					/* increase change for widespread values*/
-					if (MAX4(A,B,C,D) - MIN4(A,B,C,D) 
-					> 0x30) {
-						if(val > 0)
-							val += 1;
-						if (val < 0) 
-							val -= 1;
-					}
-					val += predictor;	
-				}
-			}
-
-			/* store pixel */
-			*outp++ = CLAMP(val);
-			col++;
+    			}else {
+    				/* value is relative to top or left pixel */
+    				val = table[code].val;
+    				lp =  outp[-2];
+    				if (row > 1) {
+        				tlp = outp[-2*width-2];
+        				tp  = outp[-2*width];
+        				trp = outp[-2*width+2];
+    				}
+    				if (row < 2) {
+        				/* top row: relative to left pixel */
+        				val += lp;
+    				}else if (col < 2) {
+        				/* left column: relative to top pixel */
+        				/* initial estimate */
+        				val += (2*tp + 2*trp +1)/4; 
+    				}else if (col > width - 3) {
+        				/* left column: relative to top pixel */
+        				val += (2*tp + 2*tlp +1)/4;
+					/* main area: average of left and top pixel */
+    				}else {
+        				/* initial estimate for predictor */
+					val += (2*lp + tp + trp + 1)/4;
+    				}
+    			}
+    			/* store pixel */
+    			*outp++ = CLAMP(val);
+    			col++;
 		}
 	}
-	/* spread the clamped values upward */
-//	for (i=0; i< sizeof(outp); i++) 
-//		outp[i] = outp[i]*255/236;
-
-	return 0;
+	return GP_OK;
 }
+
+
+
 
 
 int 
@@ -387,7 +293,7 @@ M_READ (GPPort *port, char *data, int size)
 int 
 M_COMMAND (GPPort *port, char *command, int size, char *response) 
 {
-	gp_port_write(port, command, size);	
+	gp_port_write(port, command, size);
     	M_READ(port, response, 16); 	
 	return GP_OK;
 }
@@ -409,16 +315,11 @@ mars_routine (Info *info, GPPort *port, char param, int n)
 	do_something[0]= 0x19; 
 	do_something[1]=param;
 
+
 	memset(c,0,sizeof(c));
 
 	/*Routine used in initialization, photo download, and reset. */
     	M_READ(port, c, 16); 	
-	if ( (c[0] == 0x02 ) ) {	/* Clears camera if jammed */
-		gp_port_write(port, "\x19", 1);
-		gp_port_read(port, c, 16);
-		M_READ(port, c, 16);
-	}
-
 	M_COMMAND(port, start, 2, c);
 	M_COMMAND(port, do_something, 2, c);
 	M_COMMAND(port, address1, 2, c);
@@ -434,9 +335,10 @@ mars_routine (Info *info, GPPort *port, char param, int n)
 	M_COMMAND(port, address4, 2, c);
 	M_COMMAND(port, address5, 2, c);
 	M_COMMAND(port, address6, 2, c);
-	
 	gp_port_write(port, "\x19", 1);
-	gp_port_read(port, c , 16);	
+	gp_port_read(port, c , 16);
+	/* Next thing is to switch the inep. Some cameras need a pause here */
+	usleep (MARS_SLEEP); 
 
 	return(c[0]);
 }
