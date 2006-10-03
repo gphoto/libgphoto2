9a5a1357a4d6e4318ec39ec968af259e13c210c6
Hans Ulrich Niedermann <gp@n-dimensional.de>
2006-10-03 13:51:25 +0000

    compile C++ pedantic test case, add more header files
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@9252 67ed7778-7388-44ab-90cf-0a291f65f57c

--- tests/test-pedantic-compilation.c
+++ tests/test-pedantic-compilation.c
@@ -1,5 +1,12 @@
 #include <gphoto2/gphoto2.h>
 #include <gphoto2/gphoto2-camera.h>
+#include <gphoto2/gphoto2-list.h>
+#include <gphoto2/gphoto2-version.h>
+#include <gphoto2/gphoto2-setting.h>
+#include <gphoto2/gphoto2-file.h>
+#include <gphoto2/gphoto2-filesys.h>
+#include <gphoto2/gphoto2-context.h>
+#include <gphoto2/gphoto2-abilities-list.h>
 
 int main()
 {
