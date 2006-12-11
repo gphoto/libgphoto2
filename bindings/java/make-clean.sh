ec4326f9ef1268b9b39cef61407f414eccaf5f46
Hans Ulrich Niedermann <gp@n-dimensional.de>
2006-12-11 16:22:49 +0000

    Move java bindings to new place
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@9573 67ed7778-7388-44ab-90cf-0a291f65f57c

--- bindings/java/make-clean.sh
+++
@@ -1,4 +0,0 @@
-echo 'rm -f *.class *.c *.so swig/org/gphoto2/*.{java,class} swig-org-gphoto2.jar'
-rm -f *.class *.c *.o *.so swig/org/gphoto2/*.{java,class} swig-org-gphoto2.jar
-echo rmdir -p swig/org/gphoto2
-rmdir -p swig/org/gphoto2
