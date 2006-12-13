9eebbe57c7714083e91fa148809d0a23b34c1590
Marcus Meissner <marcus@jet.franken.de>
2006-12-13 08:47:18 +0000

    compile fix for older compilers.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_3/libgphoto2@9587 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/usb.c
+++ camlibs/canon/usb.c
@@ -1308,6 +1308,7 @@ canon_usb_dialogue_full (Camera *camera, canonCommandIndex canon_funct, unsigned
         int j, canon_subfunc = 0;
         char subcmd = 0, *subfunct_descr = "";
         int additional_read_bytes = 0;
+	char *msg;
 
         /* clear this to indicate that no data is there if we abort */
         if (return_length)
@@ -1546,7 +1547,7 @@ canon_usb_dialogue_full (Camera *camera, canonCommandIndex canon_funct, unsigned
                 }
         }
 
-                char *msg = canon_usb_decode_status ( le32atoh ( buffer+0x50 ) );
+                msg = canon_usb_decode_status ( le32atoh ( buffer+0x50 ) );
                 if ( msg != NULL ) {
                         GP_DEBUG ( "canon_usb_dialogue_full: camera status \"%s\""
 				   " in response to command 0x%x 0x%x 0x%x (%s)",
