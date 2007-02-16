822571e879839d26d3ebc35aa38bd09c868e99bc
Marcus Meissner <marcus@jet.franken.de>
2007-02-16 08:24:27 +0000

    added olympus fe-200
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10007 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sierra/sierra.c
+++ camlibs/sierra/sierra.c
@@ -201,6 +201,8 @@ static struct {
 				SIERRA_WRAP_USB_OLYMPUS, &oly3040_cam_desc},
 	{"Olympus", "D-535Z", 	SIERRA_MODEL_CAM_DESC,	0x07b4, 0x109,
 				SIERRA_WRAP_USB_OLYMPUS, &oly3040_cam_desc},
+	{"Olympus", "fe-200", 	SIERRA_MODEL_CAM_DESC,	0x07b4, 0x109,
+				SIERRA_WRAP_USB_OLYMPUS, &oly3040_cam_desc},
 
 	{"Panasonic", "Coolshot NV-DCF5E", SIERRA_MODEL_DEFAULT, 0, 0, 0, NULL },
 
