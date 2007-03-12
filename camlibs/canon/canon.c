31c35d45224b28f9009753a78001bc295e16d7b3
Marcus Meissner <marcus@jet.franken.de>
2007-03-12 21:08:22 +0000

    disable canon ixus 2... served better by ptp2 driver.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10046 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/canon.c
+++ camlibs/canon/canon.c
@@ -177,8 +177,11 @@ const struct canonCamModelData models[] = {
         {"Canon:Digital IXUS v3 (normal mode)", CANON_CLASS_4,  0x04A9, 0x3070, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         /* 0x3071 is S230/IXUS v3 in PTP mode */
         /* Following cameras share the ID for PTP and Canon modes */
+#if 0 /* served better by PTP2 driver ... I own one of them too. -Marcus */
         {"Canon:PowerShot SD100 (normal mode)", CANON_CLASS_5,  0x04A9, 0x3072, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         {"Canon:Digital IXUS II (normal mode)", CANON_CLASS_5,  0x04A9, 0x3072, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
+#endif
+
         {"Canon:PowerShot A70",         CANON_CLASS_1,  0x04A9, 0x3073, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         {"Canon:PowerShot A60",         CANON_CLASS_1,  0x04A9, 0x3074, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         {"Canon:Digital IXUS 400",      CANON_CLASS_1,  0x04A9, 0x3075, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
