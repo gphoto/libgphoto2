9b6944e0ac57dc64a6fc81a94fb8de4764dbcac1
Marcus Meissner <marcus@jet.franken.de>
2007-05-28 22:37:27 +0000

    correct product id.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10336 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -490,7 +490,7 @@ static struct {
 	/* From IRC reporter. */
 	{"Nikon:Coolpix L4 (PTP mode)",   0x04b0, 0x0305, 0},
 	/* From IRC reporter. */
-	{"Nikon:Coolpix L10 (PTP mode)",   0x04b0, 0x0310, 0},
+	{"Nikon:Coolpix L10 (PTP mode)",  0x04b0, 0x030b, 0},
 	/* Nikon D100 has a PTP mode: westin 2002.10.16 */
 	{"Nikon:DSC D100 (PTP mode)",     0x04b0, 0x0402, 0},
 	/* D2H SLR in PTP mode from Steve Drew <stevedrew@gmail.com> */
