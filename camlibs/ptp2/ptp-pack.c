d35177e641a092b9ed2266840ddf25631d33109a
Marcus Meissner <marcus@jet.franken.de>
2007-08-28 21:09:44 +0000

    pack NULL strings , same as ""
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10615 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/ptp-pack.c
+++ camlibs/ptp2/ptp-pack.c
@@ -190,8 +190,12 @@ ptp_get_packed_stringcopy(PTPParams *params, char *string, uint32_t *packed_size
 	uint8_t packed[PTP_MAXSTRLEN*2+3], len;
 	size_t plen;
 	unsigned char *retcopy = NULL;
-
-	ptp_pack_string(params, string, (unsigned char*) packed, 0, &len);
+  
+	if (string == NULL)
+	  ptp_pack_string(params, "", (unsigned char*) packed, 0, &len);
+	else
+	  ptp_pack_string(params, string, (unsigned char*) packed, 0, &len);
+  
 	/* returned length is in characters, then one byte for string length */
 	plen = len*2 + 1;
 	
