b98d42a1c93857d3e6d5743002f4a8cac4572929
Theodore Kilgore <kilgota@auburn.edu>
2007-10-21 22:03:44 +0000

    new camera in camlibs/mars
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10811 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/mars/library.c
+++ camlibs/mars/library.c
@@ -75,7 +75,8 @@ static const struct {
 	{"ION digital camera", GP_DRIVER_STATUS_TESTING, 0x093a, 0x010f}, 
 	{"Argus QuickClix", GP_DRIVER_STATUS_DEPRECATED, 0x093a, 0x010f},
 	{"Pixart Gemini Keychain Camera", GP_DRIVER_STATUS_TESTING, 0x093a, 0x010e},
-	{"Sakar Digital no. 56379 Spyshot", GP_DRIVER_STATUS_TESTING, 0x093a, 0x010e},	
+	{"Sakar Digital no. 56379 Spyshot", GP_DRIVER_STATUS_TESTING, 0x093a, 0x010e},
+	{"Vivitar Mini Digital Camera", GP_DRIVER_STATUS_TESTING, 0x093a, 0x010e},	
 	{NULL,0,0,0}
 };
 
