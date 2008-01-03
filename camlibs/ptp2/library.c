354c3dd95c4eea5d26237a26f90c1e854325c16d
Marcus Meissner <marcus@jet.franken.de>
2008-01-03 13:39:02 +0000

    added kodak easyshare m883
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10924 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -336,6 +336,8 @@ static struct {
 	{"Kodak:V705",   0x040a, 0x05ab, 0},
 	/* https://launchpad.net/bugs/67532 */
 	{"Kodak:V610",   0x040a, 0x05ac, 0},
+	/* http://sourceforge.net/tracker/index.php?func=detail&aid=1861193&group_id=8874&atid=358874 */
+	{"Kodak:M883",   0x040a, 0x05ad, 0},
 	/* from Thomas <tomtechguy@gmail.com> */
 	{"Kodak:C743",   0x040a, 0x05ae, 0},
 	/* via IRC */
