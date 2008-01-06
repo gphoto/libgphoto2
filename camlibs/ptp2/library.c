3fa81a4b9f480c854026ecb8a1777e470f053748
Marcus Meissner <marcus@jet.franken.de>
2008-01-06 17:46:08 +0000

    added nikon s500
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@10926 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -504,6 +504,8 @@ static struct {
 	{"Nikon:Coolpix S6 (PTP mode)",   0x04b0, 0x014e, 0},
 	/* Ole Aamot <ole@gnome.org> */
 	{"Nikon:Coolpix P5000 (PTP mode)",0x04b0, 0x015b, PTP_CAP|PTP_NIKON_BROKEN_CAP},
+	/* Peter Pregler <Peter_Pregler@email.com> */
+	{"Nikon:Coolpix S500 (PTP mode)", 0x04b0, 0x015d, 0},
 	{"Nikon:Coolpix L12 (PTP mode)",  0x04b0, 0x015f, 0},
 	/* Marius Groeger <marius.groeger@web.de> */
 	{"Nikon:Coolpix S200 (PTP mode)", 0x04b0, 0x0161, PTP_CAP|PTP_NIKON_BROKEN_CAP},
