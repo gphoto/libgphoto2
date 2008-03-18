22ffb1d22969ccba3b6ef5169e40ba72530730ee
Marcus Meissner <marcus@jet.franken.de>
2008-03-18 20:09:31 +0000

    backported: ignore some more remount fs
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@11006 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/disk/disk.c
+++ libgphoto2_port/disk/disk.c
@@ -206,10 +206,16 @@ gp_port_library_list (GPPortInfoList *list)
 			gp_log(GP_LOG_DEBUG, "gphoto2-port/disk",
 			       "found fstab fsname %s", mntent->mnt_fsname);
 
-			if ((NULL != strstr(mntent->mnt_fsname,"fd")) ||
+			if ((NULL != strstr(mntent->mnt_fsname,"fd"))	||
 			    (NULL != strstr(mntent->mnt_fsname,"floppy")) ||
-			    (NULL != strstr(mntent->mnt_fsname, "fuse")))
-			{
+			    (NULL != strstr(mntent->mnt_fsname,"fuse"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"nfs"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"cifs"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"smbfs"))||
+			    (NULL != strstr(mntent->mnt_fsname,"afs"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"autofs"))||
+			    (NULL != strstr(mntent->mnt_fsname,"gphotofs"))
+			) {
 				continue;
 			}
 			snprintf (path, sizeof(path), "%s/DCIM", mntent->mnt_dir);
@@ -233,10 +239,15 @@ gp_port_library_list (GPPortInfoList *list)
 			gp_log(GP_LOG_DEBUG, "gphoto2-port/disk",
 			       "found mtab fsname %s", mntent->mnt_fsname);
 
-			if ((NULL != strstr(mntent->mnt_fsname,"fd")) ||
+			if ((NULL != strstr(mntent->mnt_fsname,"fd"))	||
 			    (NULL != strstr(mntent->mnt_fsname,"floppy")) ||
-			    (NULL != strstr(mntent->mnt_fsname, "fuse")))
-			{
+			    (NULL != strstr(mntent->mnt_fsname,"fuse"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"nfs"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"smbfs"))||
+			    (NULL != strstr(mntent->mnt_fsname,"afs"))	||
+			    (NULL != strstr(mntent->mnt_fsname,"autofs"))||
+			    (NULL != strstr(mntent->mnt_fsname,"gphotofs"))
+			) {
 				continue;
 			}
 
