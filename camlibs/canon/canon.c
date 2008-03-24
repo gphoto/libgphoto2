87fe10c93a1c35df02cee3498ad595785974cd7a
Marcus Meissner <marcus@jet.franken.de>
2008-03-24 22:08:27 +0000

    reset the filesystem after capture, so we "see" the new image.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@11029 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/canon.c
+++ camlibs/canon/canon.c
@@ -177,7 +177,7 @@ const struct canonCamModelData models[] = {
         {"Canon:Digital IXUS v3 (normal mode)", CANON_CLASS_4,  0x04A9, 0x3070, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         /* 0x3071 is S230/IXUS v3 in PTP mode */
         /* Following cameras share the ID for PTP and Canon modes */
-#if 0 /* served better by PTP2 driver ... I own one of them too. -Marcus */
+#if 1 /* served better by PTP2 driver ... I own one of them too. -Marcus */
         {"Canon:PowerShot SD100 (normal mode)", CANON_CLASS_5,  0x04A9, 0x3072, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
         {"Canon:Digital IXUS II (normal mode)", CANON_CLASS_5,  0x04A9, 0x3072, CAP_SUP, SL_MOVIE_LARGE, SL_THUMB, SL_PICTURE, NULL},
 #endif
@@ -1344,6 +1344,9 @@ static void canon_int_find_new_image ( Camera *camera, unsigned char *initial_st
                                 strncpy ( path->name, new_name,
                                           strlen ( new_name ) );
                                 strcpy ( path->folder, canon2gphotopath ( camera, path->folder ) );
+
+				/* FIXME: Marcus: make it less large effort... */
+				gp_filesystem_reset (camera->fs);
                                 break;
                         }
                         else {
