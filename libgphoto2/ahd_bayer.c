cb0d4c3fa6dc52f6d23b498ac2e17f54f5b5b5ad
Theodore Kilgore <kilgota@auburn.edu>
2008-05-07 00:08:23 +0000

    ahd_bayer.c licensing oops fixed. GPL -> LGPL. Change approved by both authors.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@11130 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2/ahd_bayer.c
+++ libgphoto2/ahd_bayer.c
@@ -20,7 +20,7 @@
  *
  * \par License
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
@@ -31,7 +31,7 @@
  * Lesser General Public License for more details.
  *
  * \par
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
