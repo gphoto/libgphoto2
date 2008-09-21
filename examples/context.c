9dd5dc2a1e772ce2cf7d7cb49d21cb53dffafaed
Marcus Meissner <marcus@jet.franken.de>
2008-09-21 19:38:27 +0000

    added a sample framework
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@11341 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ examples/context.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "samples.h"
+
+static void
+ctx_error_func (GPContext *context, const char *format, va_list args, void *data)
+{
+        fprintf  (stderr, "\n");
+        fprintf  (stderr, "*** Contexterror ***              \n");
+        vfprintf (stderr, format, args);
+        fprintf  (stderr, "\n");
+        fflush   (stderr);
+}
+
+static void
+ctx_status_func (GPContext *context, const char *format, va_list args, void *data)
+{
+        vfprintf (stderr, format, args);
+        fprintf  (stderr, "\n");
+        fflush   (stderr);
+}
+
+GPContext* sample_create_context() {
+	GPContext *context;
+
+	/* This is the mandatory part */
+	context = gp_context_new();
+
+	/* All the parts below are optional! */
+        gp_context_set_error_func (context, ctx_error_func, NULL);
+        gp_context_set_status_func (context, ctx_status_func, NULL);
+
+	/* also:
+	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
+        gp_context_set_message_func   (p->context, ctx_message_func, p);
+        if (isatty (STDOUT_FILENO))
+                gp_context_set_progress_funcs (p->context,
+                        ctx_progress_start_func, ctx_progress_update_func,
+                        ctx_progress_stop_func, p);
+	 */
+	return context;
+}
---
+++ examples/sample-autodetect.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+
+#include <gphoto2/gphoto2-camera.h>
+
+#include "samples.h"
+
+int main(int argc, char **argv) {
+	CameraList	*list;
+	Camera		**cams;
+	int		ret, i, count;
+	const char	*name, *value;
+	GPContext	*context;
+
+	context = sample_create_context ();
+
+	ret = gp_list_new (&list);
+	if (ret < GP_OK) return 1;
+	count = sample_autodetect (list, context);
+	printf("Number of cameras: %d\n", count);
+	cams = calloc (sizeof (Camera*),count);
+        for (i = 0; i < count; i++) {
+                gp_list_get_name  (list, i, &name);
+                gp_list_get_value (list, i, &value);
+		ret = sample_open_camera (&cams[i], name, value);
+		if (ret < GP_OK) fprintf(stderr,"Camera %s on port %s failed to open\n", name, value);
+        }
+	for (i = 0; i < count; i++) {
+		CameraText	text;
+	        ret = gp_camera_get_summary (cams[i], &text, context);
+		if (ret < GP_OK) {
+			fprintf (stderr, "Failed to get summary.\n");
+			continue;
+		}
+
+                gp_list_get_name  (list, i, &name);
+                gp_list_get_value (list, i, &value);
+                printf("%-30s %-16s\n", name, value);
+		printf("Summary:\n%s\n", text.text);
+	}
+	return 0;
+}
