75172742469926ab0ce317030bba33504c139e1a
Marcus Meissner <marcus@jet.franken.de>
2008-10-12 19:43:22 +0000

    added
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@11376 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ examples/sample-multi-detect.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <gphoto2/gphoto2-camera.h>
+
+#include "samples.h"
+
+/* Sample autodetection program.
+ *
+ * This program can autodetect multiple cameras and then calls a
+ * simple function in each of them (summary).
+ */
+
+int main(int argc, char **argv) {
+	CameraList	*list;
+	Camera		**cams;
+	int		ret, i, count;
+	const char	*name, *value;
+	GPContext	*context;
+
+	context = sample_create_context (); /* see context.c */
+
+	/* Detect all the cameras that can be autodetected... */
+	ret = gp_list_new (&list);
+	if (ret < GP_OK) return 1;
+	count = sample_autodetect (list, context);
+
+	/* Now open all cameras we autodected for usage */
+	printf("Number of cameras: %d\n", count);
+	cams = calloc (sizeof (Camera*),count);
+        for (i = 0; i < count; i++) {
+                gp_list_get_name  (list, i, &name);
+                gp_list_get_value (list, i, &value);
+		ret = sample_open_camera (&cams[i], name, value);
+		if (ret < GP_OK) fprintf(stderr,"Camera %s on port %s failed to open\n", name, value);
+        }
+	/* Now call a simple function in each of those cameras. */
+	for (i = 0; i < count; i++) {
+		CameraText	text;
+		char 		*owner;
+	        ret = gp_camera_get_summary (cams[i], &text, context);
+		if (ret < GP_OK) {
+			fprintf (stderr, "Failed to get summary.\n");
+			continue;
+		}
+
+                gp_list_get_name  (list, i, &name);
+                gp_list_get_value (list, i, &value);
+                printf("%-30s %-16s\n", name, value);
+		printf("Summary:\n%s\n", text.text);
+
+		/* Query a simple string configuration variable. */
+		ret = get_config_value_string (cams[i], "owner", &owner, context);
+		if (ret >= GP_OK) {
+			printf("Owner: %s\n", owner);
+			free (owner);
+		} else {
+			printf("Owner: No owner found.\n");
+		}
+
+	}
+	return 0;
+}
---
+++ examples/sample-owner.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <gphoto2/gphoto2-camera.h>
+
+#include "samples.h"
+
+/* Set the owner of the first camera.
+ *
+ * This program can autodetect a single camera and then sets its
+ * owner to the string passed on the cmdline.
+ *
+ * Same as:
+ *	gphoto2 --get-config owner
+ *	gphoto2 --set-config owner="Owner Name"
+ */
+
+int main(int argc, char **argv) {
+	Camera		*camera;
+	int		ret;
+	char		*owner;
+	GPContext	*context;
+
+	context = sample_create_context (); /* see context.c */
+	gp_camera_new (&camera);
+
+	/* This call will autodetect cameras, take the
+	 * first one from the list and use it. It will ignore
+	 * any others... See the *multi* examples on how to
+	 * detect and use more than the first one.
+	 */
+	ret = gp_camera_init (camera, context);
+	if (ret < GP_OK) {
+		printf("No camera auto detected.\n");
+		gp_camera_free (camera);
+		return 0;
+	}
+
+	ret = get_config_value_string (camera, "owner", &owner, context);
+	if (ret < GP_OK) {
+		printf ("Could not query owner.\n");
+		goto out;
+	}
+	printf("Current owner: %s\n", owner);
+	if (argc > 1) {
+		ret = set_config_value_string (camera, "owner", argv[1], context);
+		if (ret < GP_OK) {
+			fprintf (stderr, "Failed to set camera owner to %s; %d\n", argv[1], ret);
+		} else 
+			printf("New owner: %s\n", argv[1]);
+	}
+out:
+	gp_camera_exit (camera, context);
+	gp_camera_free (camera);
+	return 0;
+}
