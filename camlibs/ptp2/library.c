ea82feab6dcaf3e533f943f24b2c9e259164df32
Marcus Meissner <marcus@jet.franken.de>
2009-01-27 12:32:44 +0000

    p5000 sends capture complete
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@11780 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -775,7 +775,7 @@ static struct {
 	{"Nikon:Coolpix S2 (PTP mode)",   0x04b0, 0x014e, 0},
 	{"Nikon:Coolpix S6 (PTP mode)",   0x04b0, 0x014e, 0},
 	/* Ole Aamot <ole@gnome.org> */
-	{"Nikon:Coolpix P5000 (PTP mode)",0x04b0, 0x015b, PTP_CAP|PTP_NIKON_BROKEN_CAP},
+	{"Nikon:Coolpix P5000 (PTP mode)",0x04b0, 0x015b, PTP_CAP},/* mm: sends capture complete */
 	/* Peter Pregler <Peter_Pregler@email.com> */
 	{"Nikon:Coolpix S500 (PTP mode)", 0x04b0, 0x015d, 0},
 	{"Nikon:Coolpix L12 (PTP mode)",  0x04b0, 0x015f, 0},
