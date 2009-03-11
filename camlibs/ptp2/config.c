5c486f1962c5bfa65c076d5d611b6c0f5c383bcc
Marcus Meissner <marcus@jet.franken.de>
2009-03-11 22:10:40 +0000

    added canon rotation, some more error checking
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@11853 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -460,10 +460,14 @@ static int
 _get_Generic16Table(CONFIG_GET_ARGS, struct deviceproptableu16* tbl, int tblsize) {
 	int i, j;
 
-	if (!(dpd->FormFlag & PTP_DPFF_Enumeration))
+	if (!(dpd->FormFlag & PTP_DPFF_Enumeration)) {
+		gp_log (GP_LOG_DEBUG, "ptp/get_generic16", "no enumeration in 16bit table code");
 		return (GP_ERROR);
-	if (dpd->DataType != PTP_DTC_UINT16)
+	}
+	if (dpd->DataType != PTP_DTC_UINT16) {
+		gp_log (GP_LOG_DEBUG, "ptp/get_generic16", "no uint16 prop in 16bit table code");
 		return (GP_ERROR);
+	}
 
 	gp_widget_new (GP_WIDGET_RADIO, _(menu->label), widget);
 	gp_widget_set_name (*widget, menu->name);
@@ -1560,10 +1564,30 @@ GENERIC8TABLE(Nikon_CameraOrientation,nikon_orientation)
 static struct deviceproptableu16 canon_orientation[] = {
 	{ "0'",		0, 0 },
 	{ "90'",	1, 0 },
-	{ "180'",	2, 0 },	/* guessed */
+	{ "180'",	2, 0 },
 	{ "270'",	3, 0 },
 };
-GENERIC16TABLE(Canon_CameraOrientation,canon_orientation)
+
+static int
+_get_Canon_CameraOrientation(CONFIG_GET_ARGS) {
+	char	orient[20];
+	int	i;
+
+	if (dpd->DataType != PTP_DTC_UINT16)
+		return (GP_ERROR);
+	gp_widget_new (GP_WIDGET_TEXT, _(menu->label), widget);
+	gp_widget_set_name (*widget, menu->name);
+	for (i=0;i<sizeof(canon_orientation)/sizeof(canon_orientation[0]);i++) {
+		if (canon_orientation[i].value != dpd->CurrentValue.u16)
+			continue;
+		gp_widget_set_value (*widget, canon_orientation[i].label);
+		return GP_OK;
+	}
+	sprintf (orient, _("Unknown value 0x%04x"), dpd->CurrentValue.u16);
+	gp_widget_set_value (*widget, orient);
+	return GP_OK;
+}
+
 
 static struct deviceproptableu8 nikon_afsensor[] = {
 	{ N_("Centre"),	0x00, 0 },
@@ -2007,6 +2031,7 @@ static struct deviceproptableu8 canon_eos_whitebalance[] = {
       { N_("Fluorescent"),	5, 0 },
       { N_("Flash"),		6, 0 },
       { N_("Custom"),		8, 0 },
+      { N_("Color Temperature"),9, 0 },	/* 40d*/
 };
 GENERIC8TABLE(Canon_EOS_WhiteBalance,canon_eos_whitebalance)
 
@@ -3179,8 +3204,10 @@ camera_get_config (Camera *camera, CameraWidget **window, GPContext *context)
 				} else {
 					ret = cursub->getfunc (camera, &widget, cursub, NULL);
 				}
-				if (ret != GP_OK)
+				if (ret != GP_OK) {
+					gp_log (GP_LOG_DEBUG, "camera_get_config", "Unable to set Property %04x (%s), ret %d", cursub->propid, cursub->label, ret);
 					continue;
+				}
 				gp_widget_append (section, widget);
 				continue;
 			}
