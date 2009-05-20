5add7f6d95e0425bdf773a5748047ec0c04730f6
Marcus Meissner <marcus@jet.franken.de>
2009-05-20 15:06:18 +0000

    in dual jpg+raw capture, add both files to the fs, but return only the last one.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@12115 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1779,7 +1779,7 @@ camera_canon_eos_capture (Camera *camera, CameraCaptureType type, CameraFilePath
 				/* delete last / or we get confused later. */
 				path->folder[ strlen(path->folder)-1 ] = '\0';
 				gp_filesystem_append (camera->fs, path->folder, path->name, context);
-				break;
+				continue; /* for RAW+JPG mode capture, we just return the last image for now. */
 			}
 		}
 		free (entries);
