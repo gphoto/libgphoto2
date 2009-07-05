5bed841f16247180b7825f39ae3cb692284f5b25
Marcus Meissner <marcus@jet.franken.de>
2009-07-05 10:58:02 +0000

    added preview flag for EOS 50D
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@12191 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -920,7 +920,7 @@ static struct {
 	/* IRC Reporter */
 	{"Canon:EOS 5D Mark II",		0x04a9, 0x3199, PTP_CAP|PTP_CAP_PREVIEW|PTPBUG_DELETE_SENDS_EVENT},
 	/* mitch <debianuser@mll.dissimulo.com> */
-	{"Canon:EOS 50D",			0x04a9, 0x319b, PTP_CAP|PTPBUG_DELETE_SENDS_EVENT},
+	{"Canon:EOS 50D",			0x04a9, 0x319b, PTP_CAP|PTP_CAP_PREVIEW|PTPBUG_DELETE_SENDS_EVENT},
 
 	{"Canon:PowerShot A480",		0x04a9, 0x31bf, PTPBUG_DELETE_SENDS_EVENT},
 
