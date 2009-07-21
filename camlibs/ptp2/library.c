c64a6836e06c880eb62b2d1813084e0d80071942
Marcus Meissner <marcus@jet.franken.de>
2009-07-21 11:42:49 +0000

    added powershot a1100is
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@12246 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -932,6 +932,8 @@ static struct {
 	{"Canon:Digital IXUS 100 IS",		0x04a9, 0x31c2, PTP_CAP|PTP_CAP_PREVIEW|PTPBUG_DELETE_SENDS_EVENT},
 	/* https://sourceforge.net/tracker/?func=detail&aid=2769511&group_id=8874&atid=208874 */
 	{"Canon:PowerShot SD780 IS",		0x04a9, 0x31c2, PTP_CAP|PTP_CAP_PREVIEW|PTPBUG_DELETE_SENDS_EVENT},
+	/* Matthew Vernon <matthew@sel.cam.ac.uk> */
+	{"Canon:PowerShot A1100 IS",		0x04a9, 0x31c3, PTPBUG_DELETE_SENDS_EVENT},
 	/* https://sourceforge.net/tracker/index.php?func=detail&aid=2796275&group_id=8874&atid=358874 */
 	{"Canon:EOS 500D",                      0x04a9, 0x31cf, PTP_CAP|PTP_CAP_PREVIEW},
 
