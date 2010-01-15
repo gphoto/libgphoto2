ad0af56d048c7660abb14d1c60f1ecc8c6813516
Marcus Meissner <marcus@jet.franken.de>
2010-01-15 21:52:23 +0000

    special case the nikon 4300
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@12592 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sierra/sierra-desc.h
+++ camlibs/sierra/sierra-desc.h
@@ -146,6 +146,7 @@ extern const CameraDescType oly3000z_cam_desc;
 extern const CameraDescType cp880_cam_desc;
 extern const CameraDescType cp995_cam_desc;
 extern const CameraDescType cp2500_cam_desc;
+extern const CameraDescType cp4300_cam_desc;
 
 /*
  * epson-desc.c:
