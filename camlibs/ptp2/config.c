036d8bfb89674a9f9a11468bcb48c9d700e88cdf
Marcus Meissner <marcus@jet.franken.de>
2010-01-24 09:00:31 +0000

    handle error case, so we don't reference an uninitialized widget
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@12634 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -4482,7 +4482,7 @@ camera_get_config (Camera *camera, CameraWidget **window, GPContext *context)
 					memset(&dpd,0,sizeof(dpd));
 					ptp_getdevicepropdesc(params,cursub->propid,&dpd);
 					ret = cursub->getfunc (camera, &widget, cursub, &dpd);
-					if (dpd.GetSet == PTP_DPGS_Get)
+					if ((ret == GP_OK) && (dpd.GetSet == PTP_DPGS_Get))
 						gp_widget_set_readonly (widget, 1);
 					ptp_free_devicepropdesc(&dpd);
 					if (nrofsetprops)
