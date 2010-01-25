ae6cff8024c6d88f87625590df7a0a0c8dba9c42
Marcus Meissner <marcus@jet.franken.de>
2010-01-25 07:24:27 +0000

    added powershot sd970 alias for ixus 990is
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@12653 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -983,6 +983,8 @@ static struct {
 	{"Canon:PowerShot SX200 IS",		0x04a9, 0x31c0, 0},
 	/* https://sourceforge.net/tracker/index.php?func=detail&aid=2789326&group_id=8874&atid=358874 */
 	{"Canon:Digital IXUS 990 IS",		0x04a9, 0x31c1, PTPBUG_DELETE_SENDS_EVENT},
+	/* Michael Ole Olsen <gnu@gmx.net> */
+	{"Canon:PowerShot SD970 IS",		0x04a9, 0x31c1, PTPBUG_DELETE_SENDS_EVENT},
 	/* https://sourceforge.net/tracker/?func=detail&aid=2769511&group_id=8874&atid=208874 */
 	/* Do not confuse with PowerShot SX100IS */
 	{"Canon:Digital IXUS 100 IS",           0x04a9, 0x31c2, PTPBUG_DELETE_SENDS_EVENT},
