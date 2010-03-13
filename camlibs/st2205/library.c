1ebc1a7fe42abda6ca6a7b28d5f187197ac4f285
Marcus Meissner <marcus@jet.franken.de>
2010-03-13 11:23:25 +0000

    st2205 picture frame driver
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@12749 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ camlibs/st2205/library.c
@@ -0,0 +1,438 @@
+/* Sitronix st2205 picframe access library
+ *
+ * Copyright (c) 2010 Hans de Goede <hdegoede@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include "config.h"
+
+#include <string.h>
+#include <stdlib.h>
+#ifdef HAVE_ICONV
+# include <langinfo.h>
+#endif
+#ifdef HAVE_GD
+# include <gd.h>
+#endif
+
+#include <gphoto2/gphoto2-library.h>
+#include <gphoto2/gphoto2-result.h>
+#include <gphoto2/gphoto2-port.h>
+#include "st2205.h"
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (GETTEXT_PACKAGE, String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
+#else
+#  define _(String) (String)
+#  define N_(String) (String)
+#endif
+
+int
+camera_id (CameraText *id)
+{
+	strcpy (id->text, "ST2205 USB picture frame");
+
+	return GP_OK;
+}
+
+int
+camera_abilities (CameraAbilitiesList *list)
+{
+	CameraAbilities a;
+
+	memset (&a, 0, sizeof(a));
+	strcpy (a.model, "ST2205 USB picture frame");
+	a.status = GP_DRIVER_STATUS_TESTING;
+	a.port   = GP_PORT_USB_DISK_DIRECT;
+	a.speed[0] = 0;
+	a.usb_vendor = 0x1403;
+	a.usb_product= 0x0001;
+	a.operations = GP_OPERATION_NONE;
+	a.folder_operations = GP_FOLDER_OPERATION_PUT_FILE;
+	/* FIXME add support for downloading RAW images */
+	/* FIXME add support for delete all */
+	a.file_operations   = GP_FILE_OPERATION_DELETE;
+	return gp_abilities_list_append (list, a);
+}
+
+static int
+camera_summary (Camera *camera, CameraText *summary, GPContext *context)
+{
+	sprintf (summary->text,
+		 _("Your USB picture frame has a ST2205 chipset\n"));
+	return GP_OK;
+}
+
+static int
+camera_manual (Camera *camera, CameraText *manual, GPContext *context)
+{
+	strcpy(manual->text,
+	_(
+	"ST2205 based picture frames come with a variety of resolutions.\n"
+	"The gphoto driver for these devices allows you to download,\n"
+	"upload and delete pictures from the picture frame."
+	));
+
+	return GP_OK;
+}
+
+static int
+camera_about (Camera *camera, CameraText *about, GPContext *context)
+{
+	strcpy (about->text,
+	_(
+	"ST2205 USB picture frame driver\n"
+	"Hans de Goede <hdegoede@redhat.com>\n"
+	"This driver allows you to download, upload and delete pictures\n"
+	"from the picture frame."
+	));
+
+	return GP_OK;
+}
+
+static int get_file_idx(CameraPrivateLibrary *pl, const char *folder,
+	const char *filename)
+{
+	int i;
+
+	if (strcmp (folder, "/"))
+		return GP_ERROR_DIRECTORY_NOT_FOUND;
+
+	for (i = 0; i < ST2205_MAX_NO_FILES; i++) {
+		if (!strcmp (filename, pl->filenames[i]))
+			break;
+	}
+
+	if (i == ST2205_MAX_NO_FILES)
+		return GP_ERROR_FILE_NOT_FOUND;
+
+	return i;
+}
+
+static int
+get_file_func (CameraFilesystem *fs, const char *folder, const char *filename,
+	       CameraFileType type, CameraFile *file, void *data,
+	       GPContext *context)
+{
+#ifdef HAVE_GD
+	Camera *camera = data;
+	gdImagePtr im;
+	int ret, idx, size;
+	void *gdpng;
+	char *png;
+
+	idx = get_file_idx(camera->pl, folder, filename);
+	if (idx < 0)
+		return idx;
+
+	im = gdImageCreateTrueColor(camera->pl->width, camera->pl->height);
+	if (im == NULL)
+		return GP_ERROR_NO_MEMORY;
+
+	ret = st2205_read_file(camera, idx, im->tpixels);
+	if (ret < 0) {
+		gdImageDestroy (im);
+		return ret;
+	}
+
+	gdpng = gdImagePngPtr(im, &size);
+	gdImageDestroy (im);
+	if (gdpng == NULL)
+		return GP_ERROR_NO_MEMORY;
+
+	/* Ugh gd uses its own memory management */
+	png = malloc(size);
+	if (png == NULL) {
+		gdFree(gdpng);
+		return GP_ERROR_NO_MEMORY;
+	}
+	memcpy(png, gdpng, size);
+	gdFree(gdpng);
+
+	ret = gp_file_set_mime_type (file, GP_MIME_PNG);
+	if (ret < 0) { free (png); return ret; }
+
+	ret = gp_file_set_name (file, filename); 
+	if (ret < 0) { free (png); return ret; }
+
+	ret = gp_file_set_data_and_size (file, png, size);
+	if (ret < 0) { free (png); return ret; }
+
+	return GP_OK;
+#else
+	return GP_ERROR_NOT_SUPPORTED;
+#endif
+}
+
+static int
+put_file_func (CameraFilesystem *fs, const char *folder, const char *name, 
+	CameraFileType type, CameraFile *file, void *data, GPContext *context)
+{
+#ifdef HAVE_GD
+	Camera *camera = data;
+	char *in_name, *out_name, *filedata = NULL;
+	int ret;
+	size_t inc, outc;
+#ifdef HAVE_ICONV
+	char *in, *out;
+#endif
+	unsigned long filesize = 0;
+	gdImagePtr im_out, im_in = NULL;
+
+	if (strcmp (folder, "/"))
+		return GP_ERROR_DIRECTORY_NOT_FOUND;
+
+	inc = strlen (name);
+	in_name = strdup (name);
+	outc = inc;
+	out_name = malloc (outc + 1);
+	if (!in_name || !out_name) {
+		free (in_name);
+		free (out_name);
+		return GP_ERROR_NO_MEMORY;
+	}
+
+	/* Convert name to ASCII */
+#ifdef HAVE_ICONV
+	in = in_name;
+	out = out_name;
+	if (iconv (camera->pl->cd, &in, &inc, &out, &outc) == -1) {
+		free (in_name);
+		free (out_name);
+		gp_log (GP_LOG_ERROR, "iconv",
+			"Failed to convert filename to ASCII");
+		return GP_ERROR_OS_FAILURE;
+	}
+	outc = out - out_name;
+	if (outc > ST2205_FILENAME_LENGTH)
+		outc = ST2205_FILENAME_LENGTH;
+	out_name[outc] = 0;
+#else
+	for (i = 0; in_name[i] && i < ST2205_FILENAME_LENGTH; i++) {
+		if ((uint8_t)in_name[i] < 0x20 || (uint8_t)in_name[i] > 0x7E)
+			out_name[i] = '?';
+		else
+			out_name[i] = in_name[i];
+	}
+	out_name[i] = 0;
+#endif
+	free (in_name);
+
+	ret = gp_file_get_data_and_size (file, (const char **)&filedata,
+					 &filesize);
+	if (ret < 0) { free (out_name); return ret; }
+
+	/* Try loading the file using gd, starting with the most often
+	   used types first */
+
+	/* gdImageCreateFromJpegPtr is chatty on error, don't call it on
+	   non JPEG files */
+	if (filesize > 2 &&
+	    (uint8_t)filedata[0] == 0xff && (uint8_t)filedata[1] == 0xd8)
+		im_in = gdImageCreateFromJpegPtr(filesize, filedata);
+	if (im_in == NULL)
+		im_in = gdImageCreateFromPngPtr(filesize, filedata);
+	if (im_in == NULL)
+		im_in = gdImageCreateFromGifPtr(filesize, filedata);
+	if (im_in == NULL)
+		im_in = gdImageCreateFromWBMPPtr(filesize, filedata);
+	if (im_in == NULL) {
+		gp_log (GP_LOG_ERROR, "st2205",
+			"Unrecognized file format for file: %s%s",
+			folder, name);
+		free (out_name);
+		return GP_ERROR_OS_FAILURE;
+	}
+
+	im_out = gdImageCreateTrueColor(camera->pl->width, camera->pl->height);
+	if (im_out == NULL) {
+		gdImageDestroy (im_in);
+		free (out_name);
+		return GP_ERROR_NO_MEMORY;
+	}
+
+	gdImageCopyResampled (im_out, im_in, 0, 0, 0, 0,
+			      im_out->sx, im_out->sy,
+			      im_in->sx, im_in->sy);
+
+	gdImageSharpen(im_out, 100);
+
+	ret = st2205_write_file (camera, out_name, im_out->tpixels);
+	if (ret >= 0) {
+		/* Add to our filenames list */
+		ST2205_SET_FILENAME(camera->pl->filenames[ret], out_name, ret);
+		/* And commit the changes to the device */
+		ret = st2205_commit(camera);
+	}
+
+	gdImageDestroy (im_in);
+	gdImageDestroy (im_out);
+	free (out_name);
+	return GP_OK;
+#else
+	return GP_ERROR_NOT_SUPPORTED;
+#endif
+}
+
+static int
+delete_file_func (CameraFilesystem *fs, const char *folder,
+		  const char *filename, void *data, GPContext *context)
+{
+	Camera *camera = data;
+	int ret, idx;
+
+	idx = get_file_idx(camera->pl, folder, filename);
+	if (idx < 0)
+		return idx;
+
+	ret = st2205_delete_file(camera, idx);
+	if (ret < 0) return ret;
+
+	/* Also remove the file from our cached filelist */
+	camera->pl->filenames[idx][0] = 0;
+
+	return st2205_commit(camera);
+}
+
+static int
+get_info_func (CameraFilesystem *fs, const char *folder, const char *filename,
+	       CameraFileInfo *info, void *data, GPContext *context)
+{
+	memset (info, 0, sizeof(CameraFileInfo));
+	/* FIXME: fill in some stuff? */
+	return GP_OK;
+}
+
+static int
+file_list_func (CameraFilesystem *fs, const char *folder, CameraList *list,
+		void *data, GPContext *context)
+{
+	Camera *camera = data; 
+	int i, ret;
+
+	for (i = 0; i < ST2205_MAX_NO_FILES; i++) {
+		if (camera->pl->filenames[i][0]) {
+			ret = gp_list_append (list, camera->pl->filenames[i],
+					      NULL);
+			if (ret < 0) return ret;
+		}
+	}
+
+	return GP_OK;
+}
+
+static CameraFilesystemFuncs fsfuncs = {
+	.file_list_func = file_list_func,
+	.get_info_func = get_info_func,
+	.get_file_func = get_file_func,
+	.del_file_func = delete_file_func,
+	.put_file_func = put_file_func,
+};
+
+static int
+camera_exit (Camera *camera, GPContext *context) 
+{
+	if (camera->pl != NULL) {
+#ifdef HAVE_ICONV
+		if (camera->pl->cd != (iconv_t) -1)
+			iconv_close (camera->pl->cd);
+#endif
+		st2205_close (camera);
+		free (camera->pl);
+		camera->pl = NULL;
+	}
+	return GP_OK;
+}
+
+int
+camera_init (Camera *camera, GPContext *context) 
+{
+	int i, j, ret;
+#ifdef HAVE_ICONV
+	char *curloc;
+#endif
+	st2205_filename clean_name;
+
+	/* First, set up all the function pointers */
+	camera->functions->exit    = camera_exit;
+	camera->functions->summary = camera_summary;
+	camera->functions->manual  = camera_manual;
+	camera->functions->about   = camera_about;
+
+	/* Tell the CameraFilesystem where to get lists from */
+	gp_filesystem_set_funcs (camera->fs, &fsfuncs, camera);
+
+	camera->pl = calloc (1, sizeof(CameraPrivateLibrary));
+	if (!camera->pl) return GP_ERROR_NO_MEMORY;
+
+#ifdef HAVE_ICONV
+	curloc = nl_langinfo (CODESET);
+	if (!curloc)
+		curloc="UTF-8";
+	camera->pl->cd = iconv_open("ASCII", curloc);
+	if (camera->pl->cd == (iconv_t) -1) {
+		gp_log (GP_LOG_ERROR, "iconv",
+			"Failed to create iconv converter");
+		return GP_ERROR_OS_FAILURE;
+	}
+#endif
+
+#if 1
+	ret = st2205_open_device (camera);
+#else
+	ret = st2205_open_dump (camera,
+				"/home/hans/st2205tool/memdump.bin", 128, 128);
+#endif
+	if (ret != GP_OK) {
+		st2205_close (camera);
+		free(camera->pl);
+		return ret;
+	}
+
+	/* Get the filenames from the picframe */
+	ret = st2205_get_filenames (camera, camera->pl->filenames);
+	if (ret != GP_OK) {
+		free (camera->pl);
+		return ret;
+	}
+
+	/* And clean them up and make them unique */
+	for (i = 0; i < ST2205_MAX_NO_FILES; i++) {
+		if (!camera->pl->filenames[i][0])
+			continue;
+
+		/* Filter out non ASCII chars (some frames ship
+		   with sample photo's with garbage in the names) */
+		for (j = 0; camera->pl->filenames[i][j]; j++) {
+			if ((uint8_t)camera->pl->filenames[i][j] < 0x20 ||
+			    (uint8_t)camera->pl->filenames[i][j] > 0x7E)
+				clean_name[j] = '?';
+			else
+				clean_name[j] = camera->pl->filenames[i][j];
+		}
+		clean_name[j] = 0;
+
+		ST2205_SET_FILENAME(camera->pl->filenames[i], clean_name, i);
+	}
+
+	return GP_OK;
+}
---
+++ camlibs/st2205/st2205_decode.c
@@ -0,0 +1,367 @@
+/* Sitronix st2205 picframe decompression and compression code
+ *
+ *   Copyright (c) 2010 Hans de Goede <hdegoede@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include "config.h"
+
+#include <string.h>
+#include <stdlib.h>
+#ifdef HAVE_GD
+# include <gd.h>
+#endif
+
+#include "st2205.h"
+
+#ifdef HAVE_GD
+#define CLAMP256(x) (((x) > 255) ? 255 : (((x) < 0) ? 0 : (x)))
+#define CLAMP64S(x) (((x) > 63) ? 63 : (((x) < -64) ? -64 : (x)))
+
+static const int16_t st2205_corr_table[16] = {
+	-26,-22,-18,-14,-11,-7,-4,-1,1,4,7,11,14,18,22,26
+};
+
+static int
+st2205_decode_block(CameraPrivateLibrary *pl, unsigned char *src,
+	int src_length, int **dest, int dest_x, int dest_y)
+{
+	st2205_lookup_row *luma_table, *chroma_table;
+	int y_base, uv_base[2], uv_corr[2];
+	int16_t Y[64], UV[2][16];
+	int x, y, r, g, b, uv;
+	if (src_length < 4) {
+		gp_log (GP_LOG_ERROR, "st2205", "short image block");
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+
+	if (src[0] & 0x80) {
+		gp_log (GP_LOG_ERROR, "st2205",
+			"2 luma bits per pixel pictures are not supported");
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+	y_base = src[1] & 0x7f;
+	luma_table = pl->lookup[src[1] >> 7];
+	chroma_table = pl->lookup[2];
+
+	uv_base[0] = src[2] & 0x7f;
+	uv_corr[0] = src[2] & 0x80;
+	uv_base[1] = src[3] & 0x7f;
+	uv_corr[1] = src[3] & 0x80;
+
+	if (src_length != (4 + (uv_corr[0]? 10:2) + (uv_corr[1]? 10:2) + 40)) {
+		GP_DEBUG ("src_length: %d, u_corr: %x v_corr: %x\n",
+			  src_length, uv_corr[0], uv_corr[1]);
+		gp_log (GP_LOG_ERROR, "st2205", "invalid block length");
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+
+	src += 4;
+
+	for (uv = 0; uv < 2; uv++) {
+		for (y = 0; y < 4; y++)
+			for (x = 0; x < 4; x++)
+				UV[uv][y * 4 + x] = uv_base[uv] - 64 +
+				    chroma_table[src[y / 2]][(y & 1) * 4 + x];
+		src += 2;
+
+		if (uv_corr[uv]) {
+			for (x = 0; x < 16; x+= 2) {
+				uint8_t corr = src[x / 2];
+				UV[uv][x    ] += st2205_corr_table[corr >> 4];
+				UV[uv][x + 1] += st2205_corr_table[corr & 0x0f];
+			}
+			src += 8;
+		}
+	}
+
+	for (y = 0; y < 8; y++) {
+		memcpy(Y + y * 8, luma_table[src[y]], 8 * sizeof(uint16_t));
+		for (x = 0; x < 8; x+= 2) {
+			uint8_t corr = src[8 + y * 4 + x / 2];
+			Y[y * 8 + x    ] +=
+				y_base + st2205_corr_table[corr >> 4];
+			Y[y * 8 + x + 1] +=
+				y_base + st2205_corr_table[corr & 0x0f];
+		}
+	}
+
+	for (y = 0; y < 8; y++) {
+		for (x = 0; x < 8; x++) {
+			r = Y[y * 8 + x] + UV[1][(y / 2) * 4 + x / 2];
+			g = Y[y * 8 + x] - UV[0][(y / 2) * 4 + x / 2] -
+			    UV[1][(y / 2) * 4 + x / 2];
+			b = Y[y * 8 + x] + UV[0][(y / 2) * 4 + x / 2];
+			dest[dest_y + y][dest_x + x] =
+				gdTrueColor (CLAMP256(2 * r),
+					     CLAMP256(2 * g),
+					     CLAMP256(2 * b));
+		}
+	}
+
+	return 0;
+}
+
+int
+st2205_decode_image(CameraPrivateLibrary *pl, unsigned char *src,
+	int src_length, int **dest, uint8_t shuffle_pattern)
+{
+	int ret, block = 0, block_length;
+	struct st2205_coord *shuffle_table;
+
+	if (shuffle_pattern >= pl->no_shuffles) {
+		gp_log (GP_LOG_ERROR, "st2205", "invalid shuffle pattern");
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+	shuffle_table = pl->shuffle[shuffle_pattern];
+
+	while (src_length && block < (pl->width * pl->height / 64)) {
+		block_length = (src[0] & 0x7f) + 1;
+		if (block_length > src_length) {
+			gp_log (GP_LOG_ERROR, "st2205", "block %d goes outside of image buffer", block);
+			return GP_ERROR_CORRUPTED_DATA;
+		}
+
+		ret = st2205_decode_block(pl, src, block_length, dest,
+					  shuffle_table[block].x,
+					  shuffle_table[block].y);
+		if (ret < 0)
+			return ret;
+		src += block_length;
+		src_length -= block_length;
+		block++;
+	}
+
+	if (src_length) {
+		gp_log (GP_LOG_ERROR, "st2205", "data remaining after decoding %d blocks", block);
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+
+	if (block != (pl->width * pl->height / 64)) {
+		gp_log (GP_LOG_ERROR, "st2205",
+			"image only contained %d of %d blocks",
+			block, pl->width * pl->height / 64);
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+
+	return 0;
+}
+
+static uint8_t st2205_find_closest_match(st2205_lookup_row *table,
+	  int16_t *row, int *smallest_diff_ret)
+{
+	int i, j;
+	uint8_t closest_match = 0;
+	unsigned int diff, smallest_diff = -1;
+
+	for (i = 0; i < 256; i++) {
+		diff = 0;
+		for (j = 0; j < 8; j++)
+			diff += (row[j] - table[i][j]) * (row[j] - table[i][j]);
+		if (diff < smallest_diff) {
+			smallest_diff = diff;
+			closest_match = i;
+		}
+	}
+
+	if (smallest_diff_ret)
+		*smallest_diff_ret = smallest_diff;
+
+	return closest_match;
+}
+
+static uint8_t st2205_closest_correction(int16_t corr)
+{
+	int i, diff, smallest_diff;
+	uint8_t closest = 0;
+
+	smallest_diff = abs(st2205_corr_table[0] - corr);
+	for (i = 1; i < 16; i++) {
+		diff = abs(st2205_corr_table[i] - corr);
+		if (diff < smallest_diff) {
+			smallest_diff = diff;
+			closest = i;
+		}
+	}
+
+	return closest;
+}
+
+static int
+st2205_code_block(CameraPrivateLibrary *pl, int **src,
+	int src_x, int src_y, unsigned char *dest, int allow_uv_corr)
+{
+	st2205_lookup_row *luma_table;
+	int y_base, uv_base[2];
+	int16_t Y[64], UV[2][16];
+	uint8_t corr1, corr2, *pattern;
+	int x, y, r, g, b, uv, diff1, diff2, used = 0;
+
+	/* Step 1 convert to "YUV" */
+	for (y = 0; y < 8; y++) {
+		for (x = 0; x < 8; x++) {
+			int p = src[src_y + y][src_x + x];
+			Y[y * 8 + x] = (gdTrueColorGetRed(p) +
+					gdTrueColorGetGreen(p) +
+					gdTrueColorGetBlue(p)) / 6;
+		}
+	}
+
+	for (y = 0; y < 4; y++) {
+		for (x = 0; x < 4; x++) {
+			int p1 = src[src_y + y    ][src_x + x    ];
+			int p2 = src[src_y + y    ][src_x + x + 1];
+			int p3 = src[src_y + y + 1][src_x + x    ];
+			int p4 = src[src_y + y + 1][src_x + x + 1];
+
+			r = gdTrueColorGetRed(p1) + gdTrueColorGetRed(p2) +
+			    gdTrueColorGetRed(p3) + gdTrueColorGetRed(p4);
+			g = gdTrueColorGetGreen(p1) + gdTrueColorGetGreen(p2) +
+			    gdTrueColorGetGreen(p3) + gdTrueColorGetGreen(p4);
+			b = gdTrueColorGetBlue(p1) + gdTrueColorGetBlue(p2) +
+			    gdTrueColorGetBlue(p3) + gdTrueColorGetBlue(p4);
+			uv = (b * 3 - (r + g + b)) / 24;
+			UV[0][y * 4 + x] = CLAMP64S(uv);
+			uv = (r * 3 - (r + g + b)) / 24;
+			UV[1][y * 4 + x] = CLAMP64S(uv);
+		}
+	}
+
+	/* Step 2a calculate base values */
+	y_base = 0;
+	for (x = 0; x < 64; x++)
+		y_base += Y[x];
+	y_base = y_base / 64;
+
+	for (uv = 0; uv < 2; uv++) {
+		uv_base[uv] = 0;
+		for (x = 0; x < 16; x++)
+			uv_base[uv] += UV[uv][x];
+		uv_base[uv] = uv_base[uv] / 16;
+	}
+
+	dest[1] = y_base;
+	dest[2] = uv_base[0] + 64;
+	dest[3] = uv_base[1] + 64;
+	used = 4;
+
+	/* Step 2b adjust values for base values and normalize */
+	for (x = 0; x < 64; x++)
+		Y[x] -= y_base;
+
+	for (uv = 0; uv < 2; uv++)
+		for (x = 0; x < 16; x++)
+			UV[uv][x] -= uv_base[uv];
+
+	/* Step 3 encode chroma values */
+	for (uv = 0; uv < 2; uv++) {
+		pattern = dest + used;
+		dest[used++] = st2205_find_closest_match (pl->lookup[2],
+							  &UV[uv][0], &diff1);
+		dest[used++] = st2205_find_closest_match (pl->lookup[2],
+							  &UV[uv][8], &diff2);
+		if ((diff1 > 64 || diff2 > 64) && allow_uv_corr) {
+			dest[2 + uv] |= 0x80;
+			for (x = 0; x < 16; x+= 2) {
+				corr1 = st2205_closest_correction(UV[uv][x] -
+					pl->lookup[2][pattern[x / 8]][x % 8]);
+				corr2 = st2205_closest_correction(UV[uv][x + 1]
+				   - pl->lookup[2][pattern[x / 8]][x % 8 + 1]);
+				dest[used++] = (corr1 << 4) | corr2;
+			}
+		}
+	}
+
+	/* Step 4a encode luma values, choose luma table */
+	diff1 = 0;
+	diff2 = 0;
+	for (y = 0; y < 8; y++) {
+		st2205_find_closest_match(pl->lookup[0], &Y[y * 8], &x);
+		diff1 += x;
+		st2205_find_closest_match(pl->lookup[1], &Y[y * 8], &x);
+		diff2 += x;
+	}
+
+	if (diff1 <= diff2) {
+		luma_table = pl->lookup[0];
+		dest[1] |= 0x00;
+	} else {
+		luma_table = pl->lookup[1];
+		dest[1] |= 0x80;
+	}
+
+	/* Step 4b encode luma values, choose luma patterns */
+	pattern = dest + used;
+	for (y = 0; y < 8; y++)
+		dest[used++] = st2205_find_closest_match (luma_table,
+							  &Y[y * 8], NULL);
+
+	/* Step 4c encode luma values, add luma correction values */
+	for (y = 0; y < 8; y++) {
+		for (x = 0; x < 8; x += 2) {
+			corr1 = st2205_closest_correction (Y[y * 8 + x] -
+						luma_table[pattern[y]][x]);
+			corr2 = st2205_closest_correction(Y[y * 8 + x + 1] -
+						luma_table[pattern[y]][x + 1]);
+			dest[used++] = (corr1 << 4) | corr2;
+		}
+	}
+
+	dest[0] = used - 1;
+
+	return used;
+}
+
+int
+st2205_code_image(CameraPrivateLibrary *pl, int **src,
+	unsigned char *dest, uint8_t shuffle_pattern, int allow_uv_corr)
+{
+	int block = 0, used = 0, ret;
+	struct st2205_coord *shuffle_table;
+
+	if (shuffle_pattern >= pl->no_shuffles) {
+		gp_log (GP_LOG_ERROR, "st2205", "invalid shuffle pattern");
+		return GP_ERROR_CORRUPTED_DATA;
+	}
+
+	shuffle_table = pl->shuffle[shuffle_pattern];
+
+	while (block < (pl->width * pl->height / 64)) {
+		ret = st2205_code_block (pl, src, shuffle_table[block].x,
+					 shuffle_table[block].y,
+					 dest + used, allow_uv_corr);
+		if (ret < 0)
+			return ret;
+		used += ret;
+		block++;
+	}
+
+	return used;
+}
+#else
+int
+st2205_decode_image(CameraPrivateLibrary *pl, unsigned char *src,
+	int src_length, int **dest, uint8_t shuffle_pattern)
+{
+	return GP_ERROR_NOT_SUPPORTED;
+}
+
+int
+st2205_code_image(CameraPrivateLibrary *pl, int **src,
+	unsigned char *dest, uint8_t shuffle_pattern, int allow_uv_corr)
+{
+	return GP_ERROR_NOT_SUPPORTED;
+}
+#endif
