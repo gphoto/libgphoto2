58982576a6abca35332b809f461be6b15533d679
Hans de Goede <hdegoede@redhat.com>
2010-04-28 13:06:59 +0000

    st2205: Add support for 320x240 frames
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@13037 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/st2205/library.c
+++ camlibs/st2205/library.c
@@ -128,6 +128,50 @@ static int get_file_idx(CameraPrivateLibrary *pl, const char *folder,
 	return i;
 }
 
+#ifdef HAVE_GD
+static void
+rotate90 (gdImagePtr src, gdImagePtr dest)
+{
+	int x, y;
+
+	for (y = 0; y < dest->sy; y++)
+		for (x = 0; x < dest->sx; x++)
+			dest->tpixels[y][x] =
+				src->tpixels[src->sy - x - 1][y];
+}
+
+static void
+rotate270 (gdImagePtr src, gdImagePtr dest)
+{
+	int x, y;
+
+	for (y = 0; y < dest->sy; y++)
+		for (x = 0; x < dest->sx; x++)
+			dest->tpixels[y][x] =
+				src->tpixels[x][src->sx - y - 1];
+}
+
+static int
+needs_rotation (Camera *camera)
+{
+	int display_orientation, user_orientation = camera->pl->orientation;
+
+	if (camera->pl->width > camera->pl->height)
+		display_orientation = ORIENTATION_LANDSCAPE;
+	else
+		display_orientation = ORIENTATION_PORTRAIT;
+
+	if (user_orientation == ORIENTATION_AUTO) {
+		if (camera->pl->width == 240 && camera->pl->height == 320)
+			user_orientation = ORIENTATION_LANDSCAPE;
+		else
+			user_orientation = display_orientation;
+	}
+
+	return display_orientation != user_orientation;
+}
+#endif
+
 static int
 get_file_func (CameraFilesystem *fs, const char *folder, const char *filename,
 	       CameraFileType type, CameraFile *file, void *data,
@@ -136,7 +180,7 @@ get_file_func (CameraFilesystem *fs, const char *folder, const char *filename,
 	Camera *camera = data;
 	int ret, idx, size;
 #ifdef HAVE_GD
-	gdImagePtr im;
+	gdImagePtr im, rotated;
 	void *gdpng;
 #endif
 
@@ -169,6 +213,17 @@ get_file_func (CameraFilesystem *fs, const char *folder, const char *filename,
 	if (ret < 0) {
 		gdImageDestroy (im);
 		return ret;
+	}
+
+	if (needs_rotation (camera)) {
+		rotated = gdImageCreateTrueColor (im->sy, im->sx);
+		if (rotated == NULL) {
+			gdImageDestroy (im);
+			return GP_ERROR_NO_MEMORY;
+		}
+		rotate270 (im, rotated);
+		gdImageDestroy (im);
+		im = rotated;
 	}
 
 	gdpng = gdImagePngPtr(im, &size);
@@ -204,7 +259,7 @@ put_file_func (CameraFilesystem *fs, const char *folder, const char *name,
 	char *in, *out;
 #endif
 	unsigned long filesize = 0;
-	gdImagePtr im_out, im_in = NULL;
+	gdImagePtr rotated, im_out, im_in = NULL;
 
 	if (strcmp (folder, "/"))
 		return GP_ERROR_DIRECTORY_NOT_FOUND;
@@ -274,6 +329,18 @@ put_file_func (CameraFilesystem *fs, const char *folder, const char *name,
 			folder, name);
 		free (out_name);
 		return GP_ERROR_BAD_PARAMETERS;
+	}
+
+	if (needs_rotation (camera)) {
+		rotated = gdImageCreateTrueColor (im_in->sy, im_in->sx);
+		if (rotated == NULL) {
+			gdImageDestroy (im_in);
+			free (out_name);
+			return GP_ERROR_NO_MEMORY;
+		}
+		rotate90 (im_in, rotated);
+		gdImageDestroy (im_in);
+		im_in = rotated;
 	}
 
 	im_out = gdImageCreateTrueColor(camera->pl->width, camera->pl->height);
@@ -429,6 +496,34 @@ static CameraFilesystemFuncs fsfuncs = {
 	.storage_info_func = storage_info_func
 };
 
+static char *
+orientation_to_string (int orientation)
+{
+	switch (orientation) {
+	case ORIENTATION_AUTO:
+		return  _("Auto");
+	case ORIENTATION_LANDSCAPE:
+		return  _("Landscape");
+	case ORIENTATION_PORTRAIT:
+		return  _("Portrait");
+	}
+	/* Never reached */
+	return NULL;
+}
+
+static int
+string_to_orientation (const char *str)
+{
+	if (strcmp (str, _("Auto")) == 0)
+		return ORIENTATION_AUTO;
+	else if (strcmp (str, _("Landscape")) == 0)
+		return ORIENTATION_LANDSCAPE;
+	else if (strcmp (str, _("Portrait")) == 0)
+		return ORIENTATION_PORTRAIT;
+	else
+		return GP_ERROR_NOT_SUPPORTED;
+}
+
 static int
 camera_get_config (Camera *camera, CameraWidget **window, GPContext *context)
 {
@@ -443,6 +538,14 @@ camera_get_config (Camera *camera, CameraWidget **window, GPContext *context)
 			_("Synchronize frame data and time with PC"), &child);
 	gp_widget_set_value (child, &camera->pl->syncdatetime);
 	gp_widget_append (*window, child);
+
+	gp_widget_new (GP_WIDGET_RADIO, _("Orientation"), &child);
+	gp_widget_add_choice (child, orientation_to_string (0));
+	gp_widget_add_choice (child, orientation_to_string (1));
+	gp_widget_add_choice (child, orientation_to_string (2));
+       	gp_widget_set_value (child,
+			     orientation_to_string (camera->pl->orientation));
+       	gp_widget_append (*window, child);
 
 	return GP_OK;
 }
@@ -460,6 +563,16 @@ camera_set_config (Camera *camera, CameraWidget *window, GPContext *context)
 	if (ret == GP_OK)
 		gp_widget_get_value (child, &camera->pl->syncdatetime);
 
+	ret = gp_widget_get_child_by_label (window, _("Orientation"), &child);
+	if (ret == GP_OK) {
+		char *value;
+		int orientation;
+		gp_widget_get_value (child, &value);
+		orientation = string_to_orientation (value);
+		if (orientation < 0) return orientation;
+		camera->pl->orientation = orientation;
+	}
+
 	return GP_OK;
 }
 
@@ -471,7 +584,9 @@ camera_exit (Camera *camera, GPContext *context)
 	if (camera->pl != NULL) {
 		buf[0] = '0' + camera->pl->syncdatetime;
 		buf[1] = 0;
-		gp_setting_set("st2205", "syncdatetime", buf);
+		gp_setting_set ("st2205", "syncdatetime", buf);
+		gp_setting_set ("st2205", "orientation", orientation_to_string
+						(camera->pl->orientation));
 #ifdef HAVE_ICONV
 		if (camera->pl->cd != (iconv_t) -1)
 			iconv_close (camera->pl->cd);
@@ -513,6 +628,13 @@ camera_init (Camera *camera, GPContext *context)
 		camera->pl->syncdatetime = buf[0] == '1';
 	else
 		camera->pl->syncdatetime = 1;
+
+	ret = gp_setting_get("st2205", "orientation", buf);
+	if (ret == GP_OK) {
+		ret = string_to_orientation (buf);
+		if (ret >= 0)
+			camera->pl->orientation = ret;
+	}
 
 #ifdef HAVE_ICONV
 	curloc = nl_langinfo (CODESET);
