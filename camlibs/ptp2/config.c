1239491e376b0ceee12598876c9d58cc6788170c
Marcus Meissner <marcus@jet.franken.de>
2010-06-29 21:51:11 +0000

    also fix usage of ret in generic ptp lazer
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@13103 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -4897,6 +4897,7 @@ camera_set_config (Camera *camera, CameraWidget *window, GPContext *context)
 				gp_log (GP_LOG_DEBUG, "camera_set_config", "Found and setting Property %04x (%s)", cursub->propid, cursub->label);
 				if ((cursub->propid & 0x7000) == 0x5000) {
 					PTPDevicePropDesc dpd;
+					uint16_t	ret2;
 
 					memset(&dpd,0,sizeof(dpd));
 					ptp_getdevicepropdesc(params,cursub->propid,&dpd);
@@ -4907,10 +4908,10 @@ camera_set_config (Camera *camera, CameraWidget *window, GPContext *context)
 						ret = GP_ERROR_NOT_SUPPORTED;
 					}
 					if (ret == GP_OK) {
-						ret = ptp_setdevicepropvalue (params, cursub->propid, &propval, cursub->type);
-						if (ret != PTP_RC_OK) {
-							gp_context_error (context, _("The property '%s' / 0x%04x was not set, PTP errorcode 0x%04x."), _(cursub->label), cursub->propid, ret);
-							ret = GP_ERROR;
+						ret2 = ptp_setdevicepropvalue (params, cursub->propid, &propval, cursub->type);
+						if (ret2 != PTP_RC_OK) {
+							gp_context_error (context, _("The property '%s' / 0x%04x was not set, PTP errorcode 0x%04x."), _(cursub->label), cursub->propid, ret2);
+							ret = translate_ptp_result (ret2);
 						}
 					}
 					ptp_free_devicepropvalue (cursub->type, &propval);
@@ -4928,9 +4929,11 @@ camera_set_config (Camera *camera, CameraWidget *window, GPContext *context)
 				ptp_canon_eos_getdevicepropdesc (params,cursub->propid, &dpd);
 				ret = cursub->putfunc (camera, widget, &propval, &dpd);
 				if (ret == GP_OK) {
-					int ret2 = ptp_canon_eos_setdevicepropvalue (params, cursub->propid, &propval, cursub->type);
-					if (ret2 != PTP_RC_OK)
+					uint16_t ret2 = ptp_canon_eos_setdevicepropvalue (params, cursub->propid, &propval, cursub->type);
+					if (ret2 != PTP_RC_OK) {
 						gp_context_error (context, _("The property '%s' / 0x%04x was not set, PTP errorcode 0x%04x."), _(cursub->label), cursub->propid, ret2);
+						ret = translate_ptp_result (ret2);
+					}
 				} else
 					gp_context_error (context, _("Setting EOS Property %04x (%s) failed with %d!"), cursub->propid, _(cursub->label), ret);
 				ptp_free_devicepropdesc(&dpd);
