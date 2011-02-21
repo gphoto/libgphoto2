17563031d45c2585c35ad57dd15f6afb09c016fc
Marcus Meissner <marcus@jet.franken.de>
2011-02-21 10:42:41 +0000

    added .gf1
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@13532 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/directory/directory.c
+++ camlibs/directory/directory.c
@@ -104,6 +104,7 @@ static const struct {
 	{"raw",  GP_MIME_RAW},
 	{"dcr",  GP_MIME_RAW},
 	{"x3f",  GP_MIME_RAW},
+	{"gf1",  GP_MIME_RAW},
 	{"srw",  GP_MIME_RAW},
 	{"png",  GP_MIME_PNG},
 	{"wav",  GP_MIME_WAV},
