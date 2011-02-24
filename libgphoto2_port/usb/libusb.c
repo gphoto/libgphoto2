618c0e9633859cd7e30ebd4bc6b5e27fc752456e
Marcus Meissner <marcus@jet.franken.de>
2011-02-24 21:39:30 +0000

    more debug
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_4/libgphoto2@13535 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/usb/libusb.c
+++ libgphoto2_port/usb/libusb.c
@@ -1034,6 +1034,10 @@ gp_port_usb_find_device_by_class_lib(GPPort *port, int class, int subclass, int 
 
 			if ((devname[0] != '\0') && strcmp(devname, dev->filename))
 				continue;
+			gp_log (GP_LOG_VERBOSE, "gphoto2-port-usb",
+				"Looking for USB device "
+				"(class 0x%x, subclass, 0x%x, protocol 0x%x)...", 
+				class, subclass, protocol);
 
 			ret = gp_port_usb_match_device_by_class(dev, class, subclass, protocol, &config, &interface, &altsetting);
 			if (!ret)
@@ -1041,8 +1045,8 @@ gp_port_usb_find_device_by_class_lib(GPPort *port, int class, int subclass, int 
 
 			port->pl->d = dev;
 			gp_log (GP_LOG_VERBOSE, "gphoto2-port-usb",
-				"Looking for USB device "
-				"(class 0x%x, subclass, 0x%x, protocol 0x%x)... found.", 
+				"Found USB class device "
+				"(class 0x%x, subclass, 0x%x, protocol 0x%x).", 
 				class, subclass, protocol);
 			/* Set the defaults */
 			if (dev->config) {
