430ebfba55b411fe3f5cc227d1e06b883784f5f7
Marcus Meissner <marcus@jet.franken.de>
2013-06-28 06:39:29 +0000

    one more free in error path (Coverity)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14464 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/ptpip.c
+++ camlibs/ptp2/ptpip.c
@@ -457,6 +457,7 @@ ptp_ptpip_init_command_ack (PTPParams* params)
 		return ret;
 	if (hdr.type != dtoh32(PTPIP_INIT_COMMAND_ACK)) {
 		gp_log (GP_LOG_ERROR, "ptpip/init_cmd_ack", "bad type returned %d", htod32(hdr.type));
+		free (data);
 		return PTP_RC_GeneralError;
 	}
 	params->eventpipeid = dtoh32a(&data[ptpip_cmdack_idx]);
