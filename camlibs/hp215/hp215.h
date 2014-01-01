f89703121735c61aa6611d77b7812a2bde79a51d
Marcus Meissner <marcus@jet.franken.de>
2014-01-01 20:27:33 +0000

    updated fsf address
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14608 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/hp215/hp215.h
+++ camlibs/hp215/hp215.h
@@ -18,8 +18,9 @@
  *                                                                           *
  *  You should have received a copy of the GNU Library General Public        *
  *  License along with this program; see the file COPYING.LIB.  If not,      *
- *  write to the Free Software Foundation, Inc., 59 Temple Place -           *
- *  Suite 330,  Boston, MA 02111-1307, USA.                                  *
+ *  write to the 							     *
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,         *
+ *  Boston, MA  02110-1301  USA						     *
  *****************************************************************************/
 
 #define STX 0x02
--- camlibs/konica/qm150.c
+++ camlibs/konica/qm150.c
@@ -15,8 +15,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
  *
  * Modified by Aurélien Croc (AP²C) <programming@ap2c.com>
  * In particular : fix some bugs, and implementation of advanced 
