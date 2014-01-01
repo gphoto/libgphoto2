541934944c89a2336301669714696dc26f6a4c99
Marcus Meissner <marcus@jet.franken.de>
2014-01-01 20:33:08 +0000

    updated fsf address
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14610 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2/gphoto2-widget.c
+++ libgphoto2/gphoto2-widget.c
@@ -17,8 +17,8 @@
  * \par
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
  */
 #include "config.h"
 #include <gphoto2/gphoto2-widget.h>
