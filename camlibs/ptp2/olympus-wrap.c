e25500eb77bce5e27fcf858ca6057af61791a531
Marcus Meissner <marcus@jet.franken.de>
2014-01-05 22:06:48 +0000

    free(x) (Coverity)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14645 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/olympus-wrap.c
+++ camlibs/ptp2/olympus-wrap.c
@@ -590,6 +590,7 @@ parse_9581_tree (xmlNodePtr node) {
 
 
 			next = xmlNextElementSibling (next);
+			free (x);
 			continue;
 		}
 		gp_log (GP_LOG_ERROR, "olympus","9581: unhandled node type %s", next->name);
