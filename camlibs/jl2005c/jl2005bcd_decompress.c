11fc7216da55b719369e0b223ee9d035d2570788
Marcus Meissner <marcus@jet.franken.de>
2014-02-22 17:19:04 +0000

    replaced nthos with generic call
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14776 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/jl2005c/jl2005bcd_decompress.c
+++ camlibs/jl2005c/jl2005bcd_decompress.c
@@ -127,7 +127,7 @@ jl2005bcd_decompress (unsigned char *output, unsigned char *input,
 			thumb = input + 16;
 			for (i = 0; i < thumbnail_width * thumbnail_height;
 									i++) {
-				thumb[i] = ntohs(thumb[i]);
+				thumb[i] = be16toh(thumb[i]);
 				out[i * 3 + 0] = (thumb[i] & 0xf800) >> 8;
 				out[i * 3 + 1] = (thumb[i] & 0x07e0) >> 3;
 				out[i * 3 + 2] = (thumb[i] & 0x001f) << 3;
