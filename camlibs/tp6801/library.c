e2f9171eed9488ea867eb0b3847a71523e9c3a1c
Marcus Meissner <marcus@jet.franken.de>
2014-02-23 08:32:48 +0000

    use localtime, if localtime_r is not available (porting)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@14790 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/tp6801/library.c
+++ camlibs/tp6801/library.c
@@ -517,12 +517,19 @@ camera_init (Camera *camera, GPContext *context)
 	}
 
 	if (camera->pl->syncdatetime) {
+#ifdef HAVE_LOCALTIME_R
 		struct tm tm;
+#endif
+		struct tm *xtm;
 		time_t t;
 
 		t = time (NULL);
-		if (localtime_r (&t , &tm)) {
-			ret = tp6801_set_time_and_date (camera, &tm);
+#ifdef HAVE_LOCALTIME_R
+		if ((xtm = localtime_r (&t , &tm))) {
+#else
+		if ((xtm = localtime (&t))) {
+#endif
+			ret = tp6801_set_time_and_date (camera, xtm);
 			if (ret != GP_OK) {
 				camera_exit (camera, context);
 				return ret;
