cd4a3039652ce31a3c6aac497f3db9bcd79edea1
Marcus Meissner <marcus@jet.franken.de>
2014-06-27 05:26:32 +0000

    converted commenttext to ascii
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@15026 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgphoto2_port/gphoto2/gphoto2-port-library.h
+++ libgphoto2_port/gphoto2/gphoto2-port-library.h
@@ -1,6 +1,6 @@
 /** \file gphoto2-port-library.h
  *
- * \author Copyright 2001 Lutz Müller <lutz@users.sf.net>
+ * \author Copyright 2001 Lutz Mueller <lutz@users.sf.net>
  *
  * \par License
  * This library is free software; you can redistribute it and/or
