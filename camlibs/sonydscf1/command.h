736b4e5b8fa648ddd40815e066cf9cc5fb03f65e
Marcus Meissner <marcus@jet.franken.de>
2015-02-22 09:49:23 +0000

    added copyright headers
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@15368 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sonydscf1/command.h
+++ camlibs/sonydscf1/command.h
@@ -1,3 +1,26 @@
+/* command.h
+ *
+ * Copyright (C) M. Adam Kendall <joker@penguinpub.com>
+ * Copyright (C) 2002 Bart van Leeuwen <bart@netage.nl>
+ *
+ * Based on the chotplay CLI interface from Ken-ichi Hayashi 1996,1997
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 #ifndef __COMMAND_H__
 #define __COMMAND_H__
 
