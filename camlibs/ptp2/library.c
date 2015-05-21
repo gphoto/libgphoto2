e1a83d01fcea1717c0dc2bddac6a64325002cb6b
Marcus Meissner <marcus@jet.franken.de>
2015-05-21 06:57:53 +0000

    added EOS M3 id
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2@15443 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1670,6 +1670,9 @@ static struct {
 	/* Reed Johnson <rmjohns1@gmail.com> */
 	{"Canon:PowerShot Elph340HS",		0x04a9, 0x3289, PTPBUG_DELETE_SENDS_EVENT},
 
+	/* Nykhedimus S <nykhedimus@gmail.com> */
+	{"Canon:EOS M3",			0x04a9, 0x3299, PTPBUG_DELETE_SENDS_EVENT},
+
 	/* Konica-Minolta PTP cameras */
 	{"Konica-Minolta:DiMAGE A2 (PTP mode)",        0x132b, 0x0001, 0},
 	{"Konica-Minolta:DiMAGE Z2 (PictBridge mode)", 0x132b, 0x0007, 0},
