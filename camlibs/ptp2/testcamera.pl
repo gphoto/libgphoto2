76a28c874efc9fc16c5345b2290e6c8fab70369e
Marcus Meissner <marcus@jet.franken.de>
2015-10-18 16:09:22 +0200

    autodetect features and variables handle formats

--- camlibs/ptp2/testcamera.pl
+++ camlibs/ptp2/testcamera.pl
@@ -1,33 +1,45 @@
 #!/usr/bin/perl -w
 use strict;
 
+$ENV{'LANG'} = 'C';
+
 my $gphoto2 = "gphoto2";
 
-my %camera = ();
+my %eos100d = ();
 
-
-$camera{'havecapture'} 		= 1;
-$camera{'havecapturetarget'} 	= 1;
-$camera{'havecapturetarget'} 	= 1;
 my %formats = (
 	"jpg" => "imageformat=0",
 	"raw" => "imageformat=9",
 	"both" => "imageformat=8",
 );
+$eos100d{'imageformats'} = \%formats;
 
-$camera{'imageformats'} = \%formats;
+my %camera = %eos100d;
 
+##################################################
+
+# auto configured
+my $havecapture = 0;
+my $havetriggercapture = 0;
+my $havepreview = 0;
+my $havecapturetarget = 0;
+
+my @lastresult = ();
 # returns TRUE on success, FALSE on fail
 sub run_gphoto2(@) {
 	my @cmdline = @_;
 
+	@lastresult = ();
 	print STDERR "running: " . join(" ",$gphoto2,@cmdline) . "\n";
 
 	print LOGFILE "running: " . join(" ",$gphoto2,@cmdline) . "\n";
 	open(GPHOTO,'-|',$gphoto2,@cmdline)||die "open $gphoto2";
 	while (<GPHOTO>) {
+		push @lastresult, $_;
 		print;
 		print LOGFILE;
+		chomp;
+		push @lastresult, $_;
 	}
 	my $rc = close(GPHOTO);
 	print LOGFILE "returned: $rc\n";
@@ -70,7 +82,8 @@ sub run_gphoto2_capture($$@) {
 	my ($nrimages,$text,@cmd) = @_;
 	my @newcmd = @cmd;
 
-	if ($camera{'havecapturetarget'}) {
+	print "run_gphoto2_capture: havecapturetarget $havecapturetarget\n";
+	if ($havecapturetarget) {
 		my @newcmd = @cmd;
 		unshift @newcmd,"--set-config-index","capturetarget=0";
 		&run_gphoto2_capture_target($nrimages,$text,@newcmd);
@@ -96,19 +109,20 @@ sub run_gphoto2_capture_formats($$@) {
 			unshift @newcmd,"--set-config-index",$formats{$format};
 
 			if ($format eq "jpg") {
-				&run_gphoto2_capture_target($nrimages,$text,@newcmd);
+				&run_gphoto2_capture($nrimages,$text,@newcmd);
 			} elsif ($format eq "both") {
-				&run_gphoto2_capture_target($nrimages*2,$text,@newcmd);
+				&run_gphoto2_capture($nrimages*2,$text,@newcmd);
 				unshift @newcmd,"--keep-raw";
-				&run_gphoto2_capture_target($nrimages,$text,@newcmd);
+				&run_gphoto2_capture($nrimages,$text,@newcmd);
 			} elsif ($format eq "raw") {
-				&run_gphoto2_capture_target($nrimages,$text,@newcmd);
+				&run_gphoto2_capture($nrimages,$text,@newcmd);
 				unshift @newcmd,"--keep-raw";
-				&run_gphoto2_capture_target(0,$text,@newcmd);
+				&run_gphoto2_capture(0,$text,@newcmd);
 			}
 		}
+		&run_gphoto2("--set-config-index",$formats{'jpg'});
 	} else {
-		&run_gphoto2_capture_target($nrimages,$text,@cmd);
+		&run_gphoto2_capture($nrimages,$text,@cmd);
 	}
 }
 
@@ -127,13 +141,46 @@ if (!run_gphoto2("-L")) {
 	die "-L does not work, reason see above\n";
 }
 
+# Basics
 ok(run_gphoto2("-L"),"testing -L");
 ok(run_gphoto2("-l"),"testing -l");
+
+# Autodetect capabilities: capture, preview or trigger capture
+ok(run_gphoto2("-a"),"testing -a");
+my @abilities = @lastresult;
+
+$havecapture = 1 if (grep (/Capture/,@abilities));
+$havetriggercapture = 1 if (grep (/Trigger Capture/,@abilities));
+$havepreview = 1 if (grep (/Preview/,@abilities));
+
 ok(run_gphoto2("--summary"),"testing --summary");
 ok(run_gphoto2("--list-all-config"),"testing --list-all-config");
-ok(run_gphoto2("--list-config"),"testing --list-config");
 
-if ($camera{'havecapture'}) {
+# Autodetect if some config variables are present
+
+ok(run_gphoto2("--list-config"),"testing --list-config");
+my @allconfig = @lastresult;
+
+$havecapturetarget = 1 if (grep(/capturetarget/,@allconfig));
+
+my $havedatetime = 1 if (grep(/datetime/,@allconfig));
+if ($havedatetime) {
+	ok(run_gphoto2("--get-config","datetime"),"testing --get-config datetime before setting");
+	ok(run_gphoto2("--set-config","datetime=now"),"testing --set-config datetime=now");
+	ok(run_gphoto2("--get-config","datetime"),"testing --get-config datetime after setting");
+}
+
+# Capture stuff lots of it
+
+if ($havecapture) {
 	run_gphoto2_capture_formats(1,"simple capture and download","--capture-image-and-download");
-	run_gphoto2_capture_formats(5,"simple capture and download","--capture-image-and-download","-F 5","-I 3");
+	run_gphoto2_capture_formats(3,"multiframe capture and download","--capture-image-and-download","-F 3","-I 2");
+	run_gphoto2_capture_formats(1,"waitevent","--wait-event-and-download=10s");
+}
+if ($havetriggercapture) {
+	run_gphoto2_capture_formats(1,"trigger capture","--trigger-capture","--wait-event-and-download=5s");
+}
+if ($havepreview) {
+	ok(run_gphoto2("--capture-preview"),"testing --capture-preview");
+	remove_all_files();
 }
