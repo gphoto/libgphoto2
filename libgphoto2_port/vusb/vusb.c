d46dd69d7d22e260455a54019777fade12ff5a64
Marcus Meissner <marcus@jet.franken.de>
2015-12-07 15:08:17 +0100

    debug functions. track open state

--- libgphoto2_port/vusb/vusb.c
+++ libgphoto2_port/vusb/vusb.c
@@ -61,11 +61,13 @@
 #define CHECK(result) {int r=(result); if (r<0) return (r);}
 
 struct _GPPortPrivateLibrary {
+	int	isopen;
 };
 
 GPPortType
 gp_port_library_type (void)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_PORT_USB;
 }
 
@@ -74,23 +76,18 @@ gp_port_library_list (GPPortInfoList *list)
 {
 	GPPortInfo info;
 
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	gp_port_info_new (&info);
 	gp_port_info_set_type (info, GP_PORT_USB);
-	gp_port_info_set_name (info, "usb:001,001");
-			
-	CHECK (gp_port_info_list_append (list, info));
-
-	/* generic usb:/xxx/ matcher */
-	gp_port_info_new (&info);
-	gp_port_info_set_type (info, GP_PORT_USB);
-	gp_port_info_set_name (info, "");
-	gp_port_info_set_path (info, "^usb:");
+	gp_port_info_set_name (info, "Universal Serial Bus");
+	gp_port_info_set_path (info, "usb:001,001");
 	CHECK (gp_port_info_list_append (list, info));
 	return GP_OK;
 }
 
 static int gp_port_vusb_init (GPPort *dev)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	C_MEM (dev->pl = calloc (1, sizeof (GPPortPrivateLibrary)));
 
 	return GP_OK;
@@ -99,6 +96,7 @@ static int gp_port_vusb_init (GPPort *dev)
 static int
 gp_port_vusb_exit (GPPort *port)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	free (port->pl);
 	port->pl = NULL;
 
@@ -108,30 +106,41 @@ gp_port_vusb_exit (GPPort *port)
 static int
 gp_port_vusb_open (GPPort *port)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
+	if (port->pl->isopen)
+		return GP_ERROR;
+	port->pl->isopen = 1;
 	return GP_OK;
 }
 
 static int
 gp_port_vusb_close (GPPort *port)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
+	if (!port->pl->isopen)
+		return GP_ERROR;
+	port->pl->isopen = 0;
 	return GP_OK;
 }
 
 static int
 gp_port_vusb_write (GPPort *port, const char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_OK;
 }
 
 static int
 gp_port_vusb_read(GPPort *port, char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_OK;
 }
 
 static int
 gp_port_vusb_reset(GPPort *port)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         C_PARAMS (port && port->pl);
 
         return GP_OK;
@@ -140,6 +149,7 @@ gp_port_vusb_reset(GPPort *port)
 static int
 gp_port_vusb_check_int (GPPort *port, char *bytes, int size, int timeout)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         C_PARAMS (port && port->pl && timeout >= 0);
 
         return GP_ERROR_TIMEOUT;
@@ -148,6 +158,7 @@ gp_port_vusb_check_int (GPPort *port, char *bytes, int size, int timeout)
 static int
 gp_port_vusb_update (GPPort *port)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	return GP_OK;
 }
 
@@ -156,6 +167,7 @@ gp_port_vusb_clear_halt_lib(GPPort *port, int ep)
 {
         unsigned char internal_ep;
 
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         C_PARAMS (port && port->pl);
 
         switch (ep) {
@@ -184,6 +196,7 @@ static int
 gp_port_vusb_msg_interface_write_lib(GPPort *port, int request, 
         int value, int index, char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_OK;
 }
 
@@ -192,6 +205,7 @@ static int
 gp_port_vusb_msg_interface_read_lib(GPPort *port, int request, 
         int value, int index, char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_OK;	/* or bytes */
 }
 
@@ -203,6 +217,7 @@ static int
 gp_port_vusb_msg_class_write_lib(GPPort *port, int request, 
         int value, int index, char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	return GP_OK;
 }
 
@@ -212,6 +227,7 @@ static int
 gp_port_vusb_msg_class_read_lib(GPPort *port, int request, 
         int value, int index, char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
         return GP_OK;	/* or bytes */
 }
 
@@ -220,6 +236,7 @@ static int
 gp_port_vusb_msg_write_lib(GPPort *port, int request, int value, int index,
         char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	return GP_OK;
 }
 
@@ -227,18 +244,21 @@ static int
 gp_port_vusb_msg_read_lib(GPPort *port, int request, int value, int index,
         char *bytes, int size)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	return GP_OK; /* bytes */
 }
 
 static int
 gp_port_vusb_find_device_lib(GPPort *port, int idvendor, int idproduct)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"(0x%04x,0x%04x)", idvendor, idproduct);
         return GP_ERROR_IO_USB_FIND;
 }
 
 static int
 gp_port_vusb_find_device_by_class_lib(GPPort *port, int class, int subclass, int protocol)
 {
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"(0x%02x,0x%02x,0x%02x)", class, subclass, protocol);
         return GP_ERROR_IO_USB_FIND;
 }
 
@@ -249,6 +269,7 @@ gp_port_library_operations (void)
 {
 	GPPortOperations *ops;
 
+	gp_log(GP_LOG_DEBUG,__FUNCTION__,"()");
 	ops = calloc (1, sizeof (GPPortOperations));
 	if (!ops)
 		return NULL;
