3ad7500fe02c89348bb139f25e93dcf213c4de91
Marcus Meissner <marcus@jet.franken.de>
2016-01-03 19:35:19 +0100

    remove sys/param.h and dirent.h use, not needed

--- libgphoto2_port/libusb1/libusb1.c
+++ libgphoto2_port/libusb1/libusb1.c
@@ -24,13 +24,10 @@
 #include <gphoto2/gphoto2-port-library.h>
 
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/time.h>
-#include <sys/param.h>
-#include <dirent.h>
 #include <string.h>
 
 #include <libusb.h>
