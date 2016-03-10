11360bfa1f31c402e08a128e2590f22952978650
Marcus Meissner <marcus@jet.franken.de>
2016-03-10 08:19:43 +0100

    added ixus 160 id

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1804,6 +1804,9 @@ static struct {
 	/* Kiss Tamas <kisst@bgk.bme.hu> */
 	{"Canon:IXUS 165",			0x04a9, 0x32a9, PTPBUG_DELETE_SENDS_EVENT},
 
+	/* Gong <elainegong2010@gmail.com> */
+	{"Canon:IXUS 160",			0x04a9, 0x32aa, PTPBUG_DELETE_SENDS_EVENT},
+
 	/* Johannes Goecke <jg-ml@web.de> */
 	{"Canon:EOS 750D",			0x04a9, 0x32a1, PTP_CAP|PTP_CAP_PREVIEW},
 
