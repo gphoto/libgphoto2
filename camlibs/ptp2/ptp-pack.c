d6ba3cc234e01ab8c64f0a8a76428ba97b634349
Marcus Meissner <marcus@jet.franken.de>
2016-11-02 23:16:52 +0100

    return strings from customfuncex decoder even on failure (AFL)

--- camlibs/ptp2/ptp-pack.c
+++ camlibs/ptp2/ptp-pack.c
@@ -1723,20 +1723,19 @@ ptp_unpack_EOS_CustomFuncEx (PTPParams* params, unsigned char** data )
 {
 	uint32_t s = dtoh32a( *data );
 	uint32_t n = s/4, i;
-	char* str;
+	char	*str, *p;
 
 	if (s > 1024) {
 		ptp_debug (params, "customfuncex data is larger than 1k / %d... unexpected?", s);
-		return NULL;
+		return strdup("bad length");
 	}
 	str = (char*)malloc( s*2+s/4+1 ); /* n is size in uint32, maximum %x len is 8 chars and \0*/
 	if (!str)
-		return str;
-	char* p = str;
+		return strdup("malloc failed");
 
+	p = str;
 	for (i=0; i < n; ++i)
 		p += sprintf(p, "%x,", dtoh32a( *data + 4*i ));
-
 	return str;
 }
 
