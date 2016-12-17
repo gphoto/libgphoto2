6f84fc2862eda6ef411bf50b82d1de10ed883003
Marcus Meissner <marcus@jet.franken.de>
2016-12-17 11:15:14 +0100

    added nikon aw130 fixes https://github.com/gphoto/libgphoto2/issues/116

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1077,6 +1077,9 @@ static struct {
 	/* LeChuck <ofernandez84@gmail.com> */
 	{"Nikon:Coolpix AW110 (PTP mode)",0x04b0, 0x0194, PTP_CAP|PTP_NIKON_BROKEN_CAP},
 
+	/* https://github.com/gphoto/libgphoto2/issues/116 */
+	{"Nikon:Coolpix AW130 (PTP mode)",0x04b0, 0x0198, PTP_CAP|PTP_NIKON_BROKEN_CAP},
+
 	{"Nikon:Coolpix SQ (PTP mode)",   0x04b0, 0x0202, 0},
 	/* lars marowski bree, 16.8.2004 */
 	{"Nikon:Coolpix 4200 (PTP mode)", 0x04b0, 0x0204, 0},
