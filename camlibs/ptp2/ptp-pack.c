213812eb0b5f2a9be937e08ac9f140b4da39c8c3
Marcus Meissner <marcus@jet.franken.de>
2017-01-05 10:42:59 +0100

    initialize storage id array to NULL in all cases (AFL)

--- camlibs/ptp2/ptp-pack.c
+++ camlibs/ptp2/ptp-pack.c
@@ -503,11 +503,12 @@ ptp_unpack_OH (PTPParams *params, unsigned char* data, PTPObjectHandles *oh, uns
 static inline void
 ptp_unpack_SIDs (PTPParams *params, unsigned char* data, PTPStorageIDs *sids, unsigned int len)
 {
-	if (!data || !len) {
-		sids->n = 0;
-		sids->Storage = NULL;
+	sids->n = 0;
+	sids->Storage = NULL;
+
+	if (!data || !len)
 		return;
-        }
+
 	sids->n = ptp_unpack_uint32_t_array(params, data, PTP_sids, len, &sids->Storage);
 }
 
