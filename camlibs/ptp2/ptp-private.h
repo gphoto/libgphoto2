2bbe8d4d218417daccbd1c6b53bd9069c09202f0
Marcus Meissner <marcus@jet.franken.de>
2017-04-17 11:45:15 +0200

    check for EOS M to avoid confusing it with EOS 5D Mark x

--- camlibs/ptp2/ptp-private.h
+++ camlibs/ptp2/ptp-private.h
@@ -113,7 +113,7 @@ static inline int
 is_canon_eos_m(PTPParams *params) {
 	if (params->deviceinfo.VendorExtensionID != PTP_VENDOR_CANON) return 0;
 	if (!ptp_operation_issupported(params, PTP_OC_CANON_EOS_SetRemoteMode)) return 0;
-	return strchr(params->deviceinfo.Model, 'M') != NULL;
+	return strstr(params->deviceinfo.Model, "EOS M") != NULL;
 }
 
 
