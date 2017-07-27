f1c69dfe89f0acbd2cdc488cbb9b21e9ccf4ce39
Marcus Meissner <marcus@jet.franken.de>
2017-07-27 13:35:39 +0200

    0 terminate string field that is later strduped (AFL)

--- camlibs/ptp2/ptp-pack.c
+++ camlibs/ptp2/ptp-pack.c
@@ -1555,6 +1555,7 @@ ptp_unpack_Canon_EOS_FE (PTPParams *params, unsigned char* data, unsigned int si
 	fe->Time=(time_t)dtoh32a(&data[PTP_cefe_Time]);
 	for (i=0; i<PTP_CANON_FilenameBufferLen; i++)
 		fe->Filename[i]=(char)data[PTP_cefe_Filename+i];
+	fe->Filename[PTP_CANON_FilenameBufferLen-1] = 0;
 }
 
 
