gphoto/libgphoto2

commit 17a78e203800c448773d80d01e17e79c69326b90

On device property unpacking, return 0 from unpack_string is a FAILURE (AFL) on unpacking strings, if we do not have enough data to read, length is 0 (AFL)

camlibs/ptp2/ptp-pack.c
@@ -140,12 +140,13 @@ ptp_unpack_string(PTPParams *params, unsigned char* data, uint16_t offset, uint3
 		return NULL;
 
 	length = dtoh8a(&data[offset]);	/* PTP_MAXSTRLEN == 255, 8 bit len */
-	*len = length;
 	if (length == 0)		/* nothing to do? */
 		return NULL;
 
 	if (offset + 1 + length*sizeof(string[0]) > total)
 		return NULL;
+
+	*len = length;
 
 	/* copy to string[] to ensure correct alignment for iconv(3) */
 	memcpy(string, &data[offset+1], length * sizeof(string[0]));
@@ -840,9 +841,9 @@ ptp_unpack_DPV (
 			return 0;
 
 		value->str = ptp_unpack_string(params,data,*offset,total,&len);
-		*offset += len*2+1;
 		if (!value->str)
-			return 1;
+			return 0;
+		*offset += len*2+1;
 		break;
 	}
 	default:

commit d8f6ec8c1a03572f638cfca44a2ee48cccb8c465

set *len = 0 on the "empty string" case.
fixed regression introduced by commit 17a78e203800c448773d80d01e17e79c69326b90

camlibs/ptp2/ptp-pack.c
@@ -136,12 +136,16 @@ ptp_unpack_string(PTPParams *params, unsigned char* data, uint16_t offset, uint3
 	size_t nconv, srclen, destlen;
 	char *src, *dest;
 
+	*len = 0;
+
 	if (offset + 1 >= total)
 		return NULL;
 
 	length = dtoh8a(&data[offset]);	/* PTP_MAXSTRLEN == 255, 8 bit len */
-	if (length == 0)		/* nothing to do? */
+	if (length == 0) {		/* nothing to do? */
+		*len = 0;
 		return NULL;
+	}
 
 	if (offset + 1 + length*sizeof(string[0]) > total)
 		return NULL;