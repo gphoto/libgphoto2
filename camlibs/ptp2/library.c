c3d640cdbd78cda70c4aa40621db3b4b1523c498
Marcus Meissner <marcus@jet.franken.de>
2018-11-11 19:45:23 +0100

    added nikon z7

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1422,6 +1422,9 @@ static struct {
 	/* Andre Crone <visuals@elysia.nl> */
 	{"Nikon:DSC D850",                0x04b0, 0x0441, PTP_CAP|PTP_CAP_PREVIEW},
 
+	/* Horshack ?? <horshack@live.com> */
+	{"Nikon:Z7",                	  0x04b0, 0x0442, PTP_CAP|PTP_CAP_PREVIEW},
+
 	/* http://sourceforge.net/tracker/?func=detail&aid=3536904&group_id=8874&atid=108874 */
 	{"Nikon:V1",    		  0x04b0, 0x0601, PTP_CAP|PTP_NIKON_1},
 	/* https://sourceforge.net/tracker/?func=detail&atid=358874&aid=3556403&group_id=8874 */
