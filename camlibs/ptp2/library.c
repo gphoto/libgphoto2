e060a0fdfa0a5723dd052579419ac1b2d9a57b34
SparkyCola <m4ttis@riseup.net>
2019-03-03 11:53:39 +0100

    added don't close session and verified PID

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -2038,8 +2038,8 @@ static struct {
 	/* pravsripad@gmail.com */
 	{"Canon:PowerShot SX520 HS",		0x04a9, 0x329b, PTPBUG_DELETE_SENDS_EVENT},
 
-    /* sparkycoladev@gmail.com, PID from CHDK Wiki at http://chdk.wikia.com/wiki/P-ID_(Table) */
-    {"Canon:PowerShot G7 X",			0x04a9, 0x329d, PTP_CAP|PTP_CAP_PREVIEW},
+    /* sparkycoladev@gmail.com */
+    {"Canon:PowerShot G7 X",			0x04a9, 0x329d, PTP_CAP|PTP_CAP_PREVIEW|PTP_DONT_CLOSE_SESSION},
 
 	/* Marcus Meissner <marcus@jet.franken.de> */
 	{"Canon:EOS M10",			0x04a9, 0x32a0, PTP_CAP|PTP_CAP_PREVIEW},
