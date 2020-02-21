8150a9ecb7999f0ebf5a437d3b3f94c356c32b28
Marcus Meissner <marcus@jet.franken.de>
2020-02-21 10:41:31 +0100

    added SX730IS

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -2180,6 +2180,9 @@ static struct {
 	/* Marcus Meissner */
 	{"Canon:Digital IXUS 185",          	0x04a9, 0x32d4, 0},
 
+	/* Slavko Kocjancic <eslavko@gmail.com> */
+	{"Canon:Digital PowerShot SX730IS",	0x04a9, 0x32d6, PTP_CAP|PTP_CAP_PREVIEW},
+
 	/* Jasem Mutlaq <mutlaqja@ikarustech.com> */
 	{"Canon:EOS 4000D",			0x04a9, 0x32d9, PTP_CAP|PTP_CAP_PREVIEW|PTPBUG_DELETE_SENDS_EVENT},
 
