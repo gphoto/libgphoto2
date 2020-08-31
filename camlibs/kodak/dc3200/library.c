b73d3eb114679906082d7ab4fab931c1e8006ef8
Marcus Meissner <marcus@jet.franken.de>
2020-08-31 12:28:52 +0200

    simplified weird safe string constructs that used wrong sizes, to shorter versions

--- camlibs/kodak/dc3200/library.c
+++ camlibs/kodak/dc3200/library.c
@@ -241,20 +241,16 @@ int dc3200_get_data(Camera *camera, unsigned char **data, unsigned long *data_le
 				return GP_ERROR;
 
 			/* concatenate the folder + filename */
-			strncpy(file, folder, strlen(folder));
-			file[strlen(folder)] = 0;
+			strcpy(file, folder);
 			/* append the filename */
 			if(folder[strlen(folder)-1] != '\\')
-			strcat(file, "\\");
-			strncat(file, filename, strlen(filename));
+				strcat(file, "\\");
+			strcat(file, filename);
 
 		} else {
-			file = malloc(strlen(folder) + 2);
+			file = strdup(folder);
 			if(!file)
 				return GP_ERROR;
-
-			strncpy(file, folder, strlen(folder));
-			file[strlen(folder)] = 0;
 		}
 	} else {
 		return GP_ERROR;
