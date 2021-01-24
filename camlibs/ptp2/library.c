32559bc9c061387f08f9e979e58a0a3e261f1574
Marcus Meissner <marcus@jet.franken.de>
2021-01-24 08:35:12 +0100

    added canon eos m200 fixes https://github.com/gphoto/libgphoto2/issues/614

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -2416,6 +2416,8 @@ static struct {
 	{"Canon:EOS 90D",			0x04a9, 0x32ea, PTP_CAP|PTP_CAP_PREVIEW},
 	/* https://github.com/gphoto/gphoto2/issues/347 */
 	{"Canon:PowerShot SX70 HS",		0x04a9, 0x32ee, PTP_CAP|PTP_CAP_PREVIEW},
+	/* https://github.com/gphoto/libgphoto2/issues/614 */
+	{"Canon:EOS M200",			0x04a9, 0x32ef, PTP_CAP|PTP_CAP_PREVIEW},
 	/* from timelapse-view */
 	{"Canon:EOS R5",			0x04a9, 0x32f4, PTP_CAP|PTP_CAP_PREVIEW},
 	/* Steve Rencontre <steve@rsn-tech.co.uk> */
