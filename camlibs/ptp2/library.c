dd51cc9d5b149053357c2ea20ca201e45a726501
Marcus Meissner <marcus@jet.franken.de>
2021-04-19 16:30:41 +0200

    added eos m50m2 https://github.com/gphoto/libgphoto2/issues/642

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -2448,6 +2448,8 @@ static struct {
 	{"Canon:EOS R5",			0x04a9, 0x32f4, PTP_CAP|PTP_CAP_PREVIEW},
 	/* Steve Rencontre <steve@rsn-tech.co.uk> */
 	{"Canon:EOS R6",			0x04a9, 0x32f5, PTP_CAP|PTP_CAP_PREVIEW},
+	/* https://github.com/gphoto/libgphoto2/issues/642 */
+	{"Canon:EOS M50m2",			0x04a9, 0x32f9, PTP_CAP|PTP_CAP_PREVIEW},
 
 	/* Konica-Minolta PTP cameras */
 	{"Konica-Minolta:DiMAGE A2 (PTP mode)",        0x132b, 0x0001, 0},
