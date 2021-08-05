34df530471fec349d1200b82a8082963ae001b99
Jaroslav Škarvada <jskarvad@redhat.com>
2021-08-05 23:13:01 +0200

    Fixed build in mingw
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- camlibs/lumix/lumix.c
+++ camlibs/lumix/lumix.c
@@ -36,10 +36,15 @@
 #include <libxml/xmlreader.h>
 
 
-#include <sys/socket.h>
+#ifdef WIN32
+# include <winsock2.h>
+# include <ws2tcpip.h>
+#else
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+#endif
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
 
 #include <gphoto2/gphoto2-library.h>
