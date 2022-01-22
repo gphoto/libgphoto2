919671c89bb9c875bdf5cdcc31e9f3e1b4214a10
Hans Ulrich Niedermann <hun@n-dimensional.de>
2022-01-22 12:02:00 +0100

    CI: build automake based example project
    Add automake based example project using libgphoto2 to have
    the CI builds ensure that building a libgphoto2 frontend
    with this libgphoto2 is possible.

---
+++ examples/ambs-lgp2-frontend/README.md
@@ -0,0 +1,11 @@
+automake buildsystem based example libgphoto2 frontend
+====================================================
+
+This is an example libgphoto2 frontend project using an automake base
+build system.
+
+Its main purpose is to allow libgphoto2 CI builds to test building a
+libgphoto2 frontend against the built libgphoto2 without needing to
+clone and build the complete gphoto2 command line interface frontend.
+
+Additionally, it may serve as a bit of documentation by example.
---
+++ examples/ambs-lgp2-frontend/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <gphoto2/gphoto2-version.h>
+
+/* The purpose of this code is to use things defined in gphoto2 header
+ * files and then link to libgphoto2. */
+
+static
+struct {
+  const char *const title;
+  GPVersionFunc version_func;
+} versions[] = {
+  {"libgphoto2_port", gp_port_library_version},
+  {"libgphoto2",      gp_library_version}
+};
+
+int main()
+{
+  for (size_t i=0; i<(sizeof(versions)/sizeof(versions[0])); i++) {
+    const char *const title          = versions[i].title;
+    const GPVersionFunc version_func = versions[i].version_func;
+    const char **version_data = version_func(GP_VERSION_VERBOSE);
+    printf("%s\n", title);
+    for (size_t j=0; version_data[j]; j++) {
+      printf("  %s\n", version_data[j]);
+    }
+  }
+  return 0;
+}
