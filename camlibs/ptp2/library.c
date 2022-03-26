543a5875b749626f976f8989150d3acc55eddde1
Sachi King <nakato@nakato.io>
2022-03-26 17:37:19 +1100

    Add Sony ILCE-7M4, Alpha 7 IV

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1303,6 +1303,10 @@ static struct {
 	{"Sony:Alpha-A7r III (PC Control)",	0x054c, 0x0c33, PTP_CAP|PTP_CAP_PREVIEW}, /* FIXME: crosscheck */
 	{"Sony:Alpha-A7 III (PC Control)",	0x054c, 0x0c34, PTP_CAP|PTP_CAP_PREVIEW}, /* FIXME: crosscheck */
 
+	/* https://github.com/gphoto/libgphoto2/pull/782 */
+	{"Sony:Alpha-A7 IV (MTP mode)",		0x054c, 0x0da6, 0},
+	{"Sony:Alpha-A7 IV (PC Control)",	0x054c, 0x0da7, PTP_CAP|PTP_CAP_PREVIEW},
+
 	/* jackden@gmail.com */
 	{"Sony:DSC-RX100M6 (PC Control)",  	0x054c, 0x0c38, PTP_CAP|PTP_CAP_PREVIEW},
 
