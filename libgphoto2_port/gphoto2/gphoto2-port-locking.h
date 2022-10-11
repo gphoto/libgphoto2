2881f602e0d81c2f2fdd0dd1e4f2aeef0663840a
Hans Ulrich Niedermann <hun@n-dimensional.de>
2022-10-11 19:35:45 +0200

    848: internal headers do not require __cplusplus checks

--- libgphoto2_port/gphoto2/gphoto2-port-locking.h
+++ libgphoto2_port/gphoto2/gphoto2-port-locking.h
@@ -1,10 +1,6 @@
 #ifndef LIBGPHOTO2_GPHOTO2_PORT_LOCKING_H
 #define LIBGPHOTO2_GPHOTO2_PORT_LOCKING_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #ifdef _GPHOTO2_INTERNAL_CODE
 
 /** lock libltdl before calling lt_*() (libltdl is not thread safe) */
@@ -14,9 +10,5 @@ extern void gpi_libltdl_lock(void);
 extern void gpi_libltdl_unlock(void);
 
 #endif /* defined(_GPHOTO2_INTERNAL_CODE) */
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
 
 #endif /* defined(LIBGPHOTO2_GPHOTO2_PORT_LOCKING_H) */
