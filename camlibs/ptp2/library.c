3042872b4bec393cfa0a0788fcea1f12a85f38cd
Marcus Meissner <marcus@jet.franken.de>
2023-02-19 14:55:46 +0100

    added sony ilce-1 , https://github.com/gphoto/libgphoto2/issues/892

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1343,6 +1343,9 @@ static struct {
 	/* Elijah Parker, mail@timelapseplus.com */
 	{"Sony:DSC-A7S III (Control)",		0x054c, 0x0d18, PTP_CAP|PTP_CAP_PREVIEW},
 
+	/* https://github.com/gphoto/libgphoto2/issues/892 */
+	{"Sony:ILCE-1 (Control)",		0x054c, 0x0d1c, PTP_CAP|PTP_CAP_PREVIEW},
+
 	/* via email */
 	{"Sony:ILCE-7C (Control)",		0x054c, 0x0d2b, PTP_CAP|PTP_CAP_PREVIEW},
 
