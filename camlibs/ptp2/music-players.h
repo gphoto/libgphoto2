b1ad31232cdb80e387837558fe764541914a0487
Marcus Meissner <marcus@jet.franken.de>
2023-08-17 09:53:15 +0200

    another mtp id

--- camlibs/ptp2/music-players.h
+++ camlibs/ptp2/music-players.h
@@ -3938,6 +3938,8 @@
   { "Garmin", 0x091e, "Forerunner 265", 0x50a1, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/152 */
   { "Garmin", 0x091e, "Forerunner 965", 0x50db, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1934/ */
+  { "Garmin", 0x091e, "Fenix 7s pro sapphire solar", 0x5116, DEVICE_FLAGS_ANDROID_BUGS },
 
   /*
    * Wacom
