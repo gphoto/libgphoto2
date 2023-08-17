3f6cd802da532725654fa53ed1539a39778e9653
Marcus Meissner <marcus@jet.franken.de>
2023-08-17 10:30:01 +0200

    year is 2023

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -3289,7 +3289,7 @@ camera_about (Camera *camera, CameraText *text, GPContext *context)
 	   "This driver supports cameras that support PTP or PictBridge(tm), and\n"
 	   "Media Players that support the Media Transfer Protocol (MTP).\n"
 	   "\n"
-	   "Enjoy!"), 2021);
+	   "Enjoy!"), 2023);
 	return (GP_OK);
 }
 
