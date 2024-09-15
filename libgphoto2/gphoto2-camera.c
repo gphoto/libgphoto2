gphoto/libgphoto2

commit 5c9d7b27599b16a744077e7a9ef7937d2132e1b1

fix white-space and indentation inconsistencies in ... (#1002)
* examples
 * gphoto2
 * libgphoto2
 * libgphoto2_port

The code (at least in this part of the project) is effectively formated
using tabs with a tab-size of 8 as a means for indentation.

There have been a few places where spaces were introduced. Some obviously
with an indent size of 8 in mind, others with an indent size of 4.

This commit tries its best to make the use of tabs consistent again, at
least for indenting lines. The "intra line" indentation (white space after
the first non-white-space character) is still a bit of a mess.

Co-authored-by: axxel <awagger@web.de>

libgphoto2/gphoto2-camera.c
@@ -308,16 +308,16 @@ gp_camera_new (Camera **camera)
 
 	C_PARAMS (camera);
 
-        C_MEM (*camera = calloc (1, sizeof (Camera)));
+	C_MEM (*camera = calloc (1, sizeof (Camera)));
 
-        (*camera)->functions = calloc (1, sizeof (CameraFunctions));
-        (*camera)->pc        = calloc (1, sizeof (CameraPrivateCore));
+	(*camera)->functions = calloc (1, sizeof (CameraFunctions));
+	(*camera)->pc        = calloc (1, sizeof (CameraPrivateCore));
 	if (!(*camera)->functions || !(*camera)->pc) {
 		result = GP_ERROR_NO_MEMORY;
 		goto error;
 	}
 
-        (*camera)->pc->ref_count = 1;
+	(*camera)->pc->ref_count = 1;
 
 	/* Create the filesystem */
 	result = gp_filesystem_new (&(*camera)->fs);
@@ -329,7 +329,7 @@ gp_camera_new (Camera **camera)
 	if (result < GP_OK)
 		goto error;
 
-        return(GP_OK);
+	return(GP_OK);
 
 error:
 	gp_camera_free (*camera);
@@ -625,11 +625,11 @@ gp_camera_autodetect (CameraList *list, GPContext *context)
 	if (ret < GP_OK) goto out;
 
 	/* ... and autodetect the currently attached cameras. */
-        ret = gp_abilities_list_detect (al, il, xlist, context);
+	ret = gp_abilities_list_detect (al, il, xlist, context);
 	if (ret < GP_OK) goto out;
 
 	/* Filter out the "usb:" entry */
-        ret = gp_list_count (xlist);
+	ret = gp_list_count (xlist);
 	if (ret < GP_OK) goto out;
 	for (i=0;i<ret;i++) {
 		const char *name, *value;
@@ -694,7 +694,7 @@ gp_camera_init (Camera *camera, GPContext *context)
 		char		*ppath;
 		GPPortType	ptype;
 		GPPortInfo	info;
-        	CameraList	*list;
+		CameraList	*list;
 
 		result = gp_list_new (&list);
 		if (result < GP_OK)
@@ -861,7 +861,7 @@ gp_camera_get_config (Camera *camera, CameraWidget **window, GPContext *context)
 		gp_context_error (context, _("This camera does "
 			"not provide any configuration options."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->get_config (
@@ -937,8 +937,8 @@ gp_camera_get_single_config (Camera *camera, const char *name, CameraWidget **wi
 	gp_widget_set_readonly (*widget, ro);
 
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO: {
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO: {
 		char *value;
 		int i, nrofchoices;
 
@@ -953,35 +953,35 @@ gp_camera_get_single_config (Camera *camera, const char *name, CameraWidget **wi
 		gp_widget_set_value (*widget, value);
 		break;
 	}
-        case GP_WIDGET_TEXT: {
+	case GP_WIDGET_TEXT: {
 		char *value;
 
 		gp_widget_get_value (child, &value);
 		gp_widget_set_value (*widget, value);
 		break;
 	}
-        case GP_WIDGET_RANGE: {
+	case GP_WIDGET_RANGE: {
 		float value, rmin, rmax, rstep;
 
 		gp_widget_get_range (child, &rmin, &rmax, &rstep);
 		gp_widget_set_range (*widget, rmin, rmax, rstep);
 		gp_widget_get_value (child, &value);
 		gp_widget_set_value (*widget, &value);
-                break;
+		break;
 	}
-        case GP_WIDGET_TOGGLE:
-        case GP_WIDGET_DATE: {
+	case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_DATE: {
 		int value;
 
 		gp_widget_get_value (child, &value);
 		gp_widget_set_value (*widget, &value);
-                break;
+		break;
 	}
-        case GP_WIDGET_BUTTON:
-        case GP_WIDGET_SECTION:
-        case GP_WIDGET_WINDOW:
-        default:
-                ret = GP_ERROR_BAD_PARAMETERS;
+	case GP_WIDGET_BUTTON:
+	case GP_WIDGET_SECTION:
+	case GP_WIDGET_WINDOW:
+	default:
+		ret = GP_ERROR_BAD_PARAMETERS;
 		break;
 	}
 out:
@@ -1011,20 +1011,20 @@ _get_widget_names (CameraWidget *widget, CameraList *list)
 
 	gp_widget_get_type (widget, &type);
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT:
-        case GP_WIDGET_RANGE:
-        case GP_WIDGET_TOGGLE:
-        case GP_WIDGET_DATE: {
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT:
+	GP_WIDGET_RANGE:
+	case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_DATE: {
 		const char *name;
 
 		gp_widget_get_name (widget, &name);
 		gp_list_append (list, name, NULL);
-                break;
+		break;
 	}
-        case GP_WIDGET_SECTION:
-        case GP_WIDGET_WINDOW: {
+	case GP_WIDGET_SECTION:
+	case GP_WIDGET_WINDOW: {
 		int i, nrofchildren;
 
 		nrofchildren = gp_widget_count_children (widget);
@@ -1036,8 +1036,8 @@ _get_widget_names (CameraWidget *widget, CameraList *list)
 		}
 		break;
 	}
-        case GP_WIDGET_BUTTON:
-        default:
+	case GP_WIDGET_BUTTON:
+	default:
 		break;
 	}
 
@@ -1105,7 +1105,7 @@ gp_camera_set_config (Camera *camera, CameraWidget *window, GPContext *context)
 		gp_context_error (context, _("This camera does "
 			"not support setting configuration options."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->set_config (camera,
@@ -1170,35 +1170,35 @@ gp_camera_set_single_config (Camera *camera, const char *name, CameraWidget *wid
 	gp_widget_get_type (child, &type);
 	ret = GP_OK;
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT: {
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT: {
 		char *value;
 
 		gp_widget_get_value (widget, &value);
 		gp_widget_set_value (child, value);
 		break;
 	}
-        case GP_WIDGET_RANGE: {
+	case GP_WIDGET_RANGE: {
 		float value;
 
 		gp_widget_get_value (widget, &value);
 		gp_widget_set_value (child, &value);
-                break;
+		break;
 	}
-        case GP_WIDGET_TOGGLE:
-        case GP_WIDGET_DATE: {
+	case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_DATE: {
 		int value;
 
 		gp_widget_get_value (widget, &value);
 		gp_widget_set_value (child, &value);
-                break;
+		break;
 	}
-        case GP_WIDGET_BUTTON:
-        case GP_WIDGET_SECTION:
-        case GP_WIDGET_WINDOW:
-        default:
-                ret = GP_ERROR_BAD_PARAMETERS;
+	case GP_WIDGET_BUTTON:
+	case GP_WIDGET_SECTION:
+	case GP_WIDGET_WINDOW:
+	default:
+		ret = GP_ERROR_BAD_PARAMETERS;
 		break;
 	}
 	gp_widget_set_changed (child, 1);
@@ -1234,7 +1234,7 @@ gp_camera_get_summary (Camera *camera, CameraText *summary, GPContext *context)
 		gp_context_error (context, _("This camera does "
 				  "not support summaries."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->summary (camera,
@@ -1265,7 +1265,7 @@ gp_camera_get_manual (Camera *camera, CameraText *manual, GPContext *context)
 		gp_context_error (context, _("This camera "
 			"does not provide a manual."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->manual (camera,
@@ -1297,7 +1297,7 @@ gp_camera_get_about (Camera *camera, CameraText *about, GPContext *context)
 		gp_context_error (context, _("This camera does "
 			"not provide information about the driver."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->about (camera,
@@ -1330,7 +1330,7 @@ gp_camera_capture (Camera *camera, CameraCaptureType type,
 	if (!camera->functions->capture) {
 		gp_context_error (context, _("This camera can not capture."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->capture (camera,
@@ -1360,7 +1360,7 @@ gp_camera_trigger_capture (Camera *camera, GPContext *context)
 	if (!camera->functions->trigger_capture) {
 		gp_context_error (context, _("This camera can not trigger capture."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->trigger_capture (camera,
 						context), context);
@@ -1394,7 +1394,7 @@ gp_camera_capture_preview (Camera *camera, CameraFile *file, GPContext *context)
 		gp_context_error (context, _("This camera can "
 			"not capture previews."));
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->capture_preview (
@@ -1433,7 +1433,7 @@ gp_camera_capture_preview (Camera *camera, CameraFile *file, GPContext *context)
  */
 int
 gp_camera_wait_for_event (Camera *camera, int timeout,
-		          CameraEventType *eventtype, void **eventdata,
+			  CameraEventType *eventtype, void **eventdata,
 			  GPContext *context)
 {
 	C_PARAMS (camera);
@@ -1441,7 +1441,7 @@ gp_camera_wait_for_event (Camera *camera, int timeout,
 
 	if (!camera->functions->wait_for_event) {
 		CAMERA_UNUSED (camera, context);
-                return (GP_ERROR_NOT_SUPPORTED);
+		return (GP_ERROR_NOT_SUPPORTED);
 	}
 	CHECK_RESULT_OPEN_CLOSE (camera, camera->functions->wait_for_event (
 					camera, timeout, eventtype, eventdata,
@@ -1475,7 +1475,7 @@ gp_camera_folder_list_files (Camera *camera, const char *folder,
 
 	CR (camera, gp_list_sort (list), context);
 	CAMERA_UNUSED (camera, context);
-        return (GP_OK);
+	return (GP_OK);
 }
 
 /**
@@ -1503,7 +1503,7 @@ gp_camera_folder_list_folders (Camera *camera, const char* folder,
 
 	CR (camera, gp_list_sort (list), context);
 	CAMERA_UNUSED (camera, context);
-        return (GP_OK);
+	return (GP_OK);
 }
 
 /**

examples/autodetect.c
@@ -17,7 +17,7 @@ static CameraAbilitiesList	*abilities = NULL;
 int
 sample_autodetect (CameraList *list, GPContext *context) {
 	gp_list_reset (list);
-        return gp_camera_autodetect (list, context);
+	return gp_camera_autodetect (list, context);
 }
 
 /*
@@ -41,11 +41,11 @@ sample_open_camera (Camera ** camera, const char *model, const char *port, GPCon
 	}
 
 	/* First lookup the model / driver */
-        m = gp_abilities_list_lookup_model (abilities, model);
+	m = gp_abilities_list_lookup_model (abilities, model);
 	if (m < GP_OK) return ret;
-        ret = gp_abilities_list_get_abilities (abilities, m, &a);
+	ret = gp_abilities_list_get_abilities (abilities, m, &a);
 	if (ret < GP_OK) return ret;
-        ret = gp_camera_set_abilities (*camera, a);
+	ret = gp_camera_set_abilities (*camera, a);
 	if (ret < GP_OK) return ret;
 
 	if (!portinfolist) {
@@ -59,25 +59,25 @@ sample_open_camera (Camera ** camera, const char *model, const char *port, GPCon
 	}
 
 	/* Then associate the camera with the specified port */
-        p = gp_port_info_list_lookup_path (portinfolist, port);
-        switch (p) {
-        case GP_ERROR_UNKNOWN_PORT:
-                fprintf (stderr, "The port you specified "
-                        "('%s') can not be found. Please "
-                        "specify one of the ports found by "
-                        "'gphoto2 --list-ports' and make "
-                        "sure the spelling is correct "
-                        "(i.e. with prefix 'serial:' or 'usb:').",
-                                port);
-                break;
-        default:
-                break;
-        }
-        if (p < GP_OK) return p;
+	p = gp_port_info_list_lookup_path (portinfolist, port);
+	switch (p) {
+	case GP_ERROR_UNKNOWN_PORT:
+		fprintf (stderr, "The port you specified "
+			"('%s') can not be found. Please "
+			"specify one of the ports found by "
+			"'gphoto2 --list-ports' and make "
+			"sure the spelling is correct "
+			"(i.e. with prefix 'serial:' or 'usb:').",
+				port);
+		break;
+	default:
+		break;
+	}
+	if (p < GP_OK) return p;
 
-        ret = gp_port_info_list_get_info (portinfolist, p, &pi);
-        if (ret < GP_OK) return ret;
-        ret = gp_camera_set_port_info (*camera, pi);
-        if (ret < GP_OK) return ret;
+	ret = gp_port_info_list_get_info (portinfolist, p, &pi);
+	if (ret < GP_OK) return ret;
+	ret = gp_camera_set_port_info (*camera, pi);
+	if (ret < GP_OK) return ret;
 	return GP_OK;
 }

examples/best-iso.c
@@ -24,23 +24,23 @@
 static int aperture;
 static float shutterspeed;
 
-static void errordumper(GPLogLevel level, const char *domain, const char *str,
-                 void *data) {
-  fprintf(stdout, "%s\n", str);
+static void errordumper(GPLogLevel level, const char *domain, const char *str, void *data)
+{
+	fprintf(stdout, "%s\n", str);
 }
 
 static void
 ctx_error_func (GPContext *context, const char *str, void *data)
 {
-        fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
-        fflush   (stderr);
+	fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
+	fflush   (stderr);
 }
 
 static void
 ctx_status_func (GPContext *context, const char *str, void *data)
 {
-        fprintf  (stderr, "%s\n", str);
-        fflush   (stderr);
+	fprintf  (stderr, "%s\n", str);
+	fflush   (stderr);
 }
 
 static
@@ -51,16 +51,16 @@ GPContext* sample_create_context() {
 	context = gp_context_new();
 
 	/* All the parts below are optional! */
-        gp_context_set_error_func (context, ctx_error_func, NULL);
-        gp_context_set_status_func (context, ctx_status_func, NULL);
+	gp_context_set_error_func (context, ctx_error_func, NULL);
+	gp_context_set_status_func (context, ctx_status_func, NULL);
 
 	/* also:
 	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
-        gp_context_set_message_func   (p->context, ctx_message_func, p);
-        if (isatty (STDOUT_FILENO))
-                gp_context_set_progress_funcs (p->context,
-                        ctx_progress_start_func, ctx_progress_update_func,
-                        ctx_progress_stop_func, p);
+	gp_context_set_message_func   (p->context, ctx_message_func, p);
+	if (isatty (STDOUT_FILENO))
+		gp_context_set_progress_funcs (p->context,
+			ctx_progress_start_func, ctx_progress_update_func,
+			ctx_progress_stop_func, p);
 	 */
 	return context;
 }
@@ -115,9 +115,9 @@ get_config_value_string (Camera *camera, const char *key, char **str, GPContext 
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT:
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);
@@ -174,9 +174,9 @@ set_config_value_string (Camera *camera, const char *key, const char *val, GPCon
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT:
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT:
 		/* This is the actual set call. Note that we keep
 		 * ownership of the string and have to free it if necessary.
 		 */
@@ -186,7 +186,7 @@ set_config_value_string (Camera *camera, const char *key, const char *val, GPCon
 			goto out;
 		}
 		break;
-        case GP_WIDGET_TOGGLE: {
+	case GP_WIDGET_TOGGLE: {
 		int ival;
 
 		sscanf(val,"%d",&ival);
@@ -321,9 +321,9 @@ main(int argc, char **argv) {
 	int	retval, iso, tries;
 	char	buf[20];
 	GPContext *context = sample_create_context();
-        /*int	fd;*/
-        CameraFile *file;
-        /*CameraFilePath camera_file_path;*/
+	/*int	fd;*/
+	CameraFile *file;
+	/*CameraFilePath camera_file_path;*/
 
 	gp_log_add_func(GP_LOG_ERROR, errordumper, NULL);
 	gp_camera_new(&camera);
@@ -404,31 +404,31 @@ main(int argc, char **argv) {
 
 #if 0
 
-        printf("Capturing.\n");
+	printf("Capturing.\n");
 
-        retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &camera_file_path, context);
-        if (retval != GP_OK) {
+	retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &camera_file_path, context);
+	if (retval != GP_OK) {
 		printf("  capture failed: %d\n", retval);
 		exit(1);
 	}
 
-        printf("Pathname on the camera: %s/%s\n", camera_file_path.folder, camera_file_path.name);
+	printf("Pathname on the camera: %s/%s\n", camera_file_path.folder, camera_file_path.name);
 
-        fd = open("capture.jpg", O_CREAT | O_WRONLY | O_BINARY, 0644);
+	fd = open("capture.jpg", O_CREAT | O_WRONLY | O_BINARY, 0644);
 
-        retval = gp_file_new_from_fd(&file, fd);
-        if (retval != GP_OK) printf("  gp_file_new failed: %d\n", retval);
+	retval = gp_file_new_from_fd(&file, fd);
+	if (retval != GP_OK) printf("  gp_file_new failed: %d\n", retval);
 
-        retval = gp_camera_file_get(camera, camera_file_path.folder, camera_file_path.name,
-                     GP_FILE_TYPE_NORMAL, file, context);
-        if (retval != GP_OK) printf("  file_get failed: %d\n", retval);
+	retval = gp_camera_file_get(camera, camera_file_path.folder, camera_file_path.name,
+		     GP_FILE_TYPE_NORMAL, file, context);
+	if (retval != GP_OK) printf("  file_get failed: %d\n", retval);
 
-        printf("Deleting downloaded image.\n");
-        retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
-                        context);
-        if (retval != GP_OK) printf("  Retval: %d\n", retval);
+	printf("Deleting downloaded image.\n");
+	retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
+			context);
+	if (retval != GP_OK) printf("  Retval: %d\n", retval);
 
-        gp_file_free(file);
+	gp_file_free(file);
 
 #endif
 #if 1

examples/config.c
@@ -62,9 +62,9 @@ get_config_value_string (Camera *camera, const char *key, char **str, GPContext 
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT:
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);
@@ -121,9 +121,9 @@ set_config_value_string (Camera *camera, const char *key, const char *val, GPCon
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_MENU:
-        case GP_WIDGET_RADIO:
-        case GP_WIDGET_TEXT:
+	case GP_WIDGET_MENU:
+	case GP_WIDGET_RADIO:
+	case GP_WIDGET_TEXT:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);
@@ -178,7 +178,7 @@ canon_enable_capture (Camera *camera, int onoff, GPContext *context) {
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_TOGGLE:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);

examples/context.c
@@ -4,15 +4,15 @@
 static void
 ctx_error_func (GPContext *context, const char *str, void *data)
 {
-        fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
-        fflush   (stderr);
+	fprintf  (stderr, "\n*** Contexterror ***              \n%s\n",str);
+	fflush   (stderr);
 }
 
 static void
 ctx_status_func (GPContext *context, const char *str, void *data)
 {
-        fprintf  (stderr, "%s\n", str);
-        fflush   (stderr);
+	fprintf  (stderr, "%s\n", str);
+	fflush   (stderr);
 }
 
 GPContext* sample_create_context() {
@@ -22,16 +22,16 @@ GPContext* sample_create_context() {
 	context = gp_context_new();
 
 	/* All the parts below are optional! */
-        gp_context_set_error_func (context, ctx_error_func, NULL);
-        gp_context_set_status_func (context, ctx_status_func, NULL);
+	gp_context_set_error_func (context, ctx_error_func, NULL);
+	gp_context_set_status_func (context, ctx_status_func, NULL);
 
 	/* also:
 	gp_context_set_cancel_func    (p->context, ctx_cancel_func,  p);
-        gp_context_set_message_func   (p->context, ctx_message_func, p);
-        if (isatty (STDOUT_FILENO))
-                gp_context_set_progress_funcs (p->context,
-                        ctx_progress_start_func, ctx_progress_update_func,
-                        ctx_progress_stop_func, p);
+	gp_context_set_message_func   (p->context, ctx_message_func, p);
+	if (isatty (STDOUT_FILENO))
+		gp_context_set_progress_funcs (p->context,
+			ctx_progress_start_func, ctx_progress_update_func,
+			ctx_progress_stop_func, p);
 	 */
 	return context;
 }

examples/focus.c
@@ -45,7 +45,7 @@ camera_eosviewfinder(Camera *camera, GPContext *context, int onoff) {
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_TOGGLE:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);
@@ -99,7 +99,7 @@ camera_auto_focus(Camera *camera, GPContext *context, int onoff) {
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_TOGGLE:
+	case GP_WIDGET_TOGGLE:
 		break;
 	default:
 		fprintf (stderr, "widget has bad type %d\n", type);
@@ -161,7 +161,7 @@ camera_manual_focus (Camera *camera, int xx, GPContext *context) {
 		goto out;
 	}
 	switch (type) {
-        case GP_WIDGET_RADIO: {
+	case GP_WIDGET_RADIO: {
 		int choices = gp_widget_count_choices (child);
 
 		ret = gp_widget_get_value (child, &mval);
@@ -184,7 +184,7 @@ camera_manual_focus (Camera *camera, int xx, GPContext *context) {
 		}
 		break;
 	}
-        case GP_WIDGET_RANGE:
+	case GP_WIDGET_RANGE:
 		ret = gp_widget_get_value (child, &rval);
 		if (ret < GP_OK) {
 			fprintf (stderr, "could not get widget value: %d\n", ret);

examples/foo.c
@@ -1,6 +1,6 @@
 /*
 
-  My Preview  Trying to capture preview images with libgphoto2
+	My Preview  Trying to capture preview images with libgphoto2
 
 */
 
@@ -22,42 +22,42 @@ Camera  *canon;
 GPContext *canoncontext;
 
 void get_one(void) {  // capture 1 preview image
-  CameraFile *file;
-  char jpgname[] = "test2.jpg";
-  int rslt;
-  rslt  = gp_file_new(&file);
-  if (rslt) {
-    printf("Error creating new CameraFile %i\n",rslt);
-    exit(1);
-  }
-  // skipping focus for now but do it here
-  // skipping zoom also
-  rslt = gp_camera_capture_preview(canon, file, canoncontext);
-  if (rslt) {
-    printf("error capturing preview %i\n",rslt);
-    exit(1);
-  }
-  rslt = gp_file_save(file, jpgname);
-  if (rslt) {
-    printf("Error saving file (in gp_file_save) %i\n",rslt);
-    exit(1);
-  }
+	CameraFile *file;
+	char jpgname[] = "test2.jpg";
+	int rslt;
+	rslt  = gp_file_new(&file);
+	if (rslt) {
+		printf("Error creating new CameraFile %i\n",rslt);
+		exit(1);
+	}
+	// skipping focus for now but do it here
+	// skipping zoom also
+	rslt = gp_camera_capture_preview(canon, file, canoncontext);
+	if (rslt) {
+		printf("error capturing preview %i\n",rslt);
+		exit(1);
+	}
+	rslt = gp_file_save(file, jpgname);
+	if (rslt) {
+		printf("Error saving file (in gp_file_save) %i\n",rslt);
+		exit(1);
+	}
 }
 
 int main(void) {
-  int rslt;
-  // skip this for now
+	int rslt;
+	// skip this for now
 //  gp_log_add_func(GP_LOG_ERROR, errordumper, NULL);
-  gp_camera_new(&canon);
-  canoncontext =  sample_create_context();
-  printf("Camera init.  Takes about 10 seconds.\n");
-  rslt = gp_camera_init(canon, canoncontext);
-  if (rslt) {
-    printf("Error initializing camera %i\n",rslt);
-    exit(1);
-  }
-  //canon_enable_capture(canon, TRUE, canoncontext);  // in config.c
-  get_one();
-  gp_camera_exit(canon, canoncontext);
-  return 0;
+	gp_camera_new(&canon);
+	canoncontext =  sample_create_context();
+	printf("Camera init.  Takes about 10 seconds.\n");
+	rslt = gp_camera_init(canon, canoncontext);
+	if (rslt) {
+		printf("Error initializing camera %i\n",rslt);
+		exit(1);
+	}
+	//canon_enable_capture(canon, TRUE, canoncontext);  // in config.c
+	get_one();
+	gp_camera_exit(canon, canoncontext);
+	return 0;
 }

examples/lunkwill-canon-capture.c
@@ -22,12 +22,11 @@
 
 #include "samples.h"
 
-static void errordumper(GPLogLevel level, const char *domain, const char *str,
-                 void *data) {
-  struct timeval tv;
+static void errordumper(GPLogLevel level, const char *domain, const char *str, void *data) {
+	struct timeval tv;
 
-  gettimeofday (&tv, NULL);
-  fprintf(stdout, "%d.%d: %s\n", (int)tv.tv_sec, (int)tv.tv_usec, str);
+	gettimeofday (&tv, NULL);
+	fprintf(stdout, "%d.%d: %s\n", (int)tv.tv_sec, (int)tv.tv_usec, str);
 }
 
 /* This seems to have no effect on where images go

commit 6cd2d644b247384e24697d1e10967f2de40d8025

fix regression introduced in #1002
At least, I found it myself... :-/

https://github.com/gphoto/libgphoto2/commit/5c9d7b27599b16a744077e7a9ef7937d2132e1b1?diff=unified&w=1#diff-bce068530fdda102b15492a416c91c8d1dd7681b28c7443ad53b0241dfcb0068

libgphoto2/gphoto2-camera.c
@@ -1014,7 +1014,7 @@ _get_widget_names (CameraWidget *widget, CameraList *list)
 	case GP_WIDGET_MENU:
 	case GP_WIDGET_RADIO:
 	case GP_WIDGET_TEXT:
-	GP_WIDGET_RANGE:
+	case GP_WIDGET_RANGE:
 	case GP_WIDGET_TOGGLE:
 	case GP_WIDGET_DATE: {
 		const char *name;