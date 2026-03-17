71c0ff65450afb460358d28d7bc57861207c4d52
Marcus Meissner <marcus@jet.franken.de>
2026-03-17 09:02:59 +0100

    use snprintf on a fixed size buffer
    fixes https://github.com/gphoto/libgphoto2/issues/1224

--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -5122,7 +5122,7 @@ camera_sony_capture (Camera *camera, CameraCaptureType type, CameraFilePath *pat
 	log_objectinfo(params, &oi);
 	sprintf (path->folder,"/");
 	if (oi.Filename && strlen(oi.Filename) > 4) {
-		sprintf (path->name, "capt_%s", oi.Filename);  // capt prefix is mandatory when deleting file
+		snprintf (path->name, sizeof(path->name), "capt_%s", oi.Filename);  // capt prefix is mandatory when deleting file
 	} else {
 		if (oi.ObjectFormat == PTP_OFC_SONY_RAW)
 			sprintf (path->name, "capt%04d.arw", params->capcnt++);
